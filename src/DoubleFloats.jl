9e5a2c2af5924aa51e7ec7fb5b14fc68e56f22b9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-26 14:11:19 -0500

    include doublefloat

--- src/DoubleFloats.jl
+++ src/DoubleFloats.jl
@@ -6,32 +6,6 @@ import Base.IEEEFloat # Union{Float64, Float32, Float16}
 
 using AccurateArithmetic
 
-
-# Emphasis expresses algorithmic preference
-# Accuracy and Performants are Traits of Emphasis
-
-"""
-    Emphasis
-
-An abstract type for algorithmic preferences:
-
-- [`Accuracy`](@ref) emphasizes accuracy.
-- [`Performance`](@ref) emphasizes performance.
-"""
-abstract type Emphasis end
-
-"""
-    Accuracy
-
-Use this as the Emphasis for the most accurate calcuations.
-"""
-struct Accuracy    <: Emphasis end
-
-"""
-    Performance
-
-Use this as the Emphasis for the most performant calcuations.
-"""
-struct Performance <: Emphasis end
+include("DoubleFloat.jl")
 
 end # module DoubleFloats
