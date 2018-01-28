8ee379e4d861b12281d956e5c69baf1ea3243182
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-28 13:18:33 -0500

    Update arith.jl

--- src/ops/arith.jl
+++ src/ops/arith.jl
@@ -5,23 +5,23 @@ import Base: (+), (-), (*), (/), inv #square, inv, div, rem, cld, fld, mod, divr
 
 # Algorithm 6 from Tight and rigourous error bounds for basic building blocks of double-word arithmetic
 function (+)(x::Double{T, E}, y::Double{T,E}) where {T<:SysFloat, E<:Emphasis}
-    hi, lo = two_sum(x.hi, y.hi)
-    thi, tlo = two_sum(x.lo, y.lo)
+    hi, lo = add_acc(x.hi, y.hi)
+    thi, tlo = add_acc(x.lo, y.lo)
     c = lo + thi
-    hi, lo = two_sum_hilo(hi, c)
+    hi, lo = add_hilo_acc(hi, c)
     c = tlo + lo
-    hi, lo = two_sum_hilo(hi, c)
+    hi, lo = add_hilo_acc(hi, c)
     return Double{T,E}(hi, lo)
 end
 
 # Algorithm 6 from Tight and rigourous error bounds for basic building blocks of double-word arithmetic
 function add_dd_dd(xhi::T, xlo::T, yhi::T, ylo::T) where T<:SysFloat
-    hi, lo = two_sum(xhi, yhi)
-    thi, tlo = two_sum(xlo, ylo)
+    hi, lo = add_acc(xhi, yhi)
+    thi, tlo = add_acc(xlo, ylo)
     c = lo + thi
-    hi, lo = two_sum_hilo(hi, c)
+    hi, lo = add_hilo_acc(hi, c)
     c = tlo + lo
-    hi, lo = two_sum_hilo(hi, c)
+    hi, lo = add_hilo_acc(hi, c)
     return hi, lo
 end
 
@@ -31,24 +31,24 @@ end
 # Algorithm 6 from Tight and rigourous error bounds for basic building blocks of double-word arithmetic
 # reworked for subraction
 function (-)(x::Double{T, E}, y::Double{T,E}) where {T<:SysFloat, E<:Emphasis}
-    hi, lo = two_diff(x.hi, y.hi)
-    thi, tlo = two_diff(x.lo, y.lo)
+    hi, lo = sub_acc(x.hi, y.hi)
+    thi, tlo = sub_acc(x.lo, y.lo)
     c = lo + thi
-    hi, lo = two_sum_hilo(hi, c)
+    hi, lo = add_hilo_acc(hi, c)
     c = tlo + lo
-    hi, lo = two_sum_hilo(hi, c)
+    hi, lo = add_hilo_acc(hi, c)
     return Double{T,E}(hi, lo)
 end
 
 # Algorithm 6 from Tight and rigourous error bounds for basic building blocks of double-word arithmetic
 # reworked for subtraction
 function sub_dd_dd(xhi::T, xlo::T, yhi::T, ylo::T) where T<:SysFloat
-    hi, lo = two_diff(xhi, yhi)
-    thi, tlo = two_diff(xlo, ylo)
+    hi, lo = sub_acc(xhi, yhi)
+    thi, tlo = sub_acc(xlo, ylo)
     c = lo + thi
-    hi, lo = two_sum_hilo(hi, c)
+    hi, lo = add_hilo_acc(hi, c)
     c = tlo + lo
-    hi, lo = two_sum_hilo(hi, c)
+    hi, lo = add_hilo_acc(hi, c)
     return hi, lo
 end
 
@@ -59,33 +59,33 @@ experimental relerr ldexp(3.936,-106) == ldexp(1.968, -107)
 
 # Algorithm 12 from Tight and rigourous error bounds for basic building blocks of double-word arithmetic
 function prod_dd_dd(xhi::T, xlo::T, yhi::T, ylo::T) where T<:SysFloat
-    hi, lo = two_prod(xhi, yhi)
+    hi, lo = mul_acc(xhi, yhi)
     t = xlo * ylo
     t = fma(xhi, ylo, t)
     t = fma(xlo, yhi, t)
     t = lo + t
-    hi, lo = two_sum_hilo(hi, t)
+    hi, lo = add_hilo_acc(hi, t)
     return hi, lo
 end
 
 # Algorithm 12 from Tight and rigourous error bounds for basic building blocks of double-word arithmetic
 function (*)(x::Double{T,E}, y::Double{T,E}) where {T<:SysFloat,E<:Emphasis}
-    hi, lo = two_prod(x.hi, y.hi)
+    hi, lo = mul_acc(x.hi, y.hi)
     t = x.lo * y.lo
     t = fma(x.hi, y.lo, t)
     t = fma(x.lo, y.hi, t)
     t = lo + t
-    hi, lo = two_sum_hilo(hi, t)
+    hi, lo = add_hilo_acc(hi, t)
     return Double{T,E}(hi, lo)
 end
 
 function (square)(x::Double{T,E}) where {T<:SysFloat,E<:Emphasis}
-    hi, lo = two_prod(x.hi, x.hi)
+    hi, lo = mul_acc(x.hi, x.hi)
     t = x.lo * x.lo
     t = fma(x.hi, x.lo, t)
     t = fma(x.lo, x.hi, t)
     t = lo + t
-    hi, lo = two_sum_hilo(hi, t)
+    hi, lo = add_hilo_acc(hi, t)
     return Double{T,E}(hi, lo)
 end
 
@@ -93,11 +93,11 @@ end
 function (/)(a::Double{T,Performance}, b::Double{T,Performance}) where {T<:SysFloat}
     hi1 = a.hi / b.hi
     hi, lo = prod_dd_fl(b.hi, b.lo, hi1)
-    xhi, xlo = two_sum(a.hi, -hi)
+    xhi, xlo = add_acc(a.hi, -hi)
     xlo -= lo
     xlo += a.lo
     hi2 = (xhi + xlo) / b.hi
-    hi, lo = two_sum(hi1, hi2)
+    hi, lo = add_acc(hi1, hi2)
     return Double{T,Performance}(hi, lo)
 end
 
@@ -107,7 +107,7 @@ function (/)(x::Double{T,Accuracy}, y::Double{T,Accuracy}) where {T<:SysFloat}
     hi = inv(y.hi)
     rhi = fma(-y.hi, hi, one(T))
     rlo = y.lo * hi
-    rhi, rlo = two_sum_hilo(rhi, rlo)
+    rhi, rlo = add_hilo_acc(rhi, rlo)
     rhi, rlo = prod_dd_fl(rhi, rlo, hi)
     rhi, rlo = add_dd_fl(rhi, rlo, hi)
     hi, lo = prod__dd_dd(x.hi, x.lo, rhi, rlo)
@@ -123,7 +123,7 @@ function (/)(a::Double{T,Accuracy}, b::Double{T,Accuracy}) where {T<:SysFloat}
     th,tl = prod_dd_fl(b.hi,b.lo,q2)
     rh,rl = add_dd_dd(rh, rl, -th,-tl)
     q3 = rh / b.hi
-    q1, q2 = two_sum_hilo(q1, q2)
+    q1, q2 = add_hilo_acc(q1, q2)
     rh,rl = add_dd_fl(q1, q2, q3)
     return Double{T,Accuracy}(rh, rl)
 end
