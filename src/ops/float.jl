5344b94269c3737ddbbe30a5da82f71473d6db51
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-28 15:09:44 -0500

    use AbstractFloat

--- src/ops/float.jl
+++ src/ops/float.jl
@@ -2,43 +2,43 @@ import Base: inv, sqrt, (+), (-), (*), (/)
 
 export sqr, cub
 
-function sqr(::Type{E}, a::T) where {T<:SysFloat, E<:Emphasis}
+function sqr(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
    return Double{T,E}(sqr_acc(a)...,)
 end
 
-function cub(::Type{E}, a::T) where {T<:SysFloat, E<:Emphasis}
+function cub(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
    return Double{T,E}(cub_acc(a)...,)
 end
 
-function inv(::Type{E}, a::T) where {T<:SysFloat, E<:Emphasis}
+function inv(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
    return Double{T,E}(inv_acc(a)...,)
 end
 
-function sqrt(::Type{E}, a::T) where {T<:SysFloat, E<:Emphasis}
+function sqrt(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
    return Double{T,E}(sqrt_acc(a)...,)
 end
 
-function (+)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
+function (+)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
    return Double{T,E}(add_acc(a, b)...,)
 end
 
-@inline (+)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:SysFloat, F2<:SysFloat} = (+)(E, promote(a, b)...)
+@inline (+)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (+)(E, promote(a, b)...)
 
-function (-)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
+function (-)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
    return Double{T,E}(sub_acc(a, b)...,)
 end
-@inline (-)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:SysFloat, F2<:SysFloat} = (-)(E, promote(a, b)...)
+@inline (-)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (-)(E, promote(a, b)...)
 
 function (*)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
    return Double{T,E}(mul_acc(a, b)...,)
 end
-@inline (*)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:SysFloat, F2<:SysFloat} = (*)(E, promote(a, b)...)
+@inline (*)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (*)(E, promote(a, b)...)
 
-function inv(::Type{E}, a::T) where {T<:SysFloat, E<:Emphasis}
+function inv(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
    return Double{T,E}(inv_acc(a)...,)
 end
 
-function (/)(::Type{E}, a::T, b::T) where {T<:SysFloat, E<:Emphasis}
+function (/)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
    return Double{T,E}(div_acc(a)...,)
 end
-@inline (/)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:SysFloat, F2<:SysFloat} = (/)(E, promote(a, b)...) lo)
+@inline (/)(::Type{E}, a::F1, b::F2) where {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (/)(E, promote(a, b)...) lo)
