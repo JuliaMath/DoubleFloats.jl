180f9379ec76168e23457d89531aeb06627181e1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-28 14:59:46 -0500

    Update val_isa_cmp.jl

--- src/type/val_isa_cmp.jl
+++ src/type/val_isa_cmp.jl
@@ -8,36 +8,36 @@ else
     export isone
 end
 
-zero(::Type{DoubleFloat{T,E}}) where {T<:Real,E<:Emphasis} =
-    DoubleFloat(E, zero(T), zero(T))
-iszero(x::DoubleFloat{T,E}) where {T<:Real,E<:Emphasis} =
+zero(::Type{Double{T,E}}) where {T<:Real,E<:Emphasis} =
+    Double(E, zero(T), zero(T))
+iszero(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
     iszero(hi(x)) && iszero(lo(x))
 
-one(::Type{DoubleFloat{T,E}}) where {T<:Real,E<:Emphasis} = 
-   DoubleFloat(E, one(T), zero(T))
-isone(x::DoubleFloat{T,E}) where {T<:Real,E<:Emphasis} =
+one(::Type{Double{T,E}}) where {T<:Real,E<:Emphasis} = 
+   Double(E, one(T), zero(T))
+isone(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
     isone(hi(x)) && iszero(lo(x))
 
-NaN(::Type{DoubleFloat{T,E}}) where {T<:Real,E<:Emphasis} = 
-   DoubleFloat(E, T(NaN), zero(T))
-isnan(x::DoubleFloat{T,E}) where {T<:Real,E<:Emphasis} =
+NaN(::Type{Double{T,E}}) where {T<:Real,E<:Emphasis} = 
+   Double(E, T(NaN), zero(T))
+isnan(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
     isnan(hi(x)) && iszero(lo(x))
 
-Inf(::Type{DoubleFloat{T,E}}) where {T<:Real,E<:Emphasis} = 
-   DoubleFloat(E, T(Inf), zero(T))
-isinf(x::DoubleFloat{T,E}) where {T<:Real,E<:Emphasis} =
+Inf(::Type{Double{T,E}}) where {T<:Real,E<:Emphasis} = 
+   Double(E, T(Inf), zero(T))
+isinf(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
     isinf(hi(x)) && iszero(lo(x))
 
-isfinite(x::DoubleFloat{T,E}) where {T<:Real,E<:Emphasis} =
+isfinite(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
     isfinite(hi(x)) && isfinite(lo(x))
-isinteger(x::DoubleFloat{T,E}) where {T<:Real,E<:Emphasis} =
+isinteger(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
     isinteger(lo(x)) && isinteger(hi(x))
-isodd(x::DoubleFloat{T,E}) where {T<:Real,E<:Emphasis} =
+isodd(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
     iszero(lo(x)) ? isinteger(hi(x)) && isodd(hi(x)) : isinteger(lo(x)) && isodd(lo(x))
-iseven(x::DoubleFloat{T,E}) where {T<:Real,E<:Emphasis} =
+iseven(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
     iszero(lo(x)) ? isinteger(hi(x)) && iseven(hi(x)) : isinteger(lo(x)) && iseven(lo(x))
 
-issubnormal(x::DoubleFloat{T,E}) where {T<:Real,E<:Emphasis} =
+issubnormal(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
     issubnormal(hi(x)) || issubnormal(lo(x))
 
 
