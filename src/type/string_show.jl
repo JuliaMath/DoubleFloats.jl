b4baddfeecf42ae2d62830e5cda17c2cd9e2d8c8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-28 02:28:49 -0500

    Create string_show.jl

---
+++ src/type/string_show.jl
@@ -0,0 +1,13 @@
+const EMPHASIS_STR     = ""        # these are used in string()
+const ALT_EMPHASIS_STR = "Fast"    # and prepend "Double"
+
+function Base.string(x::Double{T,EMPHASIS}) where {T}
+    return string(EMPHASIS_STR,"Double(",x.hi,", ",x.lo,")")
+end
+function Base.string(x::Double{T,ALT_EMPHASIS}) where {T}
+    return string(ALT_EMPHASIS_STR,"Double(",x.hi,", ",x.lo,")")
+end
+function Base.show(io::IO, x::Double{T,E}) where  {T, E<:Emphasis}
+    print(io, string(x))
+end
+
