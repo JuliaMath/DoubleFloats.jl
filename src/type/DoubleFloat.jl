4bef8ed6f8bc27586e042ac8afb6f9a35ffff101
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-28 02:25:20 -0500

    Update DoubleFloat.jl

--- src/type/DoubleFloat.jl
+++ src/type/DoubleFloat.jl
@@ -1,1 +1,12 @@
+abstract type AbstractDouble{T}    <: MultipartFloat{T} end
 
+struct Double{T<:AbstractFloat, E<:Emphasis} <: AbstractDouble{T}
+    hi::T
+    lo::T
+end
+
+@inline hi(x::Double{T,E}) where {T,E<:Emphasis} = x.hi
+@inline lo(x::Double{T,E}) where {T,E<:Emphasis} = x.lo
+
+@inline hi(x::T) where {T<:AbstractFloat} = x
+@inline lo(x::T) where {T<:AbstractFloat} = zero(T)
