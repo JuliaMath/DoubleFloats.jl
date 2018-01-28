cee188852aed805997326a4ad5cf35e14657a6bc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-28 08:30:30 -0500

    initializers

--- src/type/DoubleFloat.jl
+++ src/type/DoubleFloat.jl
@@ -12,14 +12,25 @@ end
 @inline lo(x::T) where {T<:AbstractFloat} = zero(T)
 
 # initializers
+
 Double() = Double{Float64, Accuracy}(zero(Float64), zero(Float64))
-Double(::Type{Accuracy})   = Double{Float64, Accuracy}(zero(Float64), zero(Float64))
+Double(::Type{Accuracy}) = Double{Float64, Accuracy}(zero(Float64), zero(Float64))
 Double(::Type{Performance}) = Double{Float64, Performance}(zero(Float64), zero(Float64))
 Double(::Type{Accuracy}, hi::T) where {T<:AbstractFloat} = Double{T, Accuracy}(hi, zero(T))
 Double(::Type{Performance}, hi::T) where {T<:AbstractFloat} = Double{T, Performance}(hi, zero(T))
 Double(::Type{Accuracy}, hi::T, lo::T) where {T<:AbstractFloat} = Double{T, Accuracy}(hi, lo)
 Double(::Type{Performance}, hi::T, lo::T) where {T<:AbstractFloat} = Double{T, Performance}(hi, lo)
-Double(x::T) where {T<:AbstractFloat} = Double{Accuracy, T}(x, zero(T))
-Double(x::T, y::T) where {T<:AbstractFloat} = Double{Accuracy, T}(add_acc(x, y)...,)
+Double(x::T) where {T<:AbstractFloat} = Double{T, Accuracy}(x, zero(T))
+Double(x::T, y::T) where {T<:AbstractFloat} = Double{T, Accuracy}(add_acc(x, y)...,)
+
+FastDouble() = Double{Float64, Performance}(zero(Float64), zero(Float64))
+FastDouble(x::T) where {T<:AbstractFloat} = Double{T, Performance}(x, zero(T))
+FastDouble(x::T, y::T) where {T<:AbstractFloat} = Double{T, Performance}(add_acc(x, y)...,)
+FastDouble(::Type{Accuracy})   = Double{Float64, Performance}(zero(Float64), zero(Float64))
+FastDouble(::Type{Performance}) = Double{Float64, Performance}(zero(Float64), zero(Float64))
+FastDouble(::Type{Accuracy}, hi::T) where {T<:AbstractFloat} = Double{T, Performance}(hi, zero(T))
+FastDouble(::Type{Performance}, hi::T) where {T<:AbstractFloat} = Double{T, Performance}(hi, zero(T))
+FastDouble(::Type{Accuracy}, hi::T, lo::T) where {T<:AbstractFloat} = Double{T, Performance}(hi, lo)
+FastDouble(::Type{Performance}, hi::T, lo::T) where {T<:AbstractFloat} = Double{T, Performance}(hi, lo)
 
 include("string_show.jl")
