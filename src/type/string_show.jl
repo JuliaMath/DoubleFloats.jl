5c6215330c54d0a0f34503abd9c379bfd161b83d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-28 20:34:15 -0500

    two string forms

--- src/type/string_show.jl
+++ src/type/string_show.jl
@@ -1,10 +1,25 @@
-function Base.string(x::Double{T,EMPHASIS}) where {T}
+# use convert(String, x) to obtain a reparsable string (also show(io, x))
+# use String(x) to obtain a string of a 2-value tuple  (also show(x))
+
+function Base.convert(::Type{String}, x::Double{T,EMPHASIS}) where {T}
     return string(EMPHASIS_STR,"Double(",x.hi,", ",x.lo,")")
 end
-function Base.string(x::Double{T,ALT_EMPHASIS}) where {T}
+function Base.convert(::Type{String}, x::Double{T,ALT_EMPHASIS}) where {T}
     return string(ALT_EMPHASIS_STR,"Double(",x.hi,", ",x.lo,")")
 end
+
+function Base.string(x::Double{T,EMPHASIS}) where {T}
+    return string("(",x.hi,", ",x.lo,")")
+end
+function Base.string(x::Double{T,ALT_EMPHASIS}) where {T}
+    return string("(",x.hi,", ",x.lo,")")
+end
+
 function Base.show(io::IO, x::Double{T,E}) where  {T, E<:Emphasis}
-    print(io, string(x))
+    print(io, convert(String, x))
+end
+
+function Base.show(x::Double{T,E}) where  {T, E<:Emphasis}
+    print(Base.STDOUT, string(x))
 end
 
