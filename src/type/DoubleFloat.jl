9cda4e6aadedfe8857d7b2118dcad82434f8223f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-28 23:16:33 -0500

    accept more types

--- src/type/DoubleFloat.jl
+++ src/type/DoubleFloat.jl
@@ -19,32 +19,49 @@ Double(::Type{Accuracy}, hi::T) where {T<:AbstractFloat} = Double{T, Accuracy}(h
 Double(::Type{Performance}, hi::T) where {T<:AbstractFloat} = Double{T, Performance}(hi, zero(T))
 Double(::Type{Accuracy}, hi::T, lo::T) where {T<:AbstractFloat} = Double{T, Accuracy}(hi, lo)
 Double(::Type{Performance}, hi::T, lo::T) where {T<:AbstractFloat} = Double{T, Performance}(hi, lo)
-Double(x::T) where {T<:AbstractFloat} = Double{T, Accuracy}(x, zero(T))
-Double(x::T, y::T) where {T<:AbstractFloat} = Double{T, Accuracy}(add_acc(x, y)...,)
+
+Double(x::T) where {T<:AbstractFloat} =
+    Double{Float64, Accuracy}(Float64(x), zero(Float64))
+Double(x::T, y::T) where {T<:AbstractFloat} =
+    Double{Float64, Accuracy}(add_acc(Float64(x), Float64(y))...,)
+Double(x::T) where {T<:String} =
+    Double{Float64, Accuracy}(Float64(x), zero(Float64))
 
 FastDouble() = Double{Float64, Performance}(zero(Float64), zero(Float64))
-FastDouble(x::T) where {T<:AbstractFloat} = Double{T, Performance}(x, zero(T))
-FastDouble(x::T, y::T) where {T<:AbstractFloat} = Double{T, Performance}(add_acc(x, y)...,)
+FastDouble(x::T) where {T<:AbstractFloat} =
+    Double{Float64, Performance}(x, zero(Float64))
+FastDouble(x::T, y::T) where {T<:AbstractFloat} =
+    Double{Float64, Performance}(add_acc(x, y)...,)
+
+FastDouble(x::T) where {T<:Real} =
+    FastDouble{Float64, Performance}(Float64(x), zero(Float64))
+FastDouble(x::T, y::T) where {T<:Real} =
+    FastDouble{Float64, Performance}(add_acc(convert(Float64,x), convert(Float64,y))...,)
 
 # a fast type specific hash function helps
-
-const hash_accuracy = hash(Accuracy)
-const hash_performance = hash(Performance)
+import Base: hash, hx, fptoui
 
 const hash_doublefloat_lo = (UInt === UInt64) ? 0x9bad5ebab034fe78 : 0x72da40cb
-const hash_0_doublefloat_lo = hash(zero(UInt), hash_doublefloat_lo)
+const hash_0_dfloat_lo = hash(zero(UInt), hash_doublefloat_lo)
+const hash_accuracy_lo = hash(hash(Accuracy), hash_doublefloat_lo)
+const hash_precision_lo = hash(hash(Precision), hash_doublefloat_lo)
 
-function Base.hash(x::Double{T,Accuracy}, h::UInt) where {T}
-    isnan(hi(x)) && return (hx_NaN ⊻ h)
-    iszero(lo(x)) && return Base.hx(Base.fptoui(UInt64, abs(hi(x))), hi(x), h)
-    Base.hx(hash_accuracy ⊻ Base.fptoui(UInt64, abs(hi(x))), lo(x), h ⊻ hash_0_doublefloat_lo)
+function hash(x::Double{T,Accuracy}, h::UInt) where {T}
+    !isnan(hi(x)) ? 
+       ( iszero(lo(x)) ? 
+            hx(fptoui(UInt64, abs(hi(x))), hi(x), h ⊻ hash_accuracy_lo) :
+            hx(fptoui(UInt64, abs(hi(x))), lo(x), h ⊻ hash_accuracy_lo)  
+       ) : (hx_NaN ⊻ h)
 end
 
-function Base.hash(x::Double{T,Performance}, h::UInt) where {T}
-    isnan(hi(x)) && return (hx_NaN ⊻ h)
-    iszero(lo(x)) && return Base.hx(Base.fptoui(UInt64, abs(hi(x))), hi(x), h)
-    Base.hx(hash_performance ⊻ Base.fptoui(UInt64, abs(hi(x))), lo(x), h ⊻ hash_0_doublefloat_lo)
+function hash(x::Double{T,Precision}, h::UInt) where {T}
+    !isnan(hi(x)) ? 
+       ( iszero(lo(x)) ? 
+            hx(fptoui(UInt64, abs(hi(x))), hi(x), h ⊻ hash_precision_lo) :
+            hx(fptoui(UInt64, abs(hi(x))), lo(x), h ⊻ hash_precision_lo)  
+       ) : (hx_NaN ⊻ h)
 end
+
 
 include("val_isa_cmp.jl")
 include("string_show.jl")
