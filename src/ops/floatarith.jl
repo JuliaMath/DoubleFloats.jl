3b195c2f50082e980fad876bdc9a792dd77b4aac
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-07 01:11:13 -0500

    correct callee names

--- src/ops/floatarith.jl
+++ src/ops/floatarith.jl
@@ -1,45 +1,41 @@
 import Base: inv, sqrt, (+), (-), (*), (/)
 
+function inv(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
+   return Double{T,E}(inv_(a)...,)
+end
+
 function sqr(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(sqr_hilo(a)...,)
+   return Double{T,E}(sqr_(a)...,)
 end
 
 function cub(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(cub_hilo(a)...,)
+   return Double{T,E}(cub_(a)...,)
 end
 
 function sqrt(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(sqrt_hilo(a)...,)
+   return Double{T,E}(sqrt_(a)...,)
 end
 
 function (+)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(add_hilo(a, b)...,)
+   return Double{T,E}(add_(a, b)...,)
 end
 @inline (+)(::Type{E}, a::F1, b::F2) where 
     {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (+)(E, promote(a, b)...,)
 
 function (-)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(sub_hilo(a, b)...,)
+   return Double{T,E}(sub_(a, b)...,)
 end
 @inline (-)(::Type{E}, a::F1, b::F2) where 
     {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (-)(E, promote(a, b)...,)
 
 function (*)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(mul_hilo(a, b)...,)
+   return Double{T,E}(mul_(a, b)...,)
 end
 @inline (*)(::Type{E}, a::F1, b::F2) where 
     {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (*)(E, promote(a, b)...,)
 
-function inv(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(inv_hilo(a)...,)
-end
-
-function sqrt(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(sqrt_hilo(a)...,)
-end
-
 function (/)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(div_hilo(a, b)...,)
+   return Double{T,E}(div_(a, b)...,)
 end
 @inline (/)(::Type{E}, a::F1, b::F2) where 
     {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (/)(E, promote(a, b)...,)
