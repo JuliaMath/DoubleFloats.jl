a8e0198cfdbcfa2c8bc875edd02a2fc3e0d91cc5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-13 11:14:12 -0500

    remove extra `=`

--- src/type/promote.jl
+++ src/type/promote.jl
@@ -6,13 +6,13 @@ promote_rule(::Type{Double{T,E}}, ::Type{T}) where {T<:AbstractFloat, E<:Emphasi
 convert(::Type{Double{T,E}}, x::Type{T}) where {T<:IEEEFloat, E<:Emphasis} =
     Double{T,E}(x, zero(T))
 
-function convert(::Type{Double{T,E}}, x::Type{BigFloat}) where {T<:AbstractFloat, E<:Emphasis} =
+function convert(::Type{Double{T,E}}, x::Type{BigFloat}) where {T<:AbstractFloat, E<:Emphasis}
     hi = T(x)
     lo = T(x - hi)
     return Double{T,E}(hi, lo)
 end
 
-function convert(::Type{BigFloat} x::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis} =
+function convert(::Type{BigFloat} x::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis}
     hi = BigFloat(hi(x))
     lo = BigFloat(lo(x)
     return hi+lo
