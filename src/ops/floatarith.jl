a54249687ad540bbd65aa2c011c3c1668c7fc71b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-13 17:05:04 -0500

    specialize for fn( (a,b) )

--- src/ops/floatarith.jl
+++ src/ops/floatarith.jl
@@ -1,41 +1,57 @@
 import Base: inv, sqrt, (+), (-), (*), (/)
 
 function inv(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(inv_(a)...,)
+   return Double{T,E}(inv_2(a))
 end
 
 function sqr(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(sqr_(a)...,)
+   return Double{T,E}(sqr_2(a))
 end
 
 function cub(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(cub_(a)...,)
+   return Double{T,E}(cub_2(a))
 end
 
 function sqrt(::Type{E}, a::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(sqrt_(a)...,)
+   return Double{T,E}(sqrt_2(a))
 end
 
-function (+)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(add_(a, b)...,)
+@inline function (+)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
+   return Double{T,E}(add_2(a, b))
 end
+@inline function (+)(::Type{E}, ab::Tuple{T, T}) where {T<:AbstractFloat, E<:Emphasis}
+   return (+)(E, ab[1]. ab[2])
+end
+
+@inline function (-)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
+   return Double{T,E}(sub_2(a, b))
+end
+@inline function (-)(::Type{E}, ab::Tuple{T, T}) where {T<:AbstractFloat, E<:Emphasis}
+   return (-)(E, ab[1]. ab[2])
+end
+   
+@inline function (*)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
+   return Double{T,E}(mul_2(a, b))
+end
+@inline function (*)(::Type{E}, ab::Tuple{T, T}) where {T<:AbstractFloat, E<:Emphasis}
+   return (*)(E, ab[1]. ab[2])
+end
+      
+@inline function (/)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
+   return Double{T,E}(div_2(a, b))
+end
+@inline function (/)(::Type{E}, ab::Tuple{T, T}) where {T<:AbstractFloat, E<:Emphasis}
+   return (/)(E, ab[1]. ab[2])
+end
+
 @inline (+)(::Type{E}, a::F1, b::F2) where 
-    {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (+)(E, promote(a, b)...,)
+    {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (+)(E, promote(a, b))
 
-function (-)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(sub_(a, b)...,)
-end
 @inline (-)(::Type{E}, a::F1, b::F2) where 
-    {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (-)(E, promote(a, b)...,)
+    {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (-)(E, promote(a, b))
 
-function (*)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(mul_(a, b)...,)
-end
 @inline (*)(::Type{E}, a::F1, b::F2) where 
-    {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (*)(E, promote(a, b)...,)
+    {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (*)(E, promote(a, b))
 
-function (/)(::Type{E}, a::T, b::T) where {T<:AbstractFloat, E<:Emphasis}
-   return Double{T,E}(div_(a, b)...,)
-end
 @inline (/)(::Type{E}, a::F1, b::F2) where 
-    {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (/)(E, promote(a, b)...,)
+    {E<:Emphasis, F1<:AbstractFloat, F2<:AbstractFloat} = (/)(E, promote(a, b))
