916eaa4f7925ee90542591285c132bbe665aa9b7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-14 00:19:24 -0500

    missing at

--- src/ops/op_dbfp_db.jl
+++ src/ops/op_dbfp_db.jl
@@ -1,4 +1,4 @@
-inline function add_dbfp_db(x::Double{T,E}, y::T) where {T<:AbstractFloat, E<:Emphasis}
+@inline function add_dbfp_db(x::Double{T,E}, y::T) where {T<:AbstractFloat, E<:Emphasis}
     xhi, xlo = HILO(x)
     xhi, xlo = add_2(y, xhi, xlo)
     return Double(E, xhi, xlo)
