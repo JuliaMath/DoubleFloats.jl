3a9dd0577a2971049ecf70cb9ade034e557e791a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-14 01:12:09 -0500

    Update op_dbfp_db.jl

--- src/ops/op_dbfp_db.jl
+++ src/ops/op_dbfp_db.jl
@@ -1,3 +1,4 @@
+
 @inline function add_dbfp_db(x::Double{T,E}, y::T) where {T<:AbstractFloat, E<:Emphasis}
     xhi, xlo = HILO(x)
     xhi, xlo = add_2(y, xhi, xlo)
@@ -10,9 +11,17 @@ end
     return Double(E, xhi, xlo)
 end
 
+# Algorithm 9 from Tight and rigourous error bounds. relative error <= 2u²
+@inline function mul_dd_fl(xhi::T, xlo::T, y::T) where T<:IEEEFloat
+    hi, lo = mul_(xhi, y)
+    t = lo + xlo*y
+    hi, lo = add_hilo_(hi, t)
+    return hi, lo
+end
+
 @inline function mul_dbfp_db(x::Double{T,E}, y::T) where {T<:AbstractFloat, E<:Emphasis}
     xhi, xlo = HILO(x)
-    xhi, xlo = mul_2(y, xhi, xlo)
+    xhi, xlo = mul_dd_fl(xhi, xlo, y)
     return Double(E, xhi, xlo)
 end
 
