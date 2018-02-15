386b3153b46f5f326d969435e2a5d670c45ddb7f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-15 02:35:48 -0500

    powr2_6 root2_6

--- src/ops/op_fp_fp.jl
+++ src/ops/op_fp_fp.jl
@@ -6,10 +6,53 @@ end
    return neg(x)
 end
 
-@inline function sqrt_fp_fp(x::T) where {T<:AbstractFloat}
+@inline function inv_fp_fp(x::T) where {T<:AbstractFloat}
+   return inv(x)
+end
+
+
+@inline function powr2_fp_fp(x::T) where {T<:AbstractFloat}
+   return x*x
+end
+
+@inline function powr3_fp_fp(x::T) where {T<:AbstractFloat}
+   x2 = powr2_fp_fp(x)
+   return x2 * x
+end
+
+@inline function powr4_fp_fp(x::T) where {T<:AbstractFloat}
+   x2 = powr2_fp_fp(x)
+   return x2 * x2
+end
+
+@inline function powr5_fp_fp(x::T) where {T<:AbstractFloat}
+   x2 = powr2_fp_fp(x)
+   x3 = x2 * x
+   return x2 * x3
+end
+
+@inline function powr6_fp_fp(x::T) where {T<:AbstractFloat}
+   x3 = powr3_fp_fp(x)
+   return x3 * x3
+end
+
+
+@inline function root2_fp_fp(x::T) where {T<:AbstractFloat}
    return sqrt(x)
 end
 
-@inline function inv_fp_fp(x::T) where {T<:AbstractFloat}
-   return inv(x)
+@inline function root3_fp_fp(x::T) where {T<:AbstractFloat}
+   return cbrt(x)
+end
+
+@inline function root4_fp_fp(x::T) where {T<:AbstractFloat}
+   return sqrt(sqrt(x))
+end
+
+@inline function root5_fp_fp(x::T) where {T<:AbstractFloat}
+   exp(0.2 * log(x)) 
+end
+
+@inline function root6_fp_fp(x::T) where {T<:AbstractFloat}
+   return cbrt(sqrt(x))
 end
