01791d4ede6e04149cc3bf9ecc1ae9592b3b1b69
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 08:18:14 -0500

    Update op_fpfp_dd.jl

--- src/ops/op_fpfp_dd.jl
+++ src/ops/op_fpfp_dd.jl
@@ -1,15 +1,15 @@
-@inline function add_fpfp_dd(::Type{E}, x::T, y::T) where {T<:AbstractFloat, E<:Emphasis}
+@inline function add_fpfp_dd(x::T, y::T) where {T<:AbstractFloat}
     return add_2(x, y)
 end
 
-@inline function sub_fpfp_dd(::Type{E}, x::T, y::T) where {T<:AbstractFloat, E<:Emphasis}
+@inline function sub_fpfp_dd(x::T, y::T) where {T<:AbstractFloat}
     return sub_2(x, y)
 end
 
-@inline function mul_fpfp_dd(::Type{E}, x::T, y::T) where {T<:AbstractFloat, E<:Emphasis}
+@inline function mul_fpfp_dd(x::T, y::T) where {T<:AbstractFloat}
     return mul_2(x, y)
 end
 
-@inline function dvi_fpfp_dd(::Type{E}, x::T, y::T) where {T<:AbstractFloat, E<:Emphasis}
+@inline function dvi_fpfp_dd(::Type{E}, x::T, y::T) where {T<:AbstractFloat}
     return dvi_2(x, y)
 end
