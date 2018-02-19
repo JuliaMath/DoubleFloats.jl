b754166fd4546159c9c06520e3226889d408d6a1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 10:54:35 -0500

    Update randdoubles.jl

--- test/randdoubles.jl
+++ test/randdoubles.jl
@@ -2,6 +2,7 @@ function randdouble(::Type{T}, n::Int=1; emin::Int=exponent_min(T), emax::Int=ex
     r1 = randfloat(T, n, emin=emin, emax=emax, signed=signed)[1]
     ex = exponent(r1) - 53
     r2 = ldexp(rand(T), ex)
+    r2 = copysign(r2, rand(-1:2:1))
     return (r1, r2)
 end
 
