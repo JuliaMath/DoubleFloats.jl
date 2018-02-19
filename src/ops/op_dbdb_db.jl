bfec80bf011572e23bf500d82f040a63ac6fe64f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 04:02:31 -0500

    Update op_dbdb_db.jl

--- src/ops/op_dbdb_db.jl
+++ src/ops/op_dbdb_db.jl
@@ -15,7 +15,6 @@ end
 end
 
 @inline function dvi_dbdb_db(x::Double{T,E}, y::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis}
-    yinv = inv_dd_dd(HILO(y))
-    hi, lo = mul_dddd_dd(HILO(x), yinv)
+    hi, lo = dvi_dddd_dd(HILO(x), HILO(y))
     return Double(E, hi, lo)
 end
