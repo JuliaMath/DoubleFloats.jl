55c9f0e8f17bfa92d3235c756b41969c0ab4d9a4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 08:40:54 -0500

    Update op_fpfp_dd.jl

--- src/ops/op_fpfp_dd.jl
+++ src/ops/op_fpfp_dd.jl
@@ -1,15 +1,11 @@
-@inline function add_fpfp_dd(x::T, y::T) where {T<:AbstractFloat}
-    return add_2(x, y)
-end
 
-@inline function sub_fpfp_dd(x::T, y::T) where {T<:AbstractFloat}
-    return sub_2(x, y)
-end
+@inline add_fpfp_dd(x::T, y::T) where {T<:AbstractFloat} = add_2(x, y)
+@inline sub_fpfp_dd(x::T, y::T) where {T<:AbstractFloat} = sub_2(x, y)
+@inline mul_fpfp_dd(x::T, y::T) where {T<:AbstractFloat} = mul_2(x, y)
+@inline dvi_fpfp_dd(x::T, y::T) where {T<:AbstractFloat} = dvi_2(x, y)
+# seems to help
+@inline add_fpfp_dd(x::T, y::T) where {T<:IEEEFloat} = add_2(x, y)
+@inline sub_fpfp_dd(x::T, y::T) where {T<:IEEEFloat} = sub_2(x, y)
+@inline mul_fpfp_dd(x::T, y::T) where {T<:IEEEFloat} = mul_2(x, y)
+@inline dvi_fpfp_dd(x::T, y::T) where {T<:IEEEFloat} = dvi_2(x, y)
 
-@inline function mul_fpfp_dd(x::T, y::T) where {T<:AbstractFloat}
-    return mul_2(x, y)
-end
-
-@inline function dvi_fpfp_dd(x::T, y::T) where {T<:AbstractFloat}
-    return dvi_2(x, y)
-end
