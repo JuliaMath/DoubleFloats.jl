b17eadd00e5f9411469daf8e4d0c270b1823286b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 02:52:43 -0500

    Create op.jl

---
+++ src/ops/op.jl
@@ -0,0 +1,13 @@
+include("op_fp_fp.jl")
+include("op_fp_dd.jl")
+include("op_fp_db.jl")
+include("op_fpfp_dd.jl")
+include("op_fpfp_db.jl")
+include("op_fpdd_dd.jl")
+include("op_ddfp_dd.jl")
+include("op_fpdb_db.jl")
+include("op_dbfp_db.jl")
+include("op_dd_dd.jl")
+include("op_db_db.jl")
+include("op_dbdb_db.jl")
+
