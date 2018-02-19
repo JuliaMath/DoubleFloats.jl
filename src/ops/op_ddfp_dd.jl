b917baeda9b4537286f77082693bb418b87717ca
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 04:25:16 -0500

    Update op_ddfp_dd.jl

--- src/ops/op_ddfp_dd.jl
+++ src/ops/op_ddfp_dd.jl
@@ -10,13 +10,13 @@ end
     return hi, lo
 end
 
-@inline function mul_ddfp_db(x::Tuple{T,T}, y::T) where {T<:AbstractFloat}
+@inline function mul_ddfp_dd(x::Tuple{T,T}, y::T) where {T<:AbstractFloat}
     hi, lo = x
     hi, lo = mul_2(hi, y, lo)
     return hi, lo
 end
 
-@inline function dvi_ddfp_db(x::Tuple{T,T}, y::T) where {T<:AbstractFloat}
+@inline function dvi_ddfp_dd(x::Tuple{T,T}, y::T) where {T<:AbstractFloat}
     hi, lo = x
     yinv = inv(y)
     hi, lo = mul_2(hi, lo, yinv)
