cadf5f5230f03779da935086f843adc18335d870
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 03:31:14 -0500

    Update op_dbdb_db.jl

--- src/ops/op_dbdb_db.jl
+++ src/ops/op_dbdb_db.jl
@@ -1,76 +1,21 @@
 
 @inline function add_dbdb_db(x::Double{T,E}, y::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis}
-    xhi, xlo = HILO(x)
-    yhi, ylo = HILO(y)
-    xhi, xlo = add_dd_dd(xhi, xlo, yhi, ylo)
-    return Double(E, xhi, xlo)
+    hi, lo = add_dddd_dd(HILO(x), HILO(y))
+    return Double(E, hi, lo)
 end
 
 @inline function sub_dbdb_db(x::Double{T,E}, y::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis}
-    xhi, xlo = HILO(x)
-    yhi, ylo = HILO(y)
-    xhi, xlo = sub_dd_dd(xhi, xlo, yhi, ylo)
-    return Double(E, xhi, xlo)
+    hi, lo = sub_dddd_dd(HILO(x), HILO(y))
+    return Double(E, hi, lo)
 end
-
 
 @inline function mul_dbdb_db(x::Double{T,E}, y::Double{T,E}) where {T<:AbstractFloat, E<:Performance}
-    xhi, xlo = HILO(x)
-    yhi, ylo = HILO(y)
-    xhi, xlo = mul_dd_dd(xhi, xlo, yhi, ylo)
-    return Double(E, xhi, xlo)
+    hi, lo = mul_dddd_dd(HILO(x), HILO(y))
+    return Double(E, hi, lo)
 end
 
-@inline function mul_dbdb_db(x::Double{T,E}, y::Double{T,E}) where {T<:AbstractFloat, E<:Accuracy}
-    xhi, xlo = HILO(x)
-    yhi, ylo = HILO(y)
-    zhi, tlo = mul_2(xhi, yhi)
-    uhi, ulo = mul_2(xhi, ylo)
-    zmd, zlo = mul_2(yhi, xlo)
-    xlo *= ylo
-    uhi, ulo = add_2(uhi, zmd, ulo, zlo)
-    zlo = fma(xlo, ylo, tlo+uhi)
-    # zmd, zlo = add_2(tlo, xlo*ylo)
-    # zmd += uhi
-    # zlo += ulo
-    return Double(E, zhi, zlo)
-end
-
-@inline function dve_dbdb_db(x::Double{T,E}, y::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis}
-    yinv = inv_db_db(y)
-    return mul_dbdb_db(x, yinv)
-end
-
-# Algorithm 6 from Tight and rigourous error bounds. relative error < 3u²
-@inline function add_dd_dd(xhi::T, xlo::T, yhi::T, ylo::T) where T<:IEEEFloat
-    hi, lo = add_(xhi, yhi)
-    thi, tlo = add_(xlo, ylo)
-    c = lo + thi
-    hi, lo = add_hilo_(hi, c)
-    c = tlo + lo
-    hi, lo = add_hilo_(hi, c)
-    return hi, lo
-end
-
-# Algorithm 6 from Tight and rigourous error bounds. relative error < 3u²
-# reworked for subtraction
-@inline function sub_dd_dd(xhi::T, xlo::T, yhi::T, ylo::T) where T<:IEEEFloat
-    hi, lo = sub_(xhi, yhi)
-    thi, tlo = sub_(xlo, ylo)
-    c = lo + thi# Algorithm 9 from Tight and rigourous error bounds. relative error <= 2u²
-    hi, lo = add_hilo_(hi, c)
-    c = tlo + lo
-    hi, lo = add_hilo_(hi, c)
-    return hi, lo
-end
-
-# Algorithm 12 from Tight and rigourous error bounds.  relative error <= 5u²
-@inline function mul_dd_dd(xhi::T, xlo::T, yhi::T, ylo::T) where T<:IEEEFloat
-    hi, lo = mul_(xhi, yhi)
-    t = xlo * ylo
-    t = fma(xhi, ylo, t)
-    t = fma(xlo, yhi, t)
-    t = lo + t
-    hi, lo = add_hilo_(hi, t)
-    return hi, lo
+@inline function dvi_dbdb_db(x::Double{T,E}, y::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis}
+    yinv = inv_dd_dd(HILO(y))
+    hi, lo = mul_dddd_dd(HILO(x), yinv)
+    return Double(E, hi, lo)
 end
