14f5ece9a398127033d943e2c66892dc279ed66a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 03:57:17 -0500

    Update op_dd_dd.jl

--- src/ops/op_dd_dd.jl
+++ src/ops/op_dd_dd.jl
@@ -22,3 +22,23 @@ end
     end
     return hi, lo
 end
+
+@inline function inv_dd_dd(x::Tuple{T,T}) where {T<:AbstractFloat}
+    hi, lo = x
+    a = one(T)
+
+    q0 = 1.0 / hi
+    #r = a - (x * q0)
+    #    a - ((hi+lo)*q0)
+    #    a - hi*q0 - lo*q0
+    r  = fma(-hi,q0,a) - lo*q0
+    #q1 = HI(r) / hi
+    q1 = r/hi
+    #r = r - (x * q1)
+    r  = fma(-hi,q1,a) - lo*q1
+    #q2 = HI(r) / hi
+    q2 = r/hi
+    q1, q2 = add_2(q1, q2)
+
+    return q0, q2
+end
