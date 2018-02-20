d3ac5279c2beb29a7d743b0bf29ce91bc4db6053
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-20 06:33:55 -0500

    Update op_ddsi_dd.jl

--- src/ops/op_ddsi_dd.jl
+++ src/ops/op_ddsi_dd.jl
@@ -90,7 +90,7 @@ function root5_ddsi_dd(a::Tuple{T,T}, n::Signed) where {T<:AbstractFloat}
     #m = n
     xon = dvi_ddfp_dd(x, n_fp)
     xpn = mul_dddd_dd(x, x)
-    xpn = mul_fpfp_dd(x, xpm)
+    xpn = mul_fpfp_dd(x, xpn)
     xpn = mul_fpdd_dd(x, xpn)
     xpn = mul_fpdd_dd(x, xpn)
    #   
@@ -107,7 +107,7 @@ function root5_ddsi_dd(a::Tuple{T,T}, n::Signed) where {T<:AbstractFloat}
    # m = n
     xon = dvi_ddfp_dd(x, n_fp)
     xpn = mul_dddd_dd(x, x)
-    xpn = mul_fpfp_dd(x, xpm)
+    xpn = mul_fpfp_dd(x, xpn)
     xpn = mul_fpdd_dd(x, xpn)
     xpn = mul_fpdd_dd(x, xpn)
 
