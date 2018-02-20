b2c767b1b8832c31cde90a382972e47c6710ff70
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-20 06:15:52 -0500

    Update op_ddsi_dd.jl

--- src/ops/op_ddsi_dd.jl
+++ src/ops/op_ddsi_dd.jl
@@ -1,9 +1,3 @@
-@inline function rootn_dbsi_db(x::Double{T,E}, n::Signed) where {T<:AbstractFloat, E<:Emphasis}
-    hi, lo = rootn_ddsi_dd(HILO(x), n)
-    return Double(E, hi, lo)
-end
-
-
 #=
     compute the nth root of x
     follows mpfun90.f mpnrt
