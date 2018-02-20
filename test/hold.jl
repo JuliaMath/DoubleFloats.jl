897f60c43acd8d3caedcff7ad49992f4253675eb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-19 19:46:29 -0500

    Update hold.jl

--- test/hold.jl
+++ test/hold.jl
@@ -16,20 +16,39 @@ const Float16ulp = inv(ldexp(1.0, precision(Float16)))
 @inline ulp(x::Float32) = ufp(x) * Float32ulp
 @inline ulp(x::Float16) = ufp(x) * Float16ulp
 
+ulp(x::Double{T,E}) where {T,E} = ulp(LO(x))
+ulp(x::Tuple{T,T}) where {T} = ulp(x[2])
+
+
+function relative_ulp(given, found)
+  given_ulp = ulp(given)
+  return HI(abs(given - found)) / given_ulp
+end
+  
 
 function fin(n, val1, val2, f1, f2)
     v1=val1; v2=val2
     for i in 1:n
         v1 = f2(f1(v1,v2),v2)
     end
-    return abs(HI(val1-v1))/ulp(LO(val1))
-end
+    return relative_ulp(val1, v1)
+  end
 
 function invinv(n, val)
     v=val
     for i in 1:n
         v = inv(inv(v))
     end
-    return abs(HI(val-v))/ulp(LO(val))
+    return relative_ulp(val, v)
 end
 
+function(n, aply::Vector{Function}, unaply::Vector{Function}, val1, val2)
+    v1 = val1; v2 = val2; v=val1;
+    for fn in aply
+        v = fn(v,v2)
+    end
+    for fn in unaply
+        v = fn(v, v2)
+     end
+    return relative_ulp(v1, v)
+end
