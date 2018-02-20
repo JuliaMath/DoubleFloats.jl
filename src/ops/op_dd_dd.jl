aed8caf05749dd4c96953f2d7f57e706380c165e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-20 04:37:46 -0500

    Update op_dd_dd.jl

--- src/ops/op_dd_dd.jl
+++ src/ops/op_dd_dd.jl
@@ -64,7 +64,7 @@ function sqrt_dd_dd(x::Tuple{T,T}) where {T<:AbstractFloat}
     r = inv(sqrt(HI(x)))
     h = (HI(x) * half, LO(x) * half)
 
-    r2 = mul_dddd_dd(r, r)
+    r2 = mul_fpfp_dd(r, r)
     hr2 = mul_dddd_dd(h, r2)
     radj = sub_fpdd(half, hr2)
     radj = mul_ddfp_dd(radj, r)
