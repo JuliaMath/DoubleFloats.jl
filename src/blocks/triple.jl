6ecf8173d6c38fd280a3f7903efd9a6c348ecb20
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-21 00:14:46 -0500

    Update triple.jl

--- src/blocks/triple.jl
+++ src/blocks/triple.jl
@@ -72,3 +72,42 @@ second_significand(::Type{Float32})  = nextfloat(one(Float32)/2)
 largest_significand(::Type{Float16}) = prevfloat(one(Float16))
 second_significand(::Type{Float16})  = nextfloat(one(Float16)/2)
 
+# Algorithm 5.1 (Add33)
+# (ahi,amd,alo) + (bhi,bmd,blo) :: (zhi,zmd,zlo)
+
+function add(ahi::T, amd::T, alo::T, bhi::T, bmd::T, blo::T) where {T<:AbstractFloat}
+    zhi, t1 = add_2(ahi, bhi
+    t2, t3 = add_2(amd, bmd)
+    t7, t4 = add_2(t1, t2)
+    t6 = alo + blo
+    t5 = t3 + t4
+    t8 = t5 + t6
+    zmd, zlo = add_2(t7, t8)
+    return zhi, zmd, zlo
+end
+
+@inline add(a::Tuple{T,T,T}, b::Tuple{T,T,T}) where {T<:AbstractFloat}
+    return add(a[1], a[2], a[3], b[1], b[2], b[3])
+end
+
+# Algorithm 5.2 (Add233)
+# (ahi,alo) + (bhi,bmd,blo) :: (zhi,zmd,zlo)
+
+function add(ahi::T, alo::T, bhi::T, bmd::T, blo::T) where {T<:AbstractFloat}
+    zhi, t1 = add_2(ahi, bhi)
+    t2, t3  = add_2(alo, bmd)
+    t4, t5  = add_2(t1, t2)
+    t6 = t3 + blo
+    t7 = t6 + t5
+    zmd, zlo = add_2(t4, t7)
+    return zhi, zmd, zlo
+end
+
+@inline add(a::Tuple{T,T}, b::Tuple{T,T,T}) where {T<:AbstractFloat}
+    return add(a[1], a[2], b[1], b[2], b[3])
+end
+
+# Algorithm 6.1
+# (ahi,amd,alo) * (bhi,bmd,blo) :: (zhi,zmd,zlo)
+
+    
