7c2a483b3c405d46d177f5b42c05a687f356d435
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-24 00:08:48 -0500

    Update predicates.jl

--- src/type/predicates.jl
+++ src/type/predicates.jl
@@ -3,6 +3,54 @@ __Predicates__ are functions that ask "yes or no" questions of their argument[s]
 You can ask of a number "Is this zero?" or "Is this one?" and these predicates
 (`iszero`, `isone`) will work as expected with almost all numerical types.
 The built-in numerical types let you query finiteness (`isfinite`, `isinf`).
+These are the predicates made available for use with DoubleFloats:
+> iszero, isnonzero, isone                 #  value == 0, value != 0, value == 1
+  ispositive, isnegative,                  #  value >  0, value <  0
+  isnonnegative, isnonpositive,            #  value >= 0, value <= 0   
+  isinteger, isfractional                  #  value == round(value) 
+  isfinite, isinf,                         #  abs(value) != Inf, abs(value) == Inf
+  isposinf, isneginf,                      #  value == Inf, value == -Inf
+  isnan                                    #  value is not a number (eg 0/0)
+"""    
+
+iszero(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
+    iszero(HI(x)) && iszero(LO(x))
+    
+isone(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
+    isone(HI(x)) && iszero(LO(x))
+    
+isnan(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
+    isnan(HI(x)) && iszero(LO(x))
+    
+isinf(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
+    isinf(HI(x)) && iszero(LO(x))
+
+isfinite(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
+    isfinite(HI(x)) && isfinite(LO(x))
+    
+isinteger(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
+    isinteger(LO(x)) && isinteger(HI(x))
+    
+isodd(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
+    iszero(LO(x)) ? 
+        isinteger(HI(x)) && isodd(HI(x)) :
+        isinteger(LO(x)) && isodd(LO(x))
+        
+iseven(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
+    iszero(LO(x)) ?
+        isinteger(HI(x)) && iseven(HI(x)) :
+        isinteger(LO(x)) && iseven(LO(x))
+
+issubnormal(x::Double{T,E}) where {T<:Real,E<:Emphasis} =
+    issubnormal(HI(x)) || issubnormal(LO(x))
+
+
+
+"""
+__Predicates__ are functions that ask "yes or no" questions of their argument[s].     
+You can ask of a number "Is this zero?" or "Is this one?" and these predicates
+(`iszero`, `isone`) will work as expected with almost all numerical types.
+The built-in numerical types let you query finiteness (`isfinite`, `isinf`).
 
 These are the predicates made available for use with DoubleFloats:
 
@@ -20,8 +68,7 @@ These are the predicates made available for use with DoubleFloats:
 #=
 
 int arf_is_zero(const arf_t x)
-
-int arf_is_one(const arf_t x)
+int arf_is_one(const arf_t )
 
 int arf_is_pos_inf(const arf_t x)
 
@@ -50,20 +97,3 @@ int arf_is_finite(arf_t x)
     Returns nonzero iff x is a finite floating-point value, i.e. not one of the values
     +∞, −∞, NaN. (Note that this is not equivalent to the negation of arf_is_inf().)
 =#
-
-
-function iszero(x::Double{T,E}) where {T<:FLOPT, E<:Emphasis}
-  
-
-end
-
-  iszero, isnonzero, isone                 #  value == 0, value != 0, value == 1
-  ispositive, isnegative,                  #  value >  0, value <  0
-  isnonnegative, isnonpositive,            #  value >= 0, value <= 0   
-  isinteger, isfractional                  #  value == round(value) 
-  isfinite, isinf,                         #  abs(value) != Inf, abs(value) == Inf
-  isposinf, isneginf,                      #  value == Inf, value == -Inf
-  isnan
-
-end
-  
