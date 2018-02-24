b2aa45ddf32bcc0aee8da0707f0b947ee05e5421
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-23 21:38:16 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,10 @@ makedocs(
     authors = "Jeffrey Sarnoff, Simon Byrne, Sascha Timme, and other contributors",
     pages = [
         "Home" => "index.md",
+        "Ahead" => "lookingahead.md",
+        "Overview" => "appropriate.md",
         "Names" => "naming.md",
+        "Predicates" => "predicates.md",
         "Refs" => "references.md"
     ],
 )
