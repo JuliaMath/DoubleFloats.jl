075535cccb9606e86bdd292c74d7fddcfd27eb57
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-28 22:49:26 -0500

    Update namedconsts.jl

--- src/blocks/namedconsts.jl
+++ src/blocks/namedconsts.jl
@@ -1,36 +1,50 @@
-const k_exp1     = :k_exp1     # 2.71..
-const k_expneg1  = :k_expneg1  # 0.36..
+const k_exp1     = Val{:k_exp1}     # 2.71..
+const k_expneg1  = Val{:k_expneg1}  # 0.36..
 
-const k_log2     = :k_log2  # 0.69..
-const k_log10    = :k_log2  # 2.30..
-const k_log10of2 = :log10of2 # 0.30..
-const k_log2of10 = :log2of10 # 3.32..
+const k_log2     = Val{:k_log2} # 0.69..
+const k_log10    = Val{:k_log2} # 2.30..
+const k_log10of2 = Val{:log10of2} # 0.30..
+const k_log2of10 = Val{:log2of10} # 3.32..
 
-const k_pi    = :k_pi    # 3.14...
-const k_2pi   = :k_2pi   # 6.28..
-const k_3pi   = :k_3pi   # 9.42..
-const k_4pi   = :k_2pi   # 12.4..
-const k_1pi2  = :k_1pi2  # 1.57..
-const k_1pi3  = :k_1pi3  # 1.04..
-const k_1pi4  = :k_1pi4  # 0.78..
-const k_1pi6  = :k_1pi6  # 0.52..
-const k_2pi3  = :k_2pi3  # 2.09..
-const k_5pi6  = :k_5pi3  # 2.61..
+const k_pi    = Val{:k_pi}    # 3.14...
+const k_2pi   = Val{:k_2pi}   # 6.28..
+const k_3pi   = Val{:k_3pi}   # 9.42..
+const k_4pi   = Val{:k_2pi}   # 12.4..
+const k_1pi2  = Val{:k_1pi2}  # 1.57..
+const k_1pi3  = Val{:k_1pi3}  # 1.04..
+const k_1pi4  = Val{:k_1pi4}  # 0.78..
+const k_1pi6  = Val{:k_1pi6}  # 0.52..
+const k_2pi3  = Val{:k_2pi3}  # 2.09..
+const k_5pi6  = Val{:k_5pi3}  # 2.61..
 
-const k_pipow2 = :k_pipow2 # 9.86..
-const k_pipow3 = :k_pipow3 # 31.0..
-const k_pipow4 = :k_pipow4 # 97.4..
-const k_invpipow2 = :k_invpipow2 # 0.10..
-const k_invpipow3 = :k_invpipow3 # 0.03..
-const k_invpipow4 = :k_invpipow4 # 0.01..
+const k_pipow2 = Val{:k_pipow2} # 9.86..
+const k_pipow3 = Val{:k_pipow3} # 31.0..
+const k_pipow4 = Val{:k_pipow4} # 97.4..
+const k_invpipow2 = Val{:k_invpipow2} # 0.10..
+const k_invpipow3 = Val{:k_invpipow3} # 0.03..
+const k_invpipow4 = Val{:k_invpipow4} # 0.01..
 
 # [6//89, 31//89, 58//89, 83//89] .* pi (values for which sin(x), cos(x) are well tabled)
 # [0.06744, 0.3484, 0.6520, 0.9326] (approx)
-const k_6pi89  = :k_6pi89
-const k_31pi89 = :k_31pi89
-const k_58pi89 = :k_58pi89
-const k_83pi89 = :k_83pi89
+const k_6pi89  = Val{:k_6pi89}
+const k_31pi89 = Val{:k_31pi89}
+const k_58pi89 = Val{:k_58pi89}
+const k_83pi89 = Val{:k_83pi89}
 
+const k_sin6pi89  = Val{:k_sin6pi89}
+const k_sin31pi89 = Val{:k_sin31pi89}
+const k_sin58pi89 = Val{:k_sin58pi89}
+const k_sin83pi89 = Val{:k_sin83pi89}
+
+const k_cos6pi89  = Val{:k_cos6pi89}
+const k_cos31pi89 = Val{:k_cos31pi89}
+const k_cos58pi89 = Val{:k_cos58pi89}
+const k_cos83pi89 = Val{:k_cos83pi89}
+
+const k_tan6pi89  = Val{:k_tan6pi89}
+const k_tan31pi89 = Val{:k_tan31pi89}
+const k_tan58pi89 = Val{:k_tan58pi89}
+const k_tan83pi89 = Val{:k_tan83pi89}
 
 
 
