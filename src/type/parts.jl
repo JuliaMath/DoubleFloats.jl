620c7fdfde4777b35240ba0777322d80abaa60e8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-08 22:13:58 -0500

    Update parts.jl

--- src/type/parts.jl
+++ src/type/parts.jl
@@ -1,14 +1,26 @@
-function intpart(x::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis}
-    hi, lo = HILO(x)
+@inline function intpart(x::Tuple{T,T}) where {T<:AbstractFloat}
+    hi, lo = x
     ihi = modf(hi)[2]
     ilo = modf(lo)[2]
+    return ihi, ilo
+end
+
+@inline function fracpart(x::Tuple{T,T}) where {T<:AbstractFloat}
+    hi, lo = x
+    fhi = modf(hi)[1]
+    flo = modf(lo)[1]
+    return fhi, flo
+end
+
+
+function intpart(x::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis}
+    ihi, ilo = intpart(HILO(x))
     return Double(E, (ihi, ilo))
 end
 
+
 function fracpart(x::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis}
-    hi, lo = HILO(x)
-    fhi = modf(hi)[1]
-    flo = modf(lo)[1]
+    fhi, flo = fracpart(HILO(x))
     return Double(E, (fhi, flo))
 end
 
