e1212a7df56fd06ad2be42dcce286214889fb1cd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-09 02:37:45 -0500

    Update promote.jl

--- src/type/promote.jl
+++ src/type/promote.jl
@@ -1,5 +1,8 @@
 import Base: promote_rule, promote_type, convert
 
+promote_type(::Type{Double{T, Accuracy}}, ::Type{Double{T, Performance}}) where {T<:AbstractFloat} =
+    Double{T, Accuracy}
+
 promote_rule(::Type{Double{T, Accuracy}}, ::Type{Double{T, Performance}}) where {T<:AbstractFloat} =
     Double{T, Accuracy}
 
