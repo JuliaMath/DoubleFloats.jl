acafaed5b2c309a0212ec23850206257d5f16ed5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-08 19:41:51 -0500

    cleanup + same

--- src/type/constructors.jl
+++ src/type/constructors.jl
@@ -56,21 +56,29 @@ end
 =#
 Double(hi::T) where {T<:IEEEFloat} = 
     Double(Accuracy, hi, zero(T))
-Double(hi::T) where {T<:Union{Int32, Int16, Int8}} = 
-    Double(Accuracy, Float64(hi), zero(Float64))
-Double(hi::T) where {T<:Union{Int128, Int64}} = 
+Double(hi::T) where {T<:Signed} = 
     Double(Accuracy, Float64(hi), zero(Float64))
 
 Double(hi::T) where {T<:AbstractFloat} = 
-    Double(Accuracy, hi, zero(T))
+    Double(Accuracy, (hi, zero(T)))
 Double(hi::T, lo::T) where {T<:AbstractFloat} =
-    Double{T,Accuracy}(hi, lo)
+    Double(Accuracy, (hi, lo))
 
 FastDouble(hi::T) where {T<:AbstractFloat} = 
     Double(Performance, Float64(hi), zero(T))
 FastDouble(hi::T, lo::T) where {T<:AbstractFloat} =
-    Double{T,Performance}(hi, lo)
-  
+    Double(Performance, (hi, lo))
+
+Double{T}(hi) where {T<:IEEEFloat} = 
+    Double(Accuracy, T(hi), zero(T))
+Double{T}(hi, lo) where {T<:AbstractFloat} =
+    Double(Accuracy,(T(hi+lo), T(hi+lo-T(hi+lo))))
+FastDouble{T}(hi) where {T<:IEEEFloat} = 
+    Double(Performance, T(hi), zero(T))
+FastDouble{T}(hi, lo) where {T<:AbstractFloat} =
+    Double(Performance,(T(hi+lo), T(hi+lo-T(hi+lo))))
+
+
 @inline function bigfloat2hilo(::Type{T}, x::BigFloat) where {T<:AbstractFloat}
      hi = T(x)
      lo = T(x - hi)
@@ -117,110 +125,7 @@ function FastDouble(hi::T, lo::T) where {T<:Real}
 end
 
 
-     
-#=
 
-zero(::Type{Double{T,E}}) where {T<:AbstractFloat, E<:Emphasis} =
-    Double(E, zero(T), zero(T))
-one(::Type{Double{T,E}}) where {T<:AbstractFloat, E<:Emphasis} =
-    Double(E, one(T), zero(T))
-inf(::Type{Double{T,E}}) where {T<:AbstractFloat, E<:Emphasis} =
-    Double(E, T(Inf), zero(T))
-nan(::Type{Double{T,E}}) where {T<:AbstractFloat, E<:Emphasis} =
-    Double(E, T(NaN), zero(T))
-
-
-intmax(::Type{Float64}) = 2^precision(Float64)
-intmax(::Type{Float32}) = 2^precision(Float32)
-intmax(::Type{Float16}) = 2^precision(Float16)
-intmax2(::Type{Float64}) = Int128(2)^((2*precision(Float64))>>1)
-intmax2(::Type{Float32}) = Int128(2)^((2*precision(Float32))>>1)
-intmax2(::Type{Float16}) = Int128(2)^((2*precision(Float16))>>1)
- 
-# sizeof(IEEEInt) <= sizeof(Float64)
-const IEEEInt = Union{Int64, Int32, Int16, Int8}
-const SmallInt = Union{Int32, Int16, Int8}
-
-Double() = Double{Float64, Accuracy}(zero(Float64), zero(Float64))
-FastDouble() = Double{Float64, Performance}(zero(Float64), zero(Float64))
-
-Double{T, E}(x::T) where {T<:IEEEFloat, E<:Emphasis} = Double{T, E}(x, zero(T))
-
-Double(x::T) where {T<:IEEEFloat} = Double{T, Accuracy}(x, zero(T))
-FastDouble(x::T) where {T<:IEEEFloat} = Double{T, Performance}(x, zero(T))
-
-Double(x::T) where {T<:SmallInt} =
-    Double{T, Accuracy}(Float64(x), zero(Float64))
-FastDouble(x::T) where {T<:SmallInt} =
-    Double{T, Performance}(Float64(x), zero(Float64))
-
-function Double(x::T) where {T<:Signed}
-    if abs(x) <= intmax(Float64)
-       hi = Float64(x)
-       lo = zero(Float64)
-    elseif abs(x) <= intmax2(Float64)
-       bf = BigFloat(x)
-       hi = Float64(bf)
-       lo = Float64(bf-hi)
-    else
-       throw(DomainError("$x"))
-    end
-    Double{Float64, Accuracy}(hi, lo)
-end
-
-function FastDouble(x::T) where {T<:Signed}
-    if abs(x) <= intmax(Float64)
-       hi = Float64(x)
-       lo = zero(Float64)
-    elseif abs(x) <= intmax2(Float64)
-       bf = BigFloat(x)
-       hi = Float64(bf)
-       lo = Float64(bf-hi)
-    else
-       throw(DomainError("$x"))
-    end
-    Double{Float64, Performance}(hi, lo)
-end
-
-function Double(x::BigFloat)
-    if abs(x) <= intmax2(Float64)
-        hi = Float64(x)
-        lo = Float64(x-hi)
-    else
-       throw(DomainError("$x"))
-    end
-    Double{Float64, Accuracy}(hi, lo)
-end
-
-function FastDouble(x::BigFloat)
-    if abs(x) <= intmax2(Float64)
-        hi = Float64(x)
-        lo = Float64(x-hi)
-    else
-       throw(DomainError("$x"))
-    end
-    Double{Float64, Performance}(hi, lo)
-end
-
-Double(x::BigInt) = Double(BigFloat(x))
-FastDouble(x::BigInt) = FastDouble(BigFloat(x))
-=#
-
-
-#=
-Double(::Type{Accuracy}) = Double{Float64, Accuracy}(zero(Float64), zero(Float64))
-Double(::Type{Performance}) = Double{Float64, Performance}(zero(Float64), zero(Float64))
-Double(::Type{Accuracy}, hi::T) where {T<:AbstractFloat} = Double{T, Accuracy}(hi, zero(T))
-Double(::Type{Performance}, hi::T) where {T<:AbstractFloat} = Double{T, Performance}(hi, zero(T))
-Double(::Type{Accuracy}, hi::T, lo::T) where {T<:AbstractFloat} = Double{T, Accuracy}(hi, lo)
-Double(::Type{Performance}, hi::T, lo::T) where {T<:AbstractFloat} = Double{T, Performance}(hi, lo)
-
-Double(x::T) where {T<:IEEEFloat} = Double{T, Accuracy}(x, zero(Float64))
-Double(x::T) where {T<:Union{Int16, Int32, Int64}} = Double(Accuracy, Float64(x))
-
-FastDouble(x::T) where {T<:IEEEFloat} = Double{T, Performance}(x, zero(Float64))
-FastDouble(x::T) where {T<:Union{Int16, Int32, Int64}} = Double(Performance, Float64(x))
-=#
 
 @inline Double{T, E}(hilo::Tuple{T, T}) where {T<:AbstractFloat, E<:Emphasis} =
     Double{T, E}(hilo[1], hilo[2])
@@ -245,37 +150,3 @@ function BigFloat(x::Double{T, E}, p=precision(BigFloat)) where {T<:AbstractFloa
     BigFloat(HI(x), p) + BigFloat(LO(x), p)
 end
 
-#=
-function Double{T, E}(x::BigFloat) where {T<:AbstractFloat, E<:Emphasis}
-    hi = T(x)
-    lo = T(x-hi)
-    return Double{T, E}(hi, lo)
-end
-
-function Double{T, E}(x::BigInt) where {T<:AbstractFloat, E<:Emphasis}
-    return Double{T, E}(BigFloat(x))
-end
-
-function Double{T,E}(x::Irrational{S}) where {S, T<:AbstractFloat, E<:Emphasis}
-    y = BigFloat(x)
-    return Double{T,E}(y)
-end
-
-function Double{T,E}(x::Type{Rational{I}}) where {I<:Signed, T<:AbstractFloat, E<:Emphasis}
-    numer = Double{T,E}(numerator(x))
-    denom = Double{T,E}(denominator(x))
-    return numer/denom
-end
-=#
-#=
-FastDouble() = Double{Float64, Performance}(zero(Float64), zero(Float64))
-FastDouble(x::T) where {T<:AbstractFloat} =
-    Double{Float64, Performance}(x, zero(Float64))
-FastDouble(x::T, y::T) where {T<:AbstractFloat} =
-    Double{Float64, Performance}(add_(x, y)...,)
-
-FastDouble(x::T) where {T<:Real} =
-    FastDouble{Float64, Performance}(Float64(x), zero(Float64))
-FastDouble(x::T, y::T) where {T<:Real} =
-    FastDouble{Float64, Performance}(add_(convert(Float64,x), convert(Float64,y))...,)
-=#
