b1942a3b00f2cc3383b9869ebf3d2871a56525e9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-09 05:48:48 -0500

    Update DoubleFloats.jl

--- src/DoubleFloats.jl
+++ src/DoubleFloats.jl
@@ -27,7 +27,7 @@ import Base: (+), (-), (*), (/), (\), (^), abs, inv, sqrt, cbrt,
              sinh, cosh, tanh, csch, sech, coth,
              asinh, acosh, atanh, acsch, asech, acoth
 
-using Base.MathConstants: catalan, γ, eulergamma, golden, pi
+using Base.MathConstants: catalan, ℯ, eulergamma, golden, pi
 
 if VERSION >= v"0.7.0-"
     import Base:isone
