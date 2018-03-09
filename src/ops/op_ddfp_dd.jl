8146817ddaaf5ad4080be4d14bd43bbccfa27f18
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-08 20:02:18 -0500

    Update op_ddfp_dd.jl

--- src/ops/op_ddfp_dd.jl
+++ src/ops/op_ddfp_dd.jl
@@ -17,12 +17,6 @@ end
 end
 
 @inline function dvi_ddfp_dd(x::Tuple{T,T}, y::T) where {T<:AbstractFloat}
-    yinv = inv(y)
-    hi, lo = mul_2(x, (yinv, zero(T)))
-    return hi, lo
-end
-
-@inline function dvi_ddfp_dd(x::Tuple{T,T}, y::T) where {T<:AbstractFloat}
     yinv = inv_fp_dd(y)
     zhi, zlo = mul_dddd_dd(x, yinv)
     return zhi, zlo
