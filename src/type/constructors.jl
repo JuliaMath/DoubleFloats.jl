7c1395e19c95b8ee3c17bc6fb5bce0be4240e4ff
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-21 04:32:16 -0400

    Update constructors.jl

--- src/type/constructors.jl
+++ src/type/constructors.jl
@@ -50,7 +50,6 @@ Double{T, Accuracy}(x::T) where {T<:BigFloat} = Double(Accuracy, big2hilo(Float6
 Double{T, Performance}(x::T) where {T<:BigFloat} = Double(Performance, big2hilo(Float64, x)...,)
 Double(x::BigFloat) where {Float64, Accuracy} = Double(Accuracy, big2hilo(Float64, x)...,)
 
-Double(x::BigFloat) where {Float64, Performance} = Double(Performance, big2hilo(Float64, x)...,)
 
 Double(::Type{Accuracy}, x::B) where {B<:BigNumber} = Double{Float64, Accuracy}(x)
 Double(::Type{Performance}, x::B) where {B<:BigNumber} = Double{Float64, Performance}(x)
