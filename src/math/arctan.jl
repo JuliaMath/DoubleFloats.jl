a8db92e6c0b73e9b0b8b5f8e882ea752a3ae9b7b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-03 21:52:50 -0400

    Update arctan.jl

--- src/math/arctan.jl
+++ src/math/arctan.jl
@@ -53,30 +53,3 @@ function acot(x::Double{T,E}) where {T<:AbstractFloat, E<:Emphasis}
    return z
 end
 
-     
-#=
-http://mathworld.wolfram.com/InverseTangent.html cites Acton 1990
-=#
-#=
-function atan(x::T) where {T<:AbstractFloat}
-    return atan(x)
-end
-
-function atan(x::Double{T,Accuracy}) where {T<:AbstractFloat}
-    b = one(Double{T,Accuracy})
-    a = inv(sqrt(b+square(x)))
-    result = x * a
-    while abs(a) < abs(b)
-        a = (a + b) / 2
-        b = sqrt(a * b)
-    end
-
-    result = result * inv(a)
-    return result
-end
-
-function atan(x::Double{T,Performance}) where {T<:AbstractFloat}
-    result = arctan(Double{T,Accuracy}(x))
-    return Double(Performance, HILO(result))
-end
-=#
