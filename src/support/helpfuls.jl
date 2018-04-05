44641dd93dffd5f054c7eea0b20dc899b25dea1e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-05 13:37:59 -0400

    Update helpfuls.jl

--- src/support/helpfuls.jl
+++ src/support/helpfuls.jl
@@ -2,6 +2,8 @@
     min_to_max() functions from SortingNetworks.jl
 =#
 
+@inline min_to_max(a::T, b::T) where {T} = minmax(a,b)
+
 """
     min_to_max(x𝘪, x𝘫, x𝘬)
 
@@ -12,11 +14,13 @@ sorts three values using minmax thrice
 >       implementation is parallel-ready.
 >   The three stages are independent.
 """
-function min_to_max(a::T, b::T, c::T) where {T}
-
-    b, c = minmax(b, c)
-    a, c = minmax(a, c)
-    a, b = minmax(a, b)
+@inlinw function min_to_max(a::T, b::T, c::T) where {T}
+                        #          parallel A, B, C
+    b, c = minmax(b, c) # stage A
+    
+    a, c = minmax(a, c) # stage B
+    
+    a, b = minmax(a, b) # stage C
 
     return a, b, c
 end
@@ -30,7 +34,7 @@ sorts four values using minmax five times
 >   This implementation is parallel-ready.
 >   The three stages are independent.
 """
-function min_to_max(a::T, b::T, c::T, d::T) where {T}
+@inline function min_to_max(a::T, b::T, c::T, d::T) where {T}
                             #          parallel A, B, C
     a, b = minmax(a, b)     # stage A
     c, d = minmax(c, d)     #   sequential: two minmax
@@ -84,3 +88,8 @@ function min_to_max!(a::T, b::T, c::T, d::T) where {T}
 
     return a, b, c, d
 end
+
+
+
+function max_to_min!
+
