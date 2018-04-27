ffeb1e8c694c5d729dc12b6a2f264b6e43324a57
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-27 18:10:24 -0400

    Update fma.jl

--- src/ops/fma.jl
+++ src/ops/fma.jl
@@ -1,43 +1,13 @@
-function fma(a::T, b::T, c::T) where {F<:IEEEFloat, E<:Emphasis, T<:Double{F,E}}
-    hi, lo = fma_2(a, b, c)
-    return Double(E, hi, lo)
+function muladd(x::Double{T,E}, y::Double{T,E}, z::Double{T,E}) where
+                  {T<:AbstractFloat, E<:Emphasis}
+    xy = mul223(HILO(x), HILO(y))
+    xyz = add322(xy, HILO(z))
+    return Double(E, xyz)
 end
 
-function fma_4(a::T, b::T, c::T) where {F<:IEEEFloat, E<:Emphasis, T<:Double{F,E}}
-    c_hi = HI(c) * onehalf(F)
-    c_lo = LO(c) * onehalf(F)
-
-    fma1 = fma(HI(a), HI(b), c_hi)
-    fma2 = fma(HI(a), LO(b), c_hi)
-    fma3 = fma(LO(a), HI(b), c_lo)
-    fma4 = fma(LO(a), LO(b), c_lo)
-
-    return add_4(fma1, fma2, fma3, fma4)
-end
-
-function fma_(a::T, b::T, c::T) where {F<:IEEEFloat, E<:Emphasis, T<:Double{F,E}}
-    c_hi = HI(c) * half(F)
-    c_lo = LO(c) * half(F)
-
-    fma1 = fma(HI(a), HI(b), c_hi)
-    fma2 = fma(HI(a), LO(b), c_hi)
-    fma3 = fma(LO(a), HI(b), c_lo)
-    fma4 = fma(LO(a), LO(b), c_lo)
-
-    return add_3(fma1, fma2, fma3, fma4)
-end
-
-fma_3(a::T, b::T, c::T) where {F<:IEEEFloat, E<:Emphasis, T<:Double{F,E}} =
-    fma_(a, b, c)
-
-function fma_2(a::T, b::T, c::T) where {F<:IEEEFloat, E<:Emphasis, T<:Double{F,E}}
-    c_hi = halfx(HI(c))
-    c_lo = halfx(LO(c))
-
-    fma1 = fma(HI(a), HI(b), c_hi)
-    fma2 = fma(HI(a), LO(b), c_hi)
-    fma3 = fma(LO(a), HI(b), c_lo)
-    fma4 = fma(LO(a), LO(b), c_lo)
-
-    return add_2(fma1, fma2, fma3, fma4)
+function fma(a::Double{T,E}, b::Double{T,E}, c::Double{T,E}) where
+                  {T<:AbstractFloat, E<:Emphasis}
+     xy = mul223(HILO(x), HILO(y))
+    xyz = add322(xy, HILO(z))
+    return Double(E, xyz)   
 end
