bffe727781bc946af52b5e28f6da2812c74ecd75
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-27 20:15:33 -0400

    Update fma.jl

--- src/ops/fma.jl
+++ src/ops/fma.jl
@@ -5,9 +5,8 @@ function muladd(x::Double{T,E}, y::Double{T,E}, z::Double{T,E}) where
     return Double(E, xyz)
 end
 
-function fma(a::Double{T,E}, b::Double{T,E}, c::Double{T,E}) where
-                  {T<:IEEEFloat, E<:Emphasis}
-     xy = mul223(HILO(x), HILO(y))
-    xyz = add322(xy, HILO(z))
-    return Double(E, xyz)   
+function fma(a::Double{T,E}, b::Double{T,E}, c::Double{T,E}) where {T<:IEEEFloat, E<:Emphasis}
+    ab  = mul223(HILO(a), HILO(b))
+    abc = add322(ab, HILO(c))
+    return Double(E, abc)   
 end
