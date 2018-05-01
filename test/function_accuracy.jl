8fa9ec945c1bf94db41559c95a7d1ff498baa3c8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 06:08:08 -0400

    Update function_accuracy.jl

--- test/function_accuracy.jl
+++ test/function_accuracy.jl
@@ -1,6 +1,6 @@
 
 setprecision(BigFloat, 512)
-srand(1602)
+srand(1602);
 const nrands = 1_000 # tested with nrands = 100_000
 
 rand_accu = rand(Double, nrands);
@@ -54,19 +54,19 @@ end
 @test test_rtol(rand_bigf, rand_fast, tan, 1.0e-21)
 
 @test test_atol(rand_bigf, rand_accu, asin, 1.0e-29)
-@test test_rtol(rand_bigf, rand_accu, asin, 1.0e-31)
+@test test_rtol(rand_bigf, rand_accu, asin, 1.0e-30)
 @test test_atol(rand_bigf, rand_fast, asin, 1.0e-21)
-@test test_rtol(rand_bigf, rand_fast, asin, 1.0e-23)
+@test test_rtol(rand_bigf, rand_fast, asin, 1.0e-22)
 
 @test test_atol(rand_bigf, rand_accu, acos, 1.0e-29)
-@test test_rtol(rand_bigf, rand_accu, acos, 1.0e-31)
+@test test_rtol(rand_bigf, rand_accu, acos, 1.0e-30)
 @test test_atol(rand_bigf, rand_fast, acos, 1.0e-21)
-@test test_rtol(rand_bigf, rand_fast, acos, 1.0e-23)
+@test test_rtol(rand_bigf, rand_fast, acos, 1.0e-22)
 
 @test test_atol(rand_bigf, rand_accu, atan, 1.0e-29)
-@test test_rtol(rand_bigf, rand_accu, atan, 1.0e-31)
+@test test_rtol(rand_bigf, rand_accu, atan, 1.0e-30)
 @test test_atol(rand_bigf, rand_fast, atan, 1.0e-21)
-@test test_rtol(rand_bigf, rand_fast, atan, 1.0e-23)
+@test test_rtol(rand_bigf, rand_fast, atan, 1.0e-22)
 
 
 @test test_atol(rand_bigf, rand_accu, sinh, 1.0e-29)
