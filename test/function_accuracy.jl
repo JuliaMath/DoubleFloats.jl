15bfa727cba89106b58eca8e8d7ef464b126e6e5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 06:13:40 -0400

    Update function_accuracy.jl

--- test/function_accuracy.jl
+++ test/function_accuracy.jl
@@ -69,20 +69,21 @@ end
 @test test_rtol(rand_bigf, rand_fast, atan, 1.0e-21)
 
 
+
 @test test_atol(rand_bigf, rand_accu, sinh, 1.0e-29)
-@test test_rtol(rand_bigf, rand_accu, sinh, 1.0e-31)
-@test test_atol(rand_bigf, rand_fast, sinh, 1.0e-23)
-@test test_rtol(rand_bigf, rand_fast, sinh, 1.0e-25)
+@test test_rtol(rand_bigf, rand_accu, sinh, 1.0e-30)
+@test test_atol(rand_bigf, rand_fast, sinh, 1.0e-21)
+@test test_rtol(rand_bigf, rand_fast, sinh, 1.0e-21)
 
 @test test_atol(rand_bigf, rand_accu, cosh, 1.0e-29)
-@test test_rtol(rand_bigf, rand_accu, cosh, 1.0e-31)
-@test test_atol(rand_bigf, rand_fast, cosh, 1.0e-23)
-@test test_rtol(rand_bigf, rand_fast, cosh, 1.0e-25)
+@test test_rtol(rand_bigf, rand_accu, cosh, 1.0e-30)
+@test test_atol(rand_bigf, rand_fast, cosh, 1.0e-21)
+@test test_rtol(rand_bigf, rand_fast, cosh, 1.0e-21)
 
 @test test_atol(rand_bigf, rand_accu, tanh, 1.0e-29)
-@test test_rtol(rand_bigf, rand_accu, tanh, 1.0e-31)
-@test test_atol(rand_bigf, rand_fast, tanh, 1.0e-23)
-@test test_rtol(rand_bigf, rand_fast, tanh, 1.0e-25)
+@test test_rtol(rand_bigf, rand_accu, tanh, 1.0e-30)
+@test test_atol(rand_bigf, rand_fast, tanh, 1.5e-21)
+@test test_rtol(rand_bigf, rand_fast, tanh, 1.0e-21)
 
 @test test_atol(rand_bigf, rand_accu, asinh, 1.0e-29)
 @test test_rtol(rand_bigf, rand_accu, asinh, 1.0e-31)
