bff0abcbed235e2e71d615a831ebb7c1a46e3cdd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 05:31:21 -0400

    Update function_accuracy.jl

--- test/function_accuracy.jl
+++ test/function_accuracy.jl
@@ -48,7 +48,7 @@ end
 @test test_rtol(rand_bigf, rand_fast, cos, 1.0e-23)
 
 @test test_atol(rand_bigf, rand_accu, tan, 1.0e-29)
-@test test_rtol(rand_bigf, rand_accu, tan, 1.5e-32)
+@test test_rtol(rand_bigf, rand_accu, tan, 1.0e-31)
 @test test_atol(rand_bigf, rand_fast, tan, 1.5e-21)
 @test test_rtol(rand_bigf, rand_fast, tan, 1.0e-23)
 
