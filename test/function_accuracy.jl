dc98d2e3433fd45ba8a86f6dd19e1f15d24a9186
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 03:26:59 -0400

    Update function_accuracy.jl

--- test/function_accuracy.jl
+++ test/function_accuracy.jl
@@ -1,7 +1,7 @@
 
 setprecision(BigFloat, 768)
 srand(1602)
-const nrands = 1_000
+const nrands = 10_000
 rand_accu = rand(Double, nrands)
 rand_fast = FastDouble.(rand_accu)
 rand_bigf = BigFloat.(rand_accu)
@@ -57,3 +57,34 @@ end
 @test test_rtol(rand_bigf, rand_accu, atan, 1.0e-30)
 @test test_atol(rand_bigf, rand_fast, atan, 1.0e-23)
 @test test_rtol(rand_bigf, rand_fast, atan, 1.0e-25)
+
+
+@test test_atol(rand_bigf, rand_accu, sinh, 1.0e-29)
+@test test_rtol(rand_bigf, rand_accu, sinh, 1.0e-30)
+@test test_atol(rand_bigf, rand_fast, sinh, 1.0e-22)
+@test test_rtol(rand_bigf, rand_fast, sinh, 1.0e-25)
+
+@test test_atol(rand_bigf, rand_accu, cosh, 1.0e-29)
+@test test_rtol(rand_bigf, rand_accu, cosh, 1.0e-30)
+@test test_atol(rand_bigf, rand_fast, cosh, 1.0e-22)
+@test test_rtol(rand_bigf, rand_fast, cosh, 1.0e-25)
+
+@test test_atol(rand_bigf, rand_accu, tanh, 1.0e-29)
+@test test_rtol(rand_bigf, rand_accu, tanh, 1.0e-30)
+@test test_atol(rand_bigf, rand_fast, tanh, 1.0e-22)
+@test test_rtol(rand_bigf, rand_fast, tanh, 1.0e-25)
+
+@test test_atol(rand_bigf, rand_accu, asinh, 1.0e-29)
+@test test_rtol(rand_bigf, rand_accu, asinh, 1.0e-30)
+@test test_atol(rand_bigf, rand_fast, asinh, 1.0e-23)
+@test test_rtol(rand_bigf, rand_fast, asinh, 1.0e-25)
+
+@test test_atol(rand_bigf, rand_accu, acosh, 1.0e-29)
+@test test_rtol(rand_bigf, rand_accu, acosh, 1.0e-30)
+@test test_atol(rand_bigf, rand_fast, acosh, 1.0e-23)
+@test test_rtol(rand_bigf, rand_fast, acosh, 1.0e-25)
+
+@test test_atol(rand_bigf, rand_accu, atanh, 1.0e-29)
+@test test_rtol(rand_bigf, rand_accu, atanh, 1.0e-30)
+@test test_atol(rand_bigf, rand_fast, atanh, 1.0e-23)
+@test test_rtol(rand_bigf, rand_fast, atanh, 1.0e-25)
