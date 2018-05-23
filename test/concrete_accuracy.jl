3870a6c690a6c875eb3224029b1c96c0d887bc26
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-23 04:52:57 -0400

    Update concrete_accuracy.jl

--- test/concrete_accuracy.jl
+++ test/concrete_accuracy.jl
@@ -1,25 +1,19 @@
 
-zero_accurate = Double{Float64, Accuracy}(0.0)
-zero_performant = Double{Float64, Performance}(0.0)
-one_accurate = Double{Float64, Accuracy}(1.0)
-one_performant = Double{Float64, Performance}(1.0)
+zero_accurate = DoubleF64(0.0)
+one_accurate  = DoubleF64(1.0)
 
-pi_accurate   = Double{Float64, Accuracy}(pi)
-pi_performant = Double{Float64, Performance}(pi)
-phi_accurate   = Double{Float64, Accuracy}(phi)
-phi_performant = Double{Float64, Performance}(phi)
+pi_accurate   = DoubleF64(pi)
+phi_accurate  = DoubleF64(phi)
 
-@test pi_accurate   == Double(pi)
-@test pi_performant == FastDouble(pi)
+@test pi_accurate   == DoubleFloat{Float64}(pi)
 
 @test abs(inv(inv(pi_accurate)) - pi_accurate) <= eps(LO(pi_accurate))
-@test abs(inv(inv(pi_performant)) - pi_performant) <= eps(LO(pi_performant))
 @test abs(phi_accurate - (phi_accurate*phi_accurate - 1.0)) <= eps(LO(phi_accurate))
 
 a = Base.TwicePrecision(3.0) / Base.TwicePrecision(7.0)
-b = Double{Float64, Accuracy}(3.0) / Double{Float64, Accuracy}(7.0)
+b = DoubleF64(3.0) / DoubleF64(7.0)
 c = Base.TwicePrecision(17.0) / Base.TwicePrecision(5.0)
-d = Double{Float64, Accuracy}(17.0) / Double{Float64, Accuracy}(5.0)
+d = DoubleF64(17.0) / DoubleF64(5.0)
 
 @test a.hi == b.hi
 @test a.lo == b.lo
@@ -33,21 +27,21 @@ d = Double{Float64, Accuracy}(17.0) / Double{Float64, Accuracy}(5.0)
 @test (a/c).lo == (b/d).lo
 
 
-@test zero(Double{Float64}) == Double(0.0, 0.0)
-@test one(Double{Float64}) == Double(1.0, 0.0)
-@test zero(Double(0.0, 0.0)) == Double(0.0, 0.0)
-@test one(Double(0.0, 0.0)) == Double(1.0, 0.0)
+@test zero(Double{Float64}) == DoubleF64(0.0, 0.0)
+@test one(Double{Float64}) == DoubleF64(1.0, 0.0)
+@test zero(DoubleF32(0.0, 0.0)) == DoubleF32(0.0, 0.0)
+@test one(DoubleF32(0.0, 0.0)) == DoubleF32(1.0, 0.0)
 
-@test round(Double(123456.0, 1.0e-17), RoundUp) == Double(123457.0, 0.0)
-@test round(Double(123456.0, -1.0e-17), RoundUp) == Double(123456.0, 0.0)
-@test round(Double(123456.0, -1.0e-17), RoundDown) == Double(123455.0, 0.0)
+@test round(DoubleF64(123456.0, 1.0e-17), RoundUp) == DoubleF64(123457.0, 0.0)
+@test round(DoubleF64(123456.0, -1.0e-17), RoundUp) == DoubleF64(123456.0, 0.0)
+@test round(DoubleF64(123456.0, -1.0e-17), RoundDown) == DoubleF64(123455.0, 0.0)
 
-@test typemax(Double) == Double(typemax(Float64))
-@test realmin(Double) == Double(realmin(Float64))
+@test typemax(DoubleF64) == DoubleF64(typemax(Float64))
+@test realmin(DoubleF32) == DoubleF32(realmin(Float32))
 
-@test isnan(FastDouble(Inf) + FastDouble(-Inf))
-@test isinf(FastDouble(Inf) + FastDouble(Inf))
-@test isnan(Double(NaN) - 1)
+@test isnan( nan(DoubleF64) )
+@test isinf( inf(DoubleF16) )
 
-@test typeof(rand(Double)) == Double{Float64, Accuracy}
-@test typeof(rand(FastDouble)) == Double{Float64, Performance}
+@test typeof(rand(DoubleF64)) == DoubleF64
+@test typeof(rand(DoubleF32)) == DoubleF32
+@test typeof(rand(DoubleF16)) == DoubleF16
