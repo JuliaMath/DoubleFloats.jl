76547ebdcb0a7a613baf0f6f7e45c29ff472c95f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-23 08:10:38 -0400

    Update function_accuracy.jl

--- test/function_accuracy.jl
+++ test/function_accuracy.jl
@@ -11,43 +11,49 @@ rand1_bigf = Base.BigFloat.(rand1_vals);
 rand20_vals = rand_vals .* 20.0;
 rand20_bigf = Base.BigFloat.(rand20_vals);
 
+function excise_nans_infs(v::Vector{T}) where {T<:AbstractFloat}
+    n = length(v)
+    ns = collect(1:n)
+    keep = ns[map(x->(!isnan(x) && !isinf(x)), v)]
+    return v[ keep ]
+end
+
+function excise_nans_infs(v1::Vector{T1}, v2::Vector{T2}) where {T1<:AbstractFloat, T2<:AbstractFloat}
+    n1 = length(v1)
+    n2 = length(v2)
+    n1 == n2 || throw(ErrorException("vectors must be of the same length"))
+    ns1 = collect(1:n)
+    ns2 = copy(ns1)
+    keep1 = ns1[map(x->(!isnan(x) && !isinf(x)), v1)]
+    keep2 = ns2[map(x->(!isnan(x) && !isinf(x)), v2)]
+    keep = unique(sort(keep1..., keep2...,))
+    return v1[ keep ], v2[ keep ]
+end
+
+
 function test_atol(big_rands, dbl_rands, fn, tol)
     T = eltype(dbl_rands)
     fn_big_rands = map(fn, big_rands)
-    nansat1 = collect(1:length(fn_big_rands))[map(isnan, fn_big_rands)]
-    fn_dblbig_rands = map(T, fn_big_rands)
     fn_dbl_rands = map(fn, dbl_rands)
-    nansat2 = collect(1:length(fn_big_rands))[map(isnan, fn_dbl_rands)]
-    nansat = unique(sort([nansat1..., nansat2...,]))
-    nonnans = setdiff(Set(collect(1:length(fn_big_rands))), Set(nansat))
-    nonnansat = collect(nonnans)
-    fn_dblbig_rands = fn_dblbig_rands[nonnansat]
-    fn_dbl_rands = fn_dbl_rands[nonnansat]
+    fn_big_rands, fn_dbl_rands = excise_nans_infs(fn_big_rands, fn_dbl_rands)
+    fn_dblbig_rands = map(T, fn_big_rands)
     fn_diff = map(abs, fn_dblbig_rands .- fn_dbl_rands)
     fn_res  = maximum(fn_diff) <= tol
     return fn_res
 end
 
-
 function test_rtol(big_rands, dbl_rands, fn, tol)
     T = eltype(dbl_rands)
     fn_big_rands = map(fn, big_rands)
-    nansat1 = collect(1:length(fn_big_rands))[map(isnan, fn_big_rands)]
-    fn_dblbig_rands = map(T, fn_big_rands)
     fn_dbl_rands = map(fn, dbl_rands)
-    nansat2 = collect(1:length(fn_big_rands))[map(isnan, fn_dbl_rands)]
-    nansat = unique(sort([nansat1..., nansat2...,]))
-    nonnans = setdiff(Set(collect(1:length(fn_big_rands))), Set(nansat))
-    nonnansat = collect(nonnans)
-    fn_dblbig_rands = fn_dblbig_rands[nonnansat]
-    fn_dbl_rands = fn_dbl_rands[nonnansat]
+    fn_big_rands, fn_dbl_rands = excise_nans_infs(fn_big_rands, fn_dbl_rands)
+    fn_dblbig_rands = map(T, fn_big_rands)
     fn_diff = map(abs, fn_dblbig_rands .- fn_dbl_rands)
     fn_reldiff = fn_diff ./ fn_dblbig_rands;
     fn_res  = maximum(fn_reldiff) <= tol
     return fn_res
 end
 
-  
 @test test_atol(rand_bigf, rand_vals, exp, 1.0e-29)
 @test test_rtol(rand_bigf, rand_vals, exp, 1.5e-31)
 
