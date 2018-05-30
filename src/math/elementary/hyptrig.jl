cfd776ca74c9fad12ae21465c763892e00253e6f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-30 00:02:59 -0400

    Update hyptrig.jl

--- src/math/elementary/hyptrig.jl
+++ src/math/elementary/hyptrig.jl
@@ -3,6 +3,47 @@ function sinh(x::DoubleFloat{T}) where {T<:AbstractFloat}
     iszero(x) && return zero(x)
     !isfinite(x) && return nan(typeof(x))
 
+    y = exp(x) - exp(-x)
+    z = DoubleF64(y.hi*0.5, y.lo*0.5)
+    return z
+end
+
+function cosh(x::DoubleFloat{T}) where {T<:AbstractFloat}
+    x < 0 && return cosh(-x)
+    iszero(x) && return zero(x)
+    !isfinite(x) && return nan(typeof(x))
+
+    y = exp(x) + exp(-x)
+    z = DoubleFloat{T}(y.hi*0.5, y.lo*0.5)
+    return z
+end
+
+function tanh(x::DoubleFloat{T}) where {T<:AbstractFloat}
+    x < 0 && return -tanh(-x)
+    iszero(x) && return zero(x)
+    !isfinite(x) && return nan(typeof(x))
+    epos = exp(x)
+    eneg = exp(-x)
+    z = (epos - eneg) / (epos + eneg)
+    return z
+end
+
+csch(x::DoubleFloat{T}) where {T<:AbstractFloat} =
+    inv(sinh(x))
+
+sech(x::DoubleFloat{T}) where {T<:AbstractFloat} =
+    inv(cosh(x))
+
+coth(x::DoubleFloat{T}) where {T<:AbstractFloat} =
+    inv(tanh(x))
+
+
+#=
+function sinh(x::DoubleFloat{T}) where {T<:AbstractFloat}
+    x < 0 && return -sinh(-x)
+    iszero(x) && return zero(x)
+    !isfinite(x) && return nan(typeof(x))
+
     if abs(x.hi) > 0.05
         ex = exp(x)
         invex = inv(ex)
@@ -66,3 +107,4 @@ sech(x::DoubleFloat{T}) where {T<:AbstractFloat} =
 
 coth(x::DoubleFloat{T}) where {T<:AbstractFloat} =
     inv(tanh(x))
+=#
