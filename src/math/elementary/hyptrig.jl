17cb44b2f8ca011ed3f6bb2a90173857604b1d16
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-29 23:10:34 -0400

    Update hyptrig.jl

--- src/math/elementary/hyptrig.jl
+++ src/math/elementary/hyptrig.jl
@@ -3,9 +3,26 @@ function sinh(x::DoubleFloat{T}) where {T<:AbstractFloat}
     iszero(x) && return zero(x)
     !isfinite(x) && return nan(typeof(x))
 
-    y = exp(x) - exp(-x)
-    z = DoubleFloat{T}(y.hi*0.5, y.lo*0.5)
-    return z
+    if abs(x.hi) > 0.05
+        ea = exp(a)
+        invea = inv(ea)
+        y = ea - invea
+        z = DoubleFloat{T}(y.hi/2, y.lo/2)
+    else
+        s = x
+        t = x
+        r = sqrt(t)
+        m = 1.0
+        thresh = x.hi * eps(eps(x.hi))
+        
+        while (abs(t) > thresh)
+            m += 2.0
+            t *= r
+            t = t / ((m-1) * m)
+            s += t
+        end
+        s
+    end
 end
 
 function cosh(x::DoubleFloat{T}) where {T<:AbstractFloat}
@@ -13,8 +30,11 @@ function cosh(x::DoubleFloat{T}) where {T<:AbstractFloat}
     iszero(x) && return zero(x)
     !isfinite(x) && return nan(typeof(x))
 
-    y = exp(x) + exp(-x)
-    z = DoubleFloat{T}(y.hi*0.5, y.lo*0.5)
+    ex = exp(x)
+    invex = inv(ex)
+    y = ea - invea
+    z = DoubleFloat{T}(y.hi/2, y.lo/2)
+    
     return z
 end
 
@@ -22,9 +42,19 @@ function tanh(x::DoubleFloat{T}) where {T<:AbstractFloat}
     x < 0 && return -tanh(-x)
     iszero(x) && return zero(x)
     !isfinite(x) && return nan(typeof(x))
-    epos = exp(x)
-    eneg = exp(-x)
-    z = (epos - eneg) / (epos + eneg)
+    
+    if (abs(x.hi) > 0.05)
+        ea = exp(a)
+        invea = inv(ea)
+        y1 = ea - invea
+        y2 = ea + invea
+        z = y1 / y2
+    else
+        s = sinh(x)
+        c = sqrt(1.0 + square(s))
+        z = s / c
+    end
+
     return z
 end
 
