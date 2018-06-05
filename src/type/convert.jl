c8de46f42c35f2d4e625d313c107c20be8bb1282
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-05 19:37:35 -0400

    Update convert.jl

--- src/type/convert.jl
+++ src/type/convert.jl
@@ -2,24 +2,24 @@ convert(::Type{DoubleFloat{T}}, x::T) where {T<:IEEEFloat} = DoubleFloat{T}(x)
 convert(::Type{T}, x::DoubleFloat{T}) where {T<:IEEEFloat} = HI(x)
 
 convert(::Type{DoubleFloat{T}}, x::I) where {T<:IEEEFloat, I<:Integer} = DoubleFloat{T}(T(x))
-convert(::Type{I}, x::DoubleFloat{T}) where {T<:IEEEFloat, I<:Integer} = I(round(Base.BigInt,Base.BigFloat(HI(x))+Base.BigFloat(LO(x))))
+convert(::Type{I}, x::DoubleFloat{T}) where {T<:IEEEFloat, I<:Integer} = I(round(BigInt, BigFloat(HI(x))+ BigFloat(LO(x))))
 
-function convert(::Type{Base.BigFloat}, x::DoubleFloat{T}) where {T<:AbstractFloat}
+function convert(::Type{BigFloat}, x::DoubleFloat{T}) where {T<:AbstractFloat}
     res = Base.BigFloat(HI(x)) + Base.BigFloat(LO(x))
     return res
 end
-function convert(::Type{DoubleFloat{T}}, x::Base.BigFloat) where {T<:AbstractFloat}
+function convert(::Type{DoubleFloat{T}}, x::BigFloat) where {T<:AbstractFloat}
     hi = T(x)
     lo = T(x - hi)
     return DoubleFloat(hi, lo)
 end
   
-function convert(::Type{Base.BigInt}, x::DoubleFloat{T}) where {T<:AbstractFloat}
+function convert(::Type{BigInt}, x::DoubleFloat{T}) where {T<:AbstractFloat}
     res = round(Base.BigInt, Base.BigFloat(HI(x)) + Base.BigFloat(LO(x)))
     return res
 end
-function convert(::Type{DoubleFloat{T}}, x::Base.BigInt) where {T<:AbstractFloat}
-    return convert(DoubleFloat{T}, Bse.BigFloat(x))
+function convert(::Type{DoubleFloat{T}}, x::BigInt) where {T<:AbstractFloat}
+    return convert(DoubleFloat{T}, BigFloat(x))
 end
 
 convert(::Type{DoubleFloat{T}}, x::DoubleFloat{DoubleFloat{T}}) where {T<:IEEEFloat} = HI(x)
