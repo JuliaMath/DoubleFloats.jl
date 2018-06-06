0e45ab81c3241252f92e49d30577f535c59c5a38
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-06 05:06:45 -0400

    Update op_ddfp_dd.jl

--- src/math/ops/op_ddfp_dd.jl
+++ src/math/ops/op_ddfp_dd.jl
@@ -12,7 +12,9 @@ end
 
 @inline function mul_ddfp_dd(x::Tuple{T,T}, y::T) where {T<:AbstractFloat}
     hi, lo = x
-    hi, lo = mul_2(hi, y, lo)
+    hihi, hilo = mul_2(y, hi)
+    lohi, lolo = mul_2(y, lo)
+    hi, lo = add_2(hihi, hilo, lohi, lolo)
     return hi, lo
 end
 
