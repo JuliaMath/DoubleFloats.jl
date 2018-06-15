c83ca9a0260bd159ee63f6511338431aad64c48a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-15 06:04:04 -0400

    import number types

--- src/DoubleFloats.jl
+++ src/DoubleFloats.jl
@@ -45,7 +45,9 @@ import Base: hash, promote_type, promote_rule, convert,
              asin, acos, atan, acsc, asec, acot,
              sinh, cosh, tanh, csch, sech, coth,
              asinh, acosh, atanh, acsch, asech, acoth,
-             BigFloat, BigInt
+             BigFloat, BigInt,
+             Int8, Int16, Int32, Int64, Int128,
+             Float64, Float32, Float16
 
 # import LinearAlgebra: normalize
 using Random
