73eb627331307b9a87fba51544841b09ef7ce127
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-05 02:16:40 -0400

    faster implementation

--- src/math/ops/op_dbdb_db.jl
+++ src/math/ops/op_dbdb_db.jl
@@ -1,25 +1,21 @@
 @inline function add_dbdb_db(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    !(isfinite(x) & isfinite(y)) && return add_dbdb_db_nonfinite(x,y)
-    hi, lo = add_dddd_dd(HILO(x), HILO(y))
-    return DoubleFloat(hi, lo)
+    res = DoubleFloat{T}(add_dddd_dd(HILO(x), HILO(y)))
+    isnan(HI(res))  ? add_dbdb_db_nonfinite(x,y) : res
 end
 
 @inline function sub_dbdb_db(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    !(isfinite(x) & isfinite(y)) && return sub_dbdb_db_nonfinite(x,y)
-    hi, lo = sub_dddd_dd(HILO(x), HILO(y))
-    return DoubleFloat(hi, lo)
+    res = DoubleFloat{T}(sub_dddd_dd(HILO(x), HILO(y)))
+    isnan(HI(res))  ? sub_dbdb_db_nonfinite(x,y) : res
 end
 
 @inline function mul_dbdb_db(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    !(isfinite(x) & isfinite(y)) && return mul_dbdb_db_nonfinite(x,y)
-    hi, lo = mul_dddd_dd(HILO(x), HILO(y))
-    return DoubleFloat(hi, lo)
+    res = DoubleFloat{T}(mul_dddd_dd(HILO(x), HILO(y)))
+    isnan(HI(res))  ? mul_dbdb_db_nonfinite(x,y) : res
 end
 
 @inline function dvi_dbdb_db(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    !(isfinite(x) & isfinite(y)) && return dvi_dbdb_db_nonfinite(x,y)
-    hi, lo = dvi_dddd_dd(HILO(x), HILO(y))
-    return DoubleFloat(hi, lo)
+    res = DoubleFloat{T}(dvi_dddd_dd(HILO(x), HILO(y)))
+    isnan(HI(res))  ? dvi_dbdb_db_nonfinite(x,y) : res
 end
 
 
