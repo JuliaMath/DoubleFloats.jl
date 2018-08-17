7deb4cdc4270cf7118201258bbec7b9249f74f0b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-17 15:00:38 -0400

    Update random.jl

--- src/extras/random.jl
+++ src/extras/random.jl
@@ -4,7 +4,7 @@ import Random.rand
 function rand(rng::AbstractRNG, ::Random.SamplerTrivial{Random.CloseOpen01{DoubleFloat{T}}}) where {T<:IEEEFloat}
     hi, lo  = rand(rng, T, 2)
     frlo, xplo  = frexp(lo)
-    xplo = Base.exponent(hi) - xplo - abs(Base.exponent(eps(hi)))
+    xplo = Base.exponent(hi) - min(1, fld(xplo,4)) - abs(Base.exponent(eps(hi)))
     lo = ldexp(frlo, xplo)
     lo = rand(Bool) ? lo : -lo
 
