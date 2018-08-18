c8b9876439d0ad51153d508337ab19e532a7ad2d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-18 05:57:29 -0400

    Update triple_pi.jl

--- src/doubletriple/triple_pi.jl
+++ src/doubletriple/triple_pi.jl
@@ -33,3 +33,39 @@ function halfpi_minus(hi::T, lo::T) where {T<:Float64}
 end
 
 halfpi_minus(x::DoubleFloat{Float64}) = halfpi_minus(HI(x), LO(x))
+
+function minus_twopi(hi::T, lo::T) where {T<:Float64}
+    zhi, t1 = sub_2(6.28318530717958600, hi)
+    t2, t3 = sub_2(2.4492935982947064e-16, lo)
+    t7, t4 = add_2(t1, t2)
+    t5 = t3 + t4
+    zlo = t5 - 5.9895396194366790e-33
+    zlo += t7
+    return DoubleFloat{Float64}(-zhi, -zlo)
+end
+
+minus_twopi(x::DoubleFloat{Float64}) = minus_twopi(HI(x), LO(x))
+
+function minus_pi(hi::T, lo::T) where {T<:Float64}
+    zhi, t1 = sub_2(3.14159265358979300, hi)
+    t2, t3 = sub_2(1.2246467991473532e-16, lo)
+    t7, t4 = add_2(t1, t2)
+    t5 = t3 + t4
+    zlo = t5 - 2.9947698097183397e-33
+    zlo += t7
+    return DoubleFloat{Float64}(-zhi, -zlo)
+end
+
+minus_pi(x::DoubleFloat{Float64}) = minus_pi(HI(x), LO(x))
+
+function minus_halfpi(hi::T, lo::T) where {T<:Float64}
+    zhi, t1 = sub_2(1.57079632679489660, hi)
+    t2, t3 = sub_2(6.1232339957367660e-17, lo)
+    t7, t4 = add_2(t1, t2)
+    t5 = t3 + t4
+    zlo = t5 - 1.4973849048591698e-33
+    zlo += t7
+    return DoubleFloat{Float64}(-zhi, -zlo)
+end
+
+minus_halfpi(x::DoubleFloat{Float64}) = minus_halfpi(HI(x), LO(x))
