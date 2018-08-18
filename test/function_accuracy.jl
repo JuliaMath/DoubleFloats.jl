a8f08b2d9a2fbf4b12738e0618f9f1a7eb9e184b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-17 23:48:27 -0400

    Update function_accuracy.jl

--- test/function_accuracy.jl
+++ test/function_accuracy.jl
@@ -58,8 +58,8 @@ end
     @test test_atol(rand_bigf, rand_vals, cos, 1.0e-31)
     @test test_rtol(rand_bigf, rand_vals, cos, 1.0e-31)
 
-    @test test_atol(rand_bigf, rand_vals, tan, 1.0e-28)
-    @test test_rtol(rand_bigf, rand_vals, tan, 1.0e-28)
+    @test test_atol(rand_bigf, rand_vals, tan, 1.0e-31)
+    @test test_rtol(rand_bigf, rand_vals, tan, 1.0e-30)
 
     @test test_atol(rand_bigf, rand_vals, asin, 1.0e-29)
     @test test_rtol(rand_bigf, rand_vals, asin, 1.0e-29)
@@ -101,8 +101,8 @@ end
     @test test_atol(rand_bigf, rand_vals, cos, 1.0e-31)
     @test test_rtol(rand_bigf, rand_vals, cos, 1.0e-27)
 
-    @test test_atol(rand_bigf, rand_vals, tan, 1.0e-23)
-    @test test_rtol(rand_bigf, rand_vals, tan, 1.0e-27)
+    @test test_atol(rand_bigf, rand_vals, tan, 1.0e-24)
+    @test test_rtol(rand_bigf, rand_vals, tan, 1.0e-28)
 
     @test test_atol(rand_bigf, rand_vals, sinh, 1.0e-30)
     @test test_rtol(rand_bigf, rand_vals, sinh, 1.0e-31)
