54d215e58e62d5e172101433cbe13d53791e23ab
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-22 09:37:35 -0400

    Update specialvalues.jl

--- test/specialvalues.jl
+++ test/specialvalues.jl
@@ -1,7 +1,10 @@
-@testset "maxintfloat $T" for T in (Double16, Double32, Double64)
+@testset "maxintfloat $T" for T in (Double32, Double64)
     @test isinteger(maxintfloat(T))
 
     # Previous integer is representable, next integer is not
     @test maxintfloat(T) == (maxintfloat(T) - one(T)) + one(T)
     @test maxintfloat(T) != (maxintfloat(T) + one(T)) - one(T)
 end
+
+# Double16 is a special case
+@test maxintfloat(Double16) == floatmax(Double16)
