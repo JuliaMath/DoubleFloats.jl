56a2aa25b00d7165c4bf4e4942997fd3fb907e2e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-22 08:10:24 -0400

    Update specialvalues.jl

--- src/type/specialvalues.jl
+++ src/type/specialvalues.jl
@@ -14,7 +14,7 @@ floatmin(::Type{DoubleFloat{T}}) where {T<:AbstractFloat} = DoubleFloat{T}(float
 
 maxintfloat(::Type{DoubleFloat{Float64}}) = DoubleFloat{Float64}(ldexp(0.5, 108), ldexp(0.5, 54))
 maxintfloat(::Type{DoubleFloat{Float32}}) = DoubleFloat{Float32}(ldexp(0.5f0, 50), ldexp(0.5f0, 25))
-maxintfloat(::Type{DoubleFloat{Float16}}) = DoubleFloat{Float16}(ldexp(Float16(0.9995),16), ldexp(Float16(0.5),5)-2)
+maxintfloat(::Type{DoubleFloat{Float16}}) = DoubleFloat{Float16}(65504.0)
 
 isnan(x::DoubleFloat{T}) where {T<:IEEEFloat} = isnan(HI(x))
 isinf(x::DoubleFloat{T}) where {T<:IEEEFloat} = (HI(x) === T(Inf))
