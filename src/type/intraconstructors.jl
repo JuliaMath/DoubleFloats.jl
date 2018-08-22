031ef16707ca476a5bbe7bb789107f8799a91d69
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-22 08:58:05 -0400

    more robust construction from relatively larger values

--- src/type/intraconstructors.jl
+++ src/type/intraconstructors.jl
@@ -2,6 +2,58 @@ Double64(x::Double32) = Double64(FastTwoSum(Float64(HI(x)), Float64(LO(x)))...,)
 Double64(x::Double16) = Double64(FastTwoSum(Float64(HI(x)), Float64(LO(x)))...,)
 Double32(x::Double16) = Double32(FastTwoSum(Float32(HI(x)), Float32(LO(x)))...,)
 
+function Double16(x::Float64)
+    hi = Float16(x)
+    lo = Float16(x - Float64(hi))
+    return DoubleFloat{Float16}(hi,lo)
+end
+
+function Double16(x::Float32)
+    hi = Float16(x)
+    lo = Float16(x - Float32(hi))
+    return DoubleFloat{Float16}(hi,lo)
+end
+
+function Double32(x::Float64)
+    hi = Float32(x)
+    lo = Float32(x - Float64(hi))
+    return DoubleFloat{Float16}(hi,lo)
+end
+
+function Double16(x::T) where {T<:Signed}
+    if x <= maxintfloat(Double16)
+        Double16(Float64(x))
+    else
+        throw(DomainError("$x"))
+    end
+end
+
+function Double32(x::T) where {T<:Signed}
+    if x <= maxintfloat(Double32)
+        Double32(Float64(x))
+    else
+        throw(DomainError("$x"))
+    end
+end
+
+function Double64(x::T) where {T<:Signed}
+    if x <= maxintfloat(Double64)
+        if x <= maxintfloat(Float64)
+             Double64(Float64(x))
+        else
+             Double64(BigFloat(BigInt(x)))
+        end            
+    else
+        throw(DomainError("$x"))
+    end
+end
+
+
+
+#=
+
+e.g. DoubleFloat{Float16}(65519.0) does not work properly in these
+
 function Double32(x::Double64)
     hi64, lo64 = HILO(x)
     hi64a = reinterpret(Float64, (reinterpret(UInt64, hi64) & 0xFFFFFFFF00000000))
@@ -38,3 +90,4 @@ function Double16(x::Double32)
     return Double16(hi16, lo16)
 end
 
+=#
