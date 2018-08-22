0fe351eb5b48d391430f23ee256f3fcd3b6a5aa7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-22 07:21:29 -0400

    Update basic.jl

--- test/basic.jl
+++ test/basic.jl
@@ -1,5 +1,14 @@
-@testset "Basics" begin
+@testset "precision" begin
     @test precision(Double64) == 113
     @test precision(Double32) == 53
     @test precision(Double32) == 24
+end
+
+@testset "maxintfloat" begin
+    @test maxintfloat(Double64) >   maxintfloat(Double64) - 1
+    @test maxintfloat(Double64) == (maxintfloat(Double64) - 1) + 1
+    @test maxintfloat(Double32) >   maxintfloat(Double32) - 1
+    @test maxintfloat(Double32) == (maxintfloat(Double32) - 1) + 1
+    @test maxintfloat(Double16) >   maxintfloat(Double16) - 1
+    @test maxintfloat(Double16) == (maxintfloat(Double16) - 1) + 1
 end
