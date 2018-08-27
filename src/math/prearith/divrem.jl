7f4245c7fdff9c8c48c07aa894627c5b37b6f20c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-27 15:49:45 -0400

    Create divrem.jl

---
+++ src/math/prearith/divrem.jl
@@ -0,0 +1,45 @@
+function div(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
+    (!isfinite(x) || isnan(y)) && return nan(DoubleFloat{T})
+    !isfinite(y) && return zero(DoubleFloat{T})
+    return trunc(x / y)
+end
+
+function fld(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
+    (!isfinite(x) || isnan(y)) && return nan(DoubleFloat{T})
+    !isfinite(y) && return zero(DoubleFloat{T})
+    return floor(x / y)
+end
+
+function cld(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
+    (!isfinite(x) || isnan(y)) && return nan(DoubleFloat{T})
+    !isfinite(y) && return zero(DoubleFloat{T})
+    return ceil(x / y)
+end
+
+function rem(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
+    (!isfinite(x) || isnan(y)) && return nan(DoubleFloat{T})
+    !isfinite(y) && return x
+    return x - div(x,y) * y
+end
+
+function mod(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
+    (!isfinite(x) || isnan(y)) && return nan(DoubleFloat{T})
+    !isfinite(y) && return x
+    return x - fld(x,y) * y
+end
+
+function divrem(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
+    (!isfinite(x) || isnan(y)) && return nan(DoubleFloat{T})
+    !isfinite(y) && return zero(DoubleFloat{T})
+    dv = trunc(x / y)
+    rm = x - dv * y
+    return dv, rm
+end
+
+function fldmod(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
+    (!isfinite(x) || isnan(y)) && return nan(DoubleFloat{T})
+    !isfinite(y) && return zero(DoubleFloat{T})
+    fr = floor(x / y)
+    md = x - fr * y
+    return fr, md
+end
