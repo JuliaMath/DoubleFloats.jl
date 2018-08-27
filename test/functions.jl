6fc19fd13c36b6425829f08cf7ef071ea823346d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-27 13:29:56 -0400

    test nextfloat, prevfloat

--- test/functions.jl
+++ test/functions.jl
@@ -1,3 +1,14 @@
+@testset "Adjacent values" begin
+    @test nextfloat(Double64(10))  > Double64(10)
+    @test nextfloat(Double32(-10)) > Double32(-10)
+    @test prevfloat(Double64(10))  < Double64(10)
+    @test prevfloat(Double32(-10)) < Double32(-10)    
+    @test nextfloat(Double64(10),3)  > nextfloat(Double64(10),2)
+    @test nextfloat(Double32(-10),3) > nextfloat(Double32(-10),2)
+    @test prevfloat(Double64(10),3)  < prevfloat(Double64(10),2)
+    @test prevfloat(Double32(-10),3) < prevfloat(Double32(-10),2)    
+end
+
 @testset "Exponential functions"  begin
     @test Double64(10.0)^0 == Double64(1.0)
     @test Double64(10.0)^1 == Double64(10.0)
