fd54b086c4ae747ef052cf0cbe49f83e188ade9f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-27 22:18:30 -0400

    fixup

--- src/math/ops/op_dbfp_db.jl
+++ src/math/ops/op_dbfp_db.jl
@@ -1,21 +1,21 @@
 @inline function add_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
     res = DoubleFloat{T}(add_ddfp_dd(HILO(x), y))
-    isnan(HI(res))  ? add_dbdb_db_nonfinite(x,y) : res
+    isnan(HI(res))  ? add_dbfp_db_nonfinite(x,y) : res
 end
 
 @inline function sub_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
     res = DoubleFloat{T}(sub_ddfp_dd(HILO(x), y))
-    isnan(HI(res))  ? sub_dbdb_db_nonfinite(x,y) : res
+    isnan(HI(res))  ? sub_dbfp_db_nonfinite(x,y) : res
 end
 
 @inline function mul_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
     res = DoubleFloat{T}(mul_ddfp_dd(HILO(x), y))
-    isnan(HI(res))  ? mul_dbdb_db_nonfinite(x,y) : res
+    isnan(HI(res))  ? mul_dbfp_db_nonfinite(x,y) : res
 end
 
 @inline function dvi_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
     res = DoubleFloat{T}(dvi_ddfp_dd(HILO(x), y))
-    isnan(HI(res))  ? dvi_dbdb_db_nonfinite(x,y) : res
+    isnan(HI(res))  ? dvi_dbfp_db_nonfinite(x,y) : res
 end
 
 
