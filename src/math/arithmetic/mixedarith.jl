30c19ce834ceb6cd998f42d7d2099bedd3fc3606
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-05 18:23:20 -0400

    Update mixedarith.jl

--- src/math/arithmetic/mixedarith.jl
+++ src/math/arithmetic/mixedarith.jl
@@ -1,9 +1,9 @@
 for F in (:(+), :(-), :(*), :(/), :(%), :(^))
   @eval begin
-      function $F(x::DoubleFloat{T1}, y::Union{Signed, Unsigned, AbstractFloat}) where {T1<:IEEEFloat}
+      function $F(x::DoubleFloat{T1}, y::Union{Signed, Unsigned, IEEEFloat}) where {T1<:IEEEFloat}
          return $F(x, DoubleFloat{T1}(y))
       end
-      function $F(x::Union{Signed, Unsigned, AbstractFloat}, y::DoubleFloat{T1}) where {T1<:IEEEFloat}
+      function $F(x::Union{Signed, Unsigned, IEEEFloat}, y::DoubleFloat{T1}) where {T1<:IEEEFloat}
          return $F(DoubleFloat{T1}(x), y)
       end
   end
