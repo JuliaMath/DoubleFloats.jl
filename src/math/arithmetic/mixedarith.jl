a8d2a96f07d6bc1d495b1968cfdfed2cd0843e74
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-05 18:52:09 -0400

    Update mixedarith.jl

--- src/math/arithmetic/mixedarith.jl
+++ src/math/arithmetic/mixedarith.jl
@@ -1,2 +1,2 @@
-Base.sum(x::DoubleFloat{T}, xs::F...) where {T<:IEEEFloat, Union{IEEEFloat, Signed}}  = sum(x, DoubleFloat{T}.(xs...,))
-Base.prod(x::DoubleFloat{T}, xs::F...) where {T<:IEEEFloat, Union{IEEEFloat, Signed}}  = prod(x, DoubleFloat{T}.(xs...,))
+Base.sum(x::DoubleFloat{T}, xs::F...) where {T<:IEEEFloat, F<:Union{IEEEFloat, Signed}}  = sum(x, DoubleFloat{T}.(xs...,))
+Base.prod(x::DoubleFloat{T}, xs::F...) where {T<:IEEEFloat, F<:Union{IEEEFloat, Signed}}  = prod(x, DoubleFloat{T}.(xs...,))
