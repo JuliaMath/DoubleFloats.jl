3cdd9c891290cc75a5b1001db68b3fdef966eaca
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-06 00:10:13 -0400

    Update mixedarith.jl

--- src/math/arithmetic/mixedarith.jl
+++ src/math/arithmetic/mixedarith.jl
@@ -6,6 +6,3 @@ Base.:(+)(x::FloatSigned, y::DoubleFloat{T}, z::FloatSigned) where {T<:IEEEFloat
 Base.:(*)(x::FloatSigned, y::DoubleFloat{T}, z::FloatSigned) where {T<:IEEEFloat}  = *(DoubleFloat{T}(x), y, DoubleFloat{T}(z))
 Base.:(+)(x::FloatSigned, y::FloatSigned, z::DoubleFloat{T}) where {T<:IEEEFloat}  = +(DoubleFloat{T}(x), DoubleFloat{T}(y), z)
 Base.:(*)(x::FloatSigned, y::FloatSigned, z::DoubleFloat{T}) where {T<:IEEEFloat}  = *(DoubleFloat{T}(x), DoubleFloat{T}(y), z)
-
-Base.:(+)(x::DoubleFloat{T}, xs::Vector{F}) where {T<:IEEEFloat, F<:Union{IEEEFloat, Signed}}  = +(x, DoubleFloat{T}.(xs))
-Base.:(*)(x::DoubleFloat{T}, xs::Vector{F}) where {T<:IEEEFloat, F<:Union{IEEEFloat, Signed}}  = *(x, DoubleFloat{T}.(xs))
