8fee8eace64d3e443d48fd532cf069f665e89df8
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2018-09-14 11:20:35 +0200

    More tests

--- test/specialvalues.jl
+++ test/specialvalues.jl
@@ -12,8 +12,19 @@ end
 
 
 @testset "Inf and NaN generation $T" for T in (Double16, Double32, Double64)
+
     @test T(Inf) == inf(T)
     @test T(Inf) == posinf(T)
     @test T(-Inf) == neginf(T)
     @test T(NaN) == nan(T)
+
+    @test T(Inf32) == inf(T)
+    @test T(Inf32) == posinf(T)
+    @test T(-Inf32) == neginf(T)
+    @test T(NaN32) == nan(T)
+
+    @test T(Inf16) == inf(T)
+    @test T(Inf16) == posinf(T)
+    @test T(-Inf16) == neginf(T)
+    @test T(NaN16) == nan(T)
 end
