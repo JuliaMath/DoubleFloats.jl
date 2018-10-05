0850e4b8fbb4b77610600a9fa51560f2f3fa1a4b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-05 07:58:42 -0400

    test predicates

--- test/coverage.jl
+++ test/coverage.jl
@@ -11,3 +11,32 @@ sqrt2 = sqrt(2.0)
 @test Double64(sqrt2) == convert(Double64, sqrt2)
 
 end # convert
+@testset "maxintfloat $T" for T in (Double32, Double64)
+    @test isinteger(maxintfloat(T))
+
+    # Previous integer is representable, next integer is not
+    @test maxintfloat(T) == (maxintfloat(T) - one(T)) + one(T)
+    @test maxintfloat(T) != (maxintfloat(T) + one(T)) - one(T)
+end
+
+@testset "predicates $T" for T in (Double32, Double64)
+  
+    @test iszero(zero(T))
+    @test isone(one(T))
+    @test ispos(one(T))
+    @test isneg(-one(T))
+    @test isnonpos(-one(T))
+    @test isnonpos(zero(T))
+    @test isnonneg(one(T))
+    @test isnonneg(zero(T))
+  
+    @test isinf(T(Inf))
+    @test isposinf(T(Inf))
+    @test isneginf(T(-Inf))
+    @test isnan(T(NaN))
+  
+    @test isnormal(one(T))
+    @test isinteger(one(T))
+    @test isfractional(one(T)/2)
+  
+end # predicates
