94ea7d28e4ff64db5c8c5c11f6fda6668ded0675
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-25 19:28:25 -0400

    call faster constructor form

--- src/math/ops/op_fpdb_db.jl
+++ src/math/ops/op_fpdb_db.jl
@@ -1,20 +1,20 @@
 @inline function add_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(add_fpdd_dd(x, HILO(y)))
+    res = DoubleFloat(add_fpdd_dd(x, HILO(y)))
     isnan(HI(res))  ? add_fpdb_db_nonfinite(x,y) : res
 end
 
 @inline function sub_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(sub_fpdd_dd(x, HILO(y)))
+    res = DoubleFloat(sub_fpdd_dd(x, HILO(y)))
     isnan(HI(res))  ? sub_fpdb_db_nonfinite(x,y) : res
 end
 
 @inline function mul_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(mul_fpdd_dd(x, HILO(y)))
+    res = DoubleFloat(mul_fpdd_dd(x, HILO(y)))
     isnan(HI(res))  ? mul_fpdb_db_nonfinite(x,y) : res
 end
 
 @inline function dvi_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(dvi_fpdd_dd(x, HILO(y)))
+    res = DoubleFloat(dvi_fpdd_dd(x, HILO(y)))
     isnan(HI(res))  ? dvi_fpdb_db_nonfinite(x,y) : res
 end
 
