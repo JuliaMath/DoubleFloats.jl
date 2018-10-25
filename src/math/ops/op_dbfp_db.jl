0a5dab0c0f158c34e59cf270d9dd538ac32dd833
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-25 19:27:25 -0400

    call faster constructor form

--- src/math/ops/op_dbfp_db.jl
+++ src/math/ops/op_dbfp_db.jl
@@ -1,20 +1,20 @@
 @inline function add_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(add_ddfp_dd(HILO(x), y))
+    res = DoubleFloat(add_ddfp_dd(HILO(x), y))
     isnan(HI(res))  ? add_dbfp_db_nonfinite(x,y) : res
 end
 
 @inline function sub_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(sub_ddfp_dd(HILO(x), y))
+    res = DoubleFloat(sub_ddfp_dd(HILO(x), y))
     isnan(HI(res))  ? sub_dbfp_db_nonfinite(x,y) : res
 end
 
 @inline function mul_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(mul_ddfp_dd(HILO(x), y))
+    res = DoubleFloat(mul_ddfp_dd(HILO(x), y))
     isnan(HI(res))  ? mul_dbfp_db_nonfinite(x,y) : res
 end
 
 @inline function dvi_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(dvi_ddfp_dd(HILO(x), y))
+    res = DoubleFloat(dvi_ddfp_dd(HILO(x), y))
     isnan(HI(res))  ? dvi_dbfp_db_nonfinite(x,y) : res
 end
 
