84407072809a9d148b4f140ca6709ee5611ee0cf
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-25 19:26:10 -0400

    call faster constructor form

--- src/math/ops/op_dbdb_db.jl
+++ src/math/ops/op_dbdb_db.jl
@@ -1,20 +1,20 @@
 @inline function add_dbdb_db(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(add_dddd_dd(HILO(x), HILO(y)))
+    res = DoubleFloat(add_dddd_dd(HILO(x), HILO(y)))
     isnan(HI(res))  ? add_dbdb_db_nonfinite(x,y) : res
 end
 
 @inline function sub_dbdb_db(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(sub_dddd_dd(HILO(x), HILO(y)))
+    res = DoubleFloat(sub_dddd_dd(HILO(x), HILO(y)))
     isnan(HI(res))  ? sub_dbdb_db_nonfinite(x,y) : res
 end
 
 @inline function mul_dbdb_db(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(mul_dddd_dd(HILO(x), HILO(y)))
+    res = DoubleFloat(mul_dddd_dd(HILO(x), HILO(y)))
     isnan(HI(res))  ? mul_dbdb_db_nonfinite(x,y) : res
 end
 
 @inline function dvi_dbdb_db(x::DoubleFloat{T}, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(dvi_dddd_dd(HILO(x), HILO(y)))
+    res = DoubleFloat(dvi_dddd_dd(HILO(x), HILO(y)))
     isnan(HI(res))  ? dvi_dbdb_db_nonfinite(x,y) : res
 end
 
