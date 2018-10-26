30beab3a533b793c7f4b844dafe4867f8105c968
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-26 05:29:47 -0400

    restore constructor

--- src/math/ops/op_fp_db.jl
+++ src/math/ops/op_fp_db.jl
@@ -1,51 +1,51 @@
 @inline function abs_fpdb(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(abs_fp_dd(x))
+    return DoubleFloat{T}(abs_fp_dd(x))
 end
 
 @inline function negabs_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(negabs_fp_dd(x))
+    return DoubleFloat{T}(negabs_fp_dd(x))
 end
 
 @inline function neg_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(neg_fp_dd(x))
+    return DoubleFloat{T}(neg_fp_dd(x))
 end
 
 @inline function inv_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(inv_fp_dd(x))
+    return DoubleFloat{T}(inv_fp_dd(x))
 end
 
 
 @inline function powr2_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(powr2_fp_dd(x))
+    return DoubleFloat{T}(powr2_fp_dd(x))
 end
 
 @inline function powr3_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(powr3_fp_dd(x))
+    return DoubleFloat{T}(powr3_fp_dd(x))
 end
 
 @inline function powr4_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(powr4_fp_dd(x))
+    return DoubleFloat{T}(powr4_fp_dd(x))
 end
 
 @inline function powr5_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(powr5_fp_dd(x))
+    return DoubleFloat{T}(powr5_fp_dd(x))
 end
 
 @inline function powr6_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(powr6_fp_dd(x))
+    return DoubleFloat{T}(powr6_fp_dd(x))
 end
 
 
 @inline function root2_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(root2_fp_dd(x))
+    return DoubleFloat{T}(root2_fp_dd(x))
 end
 
 @inline function root3_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(root3_fp_dd(x))
+    return DoubleFloat{T}(root3_fp_dd(x))
 end
 
 @inline function root4_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(root4_fp_dd(x))
+    return DoubleFloat{T}(root4_fp_dd(x))
 end
 
 @inline function root5_fp_db(x::T) where {T<:AbstractFloat}
@@ -53,5 +53,5 @@ end
 end
 
 @inline function root6_fp_db(x::T) where {T<:AbstractFloat}
-    return DoubleFloat(root6_fp_dd(x))
+    return DoubleFloat{T}(root6_fp_dd(x))
 end
