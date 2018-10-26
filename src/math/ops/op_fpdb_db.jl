fb5a213ef5aea21147bbcc00e20c371fdf72d3d1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-26 05:26:41 -0400

    restore constructors

--- src/math/ops/op_fpdb_db.jl
+++ src/math/ops/op_fpdb_db.jl
@@ -1,40 +1,40 @@
 @inline function add_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat(add_fpdd_dd(x, HILO(y)))
-    isnan(HI(res))  ? add_fpdb_db_nonfinite(x,y) : res
+    res = DoubleFloat{T}(add_fpdd_dd(x, HILO(y)))
+    return !isnan(HI(res))  ? res : add_fpdb_db_nonfinite(x,y)
 end
 
 @inline function sub_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat(sub_fpdd_dd(x, HILO(y)))
-    isnan(HI(res))  ? sub_fpdb_db_nonfinite(x,y) : res
+    res = DoubleFloat{T}(sub_fpdd_dd(x, HILO(y)))
+    return !isnan(HI(res))  ? res : sub_fpdb_db_nonfinite(x,y)
 end
 
 @inline function mul_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat(mul_fpdd_dd(x, HILO(y)))
-    isnan(HI(res))  ? mul_fpdb_db_nonfinite(x,y) : res
+    res = DoubleFloat{T}(mul_fpdd_dd(x, HILO(y)))
+    return !isnan(HI(res))  ? res : mul_fpdb_db_nonfinite(x,y)
 end
 
 @inline function dvi_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat(dvi_fpdd_dd(x, HILO(y)))
-    isnan(HI(res))  ? dvi_fpdb_db_nonfinite(x,y) : res
+    res = DoubleFloat{T}(dvi_fpdd_dd(x, HILO(y)))
+    return !isnan(HI(res))  ? res : dvi_fpdb_db_nonfinite(x,y)
 end
 
 
 @inline function add_fpdb_db_nonfinite(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
     z = x + HI(y)
-    return DoubleFloat(z, zero(T))
+    return DoubleFloat{T}(z, zero(T))
 end
 
 @inline function sub_fpdb_db_nonfinite(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
     z = x - HI(y)
-    return DoubleFloat(z, zero(T))
+    return DoubleFloat{T}(z, zero(T))
 end
 
 @inline function mul_fpdb_db_nonfinite(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
     z = x * HI(y)
-    return DoubleFloat(z, zero(T))
+    return DoubleFloat{T}(z, zero(T))
 end
 
 @inline function dvi_fpdb_db_nonfinite(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
     z = div(x, HI(y))
-    return DoubleFloat(z, zero(T))
+    return DoubleFloat{T}(z, zero(T))
 end
