5bb6d2250d8077a1436d23d52aa4bccc3728e092
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-26 05:24:31 -0400

    restore constructors

--- src/math/ops/op_dbfp_db.jl
+++ src/math/ops/op_dbfp_db.jl
@@ -1,40 +1,40 @@
 @inline function add_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    res = DoubleFloat(add_ddfp_dd(HILO(x), y))
-    isnan(HI(res))  ? add_dbfp_db_nonfinite(x,y) : res
+    res = DoubleFloat{T}(add_ddfp_dd(HILO(x), y))
+    !isnan(HI(res)) ? res : add_dbfp_db_nonfinite(x,y)
 end
 
 @inline function sub_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    res = DoubleFloat(sub_ddfp_dd(HILO(x), y))
-    isnan(HI(res))  ? sub_dbfp_db_nonfinite(x,y) : res
+    res = DoubleFloat{T}(sub_ddfp_dd(HILO(x), y))
+    !isnan(HI(res))  ? res : sub_dbfp_db_nonfinite(x,y)
 end
 
 @inline function mul_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    res = DoubleFloat(mul_ddfp_dd(HILO(x), y))
-    isnan(HI(res))  ? mul_dbfp_db_nonfinite(x,y) : res
+    res = DoubleFloat{T}(mul_ddfp_dd(HILO(x), y))
+    !isnan(HI(res))  ? res : mul_dbfp_db_nonfinite(x,y)
 end
 
 @inline function dvi_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    res = DoubleFloat(dvi_ddfp_dd(HILO(x), y))
-    isnan(HI(res))  ? dvi_dbfp_db_nonfinite(x,y) : res
+    res = DoubleFloat{T}(dvi_ddfp_dd(HILO(x), y))
+    !isnan(HI(res)) ? res : dvi_dbfp_db_nonfinite(x,y)
 end
 
 
 @inline function add_dbfp_db_nonfinite(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
     z = HI(x) + y
-    return DoubleFloat(z, zero(T))
+    return DoubleFloat{T}(z, zero(T))
 end
 
 @inline function sub_dbfp_db_nonfinite(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
     z = HI(x) - y
-    return DoubleFloat(z, zero(T))
+    return DoubleFloat{T}(z, zero(T))
 end
 
 @inline function mul_dbfp_db_nonfinite(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
     z = HI(x) * y
-    return DoubleFloat(z, zero(T))
+    return DoubleFloat{T}(z, zero(T))
 end
 
 @inline function dvi_dbfp_db_nonfinite(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
     z = div(HI(x), y)
-    return DoubleFloat(z, zero(T))
+    return DoubleFloat{T}(z, zero(T))
 end
