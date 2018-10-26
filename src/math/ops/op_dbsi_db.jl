5e0a7deb76953d7895cab243250837e872f35c24
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-26 05:28:26 -0400

    Update op_dbsi_db.jl

--- src/math/ops/op_dbsi_db.jl
+++ src/math/ops/op_dbsi_db.jl
@@ -1,3 +1,3 @@
 @inline function rootn_dbsi_db(x::DoubleFloat{T}, n::Signed) where {T<:AbstractFloat}
-    return DoubleFloat(rootn_ddsi_dd(HILO(x), n))
+    return DoubleFloat{T}(rootn_ddsi_dd(HILO(x), n))
 end
