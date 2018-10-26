a96820102697b219858214593a776c3667a043db
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-26 05:28:07 -0400

    restore constructor

--- src/math/ops/op_db_db.jl
+++ src/math/ops/op_db_db.jl
@@ -1,31 +1,31 @@
 @inline function abs_db_db(x::DoubleFloat{T}) where {T<:AbstractFloat}
-    return DoubleFloat(abs_dd_dd(HILO(x)))
+    return DoubleFloat{T}(abs_dd_dd(HILO(x)))
 end
 
 @inline function neg_db_db(x::DoubleFloat{T}) where {T<:AbstractFloat}
-    return DoubleFloat(neg_dd_dd(HILO(x)))
+    return DoubleFloat{T}(neg_dd_dd(HILO(x)))
 end
 
 @inline function negabs_db_db(x::DoubleFloat{T}) where {T<:AbstractFloat}
-    return DoubleFloat(negabs_dd_dd(HILO(x)))
+    return DoubleFloat{T}(negabs_dd_dd(HILO(x)))
 end
 
 @inline function inv_db_db(x::DoubleFloat{T}) where {T<:AbstractFloat}
-    return DoubleFloat(inv_dd_dd(HILO(x)))
+    return DoubleFloat{T}(inv_dd_dd(HILO(x)))
 end
 
 @inline function square_db_db(x::DoubleFloat{T}) where {T<:AbstractFloat}
-    return DoubleFloat(square_dd_dd(HILO(x)))
+    return DoubleFloat{T}(square_dd_dd(HILO(x)))
 end
 
 @inline function cube_db_db(x::DoubleFloat{T}) where {T<:AbstractFloat}
-    return DoubleFloat(cube_dd_dd(HILO(x)))
+    return DoubleFloat{T}(cube_dd_dd(HILO(x)))
 end
 
 @inline function sqrt_db_db(x::DoubleFloat{T}) where {T<:AbstractFloat}
-    return DoubleFloat(sqrt_dd_dd(HILO(x)))
+    return DoubleFloat{T}(sqrt_dd_dd(HILO(x)))
 end
 
 @inline function cbrt_db_db(x::DoubleFloat{T}) where {T<:AbstractFloat}
-    return DoubleFloat(cbrt_dd_dd(HILO(x)))
+    return DoubleFloat{T}(cbrt_dd_dd(HILO(x)))
 end
