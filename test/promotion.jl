2b23d7df8063fbb7b44d8827fa5057f909e241e0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-03 09:06:34 -0400

    test promote_type

--- test/promotion.jl
+++ test/promotion.jl
@@ -2,6 +2,18 @@
 
 f2 = 2.0
   
+@test promote_type(DoubleFloat{Float64}, DoubleFloat{Float32}) == DoubleFloat{Float64}
+@test promote_type(DoubleFloat{Float64}, DoubleFloat{Float16}) == DoubleFloat{Float64}
+@test promote_type(DoubleFloat{Float32}, DoubleFloat{Float16}) == DoubleFloat{Float32}
+
+@test promote_type(DoubleFloat{Float64}, BigFloat) == DoubleFloat{Float64}
+@test promote_type(DoubleFloat{Float32}, BigFloat) == DoubleFloat{Float32}
+@test promote_type(DoubleFloat{Float16}, BigFloat) == DoubleFloat{Float16}
+
+@test promote_type(DoubleFloat{Float64}, BigInt) == DoubleFloat{Float64}
+@test promote_type(DoubleFloat{Float32}, BigInt) == DoubleFloat{Float32}
+@test promote_type(DoubleFloat{Float16}, BigInt) == DoubleFloat{Float16}
+
 @test promote(DoubleFloat{Float64}(f2), DoubleFloat{Float32}(f2))[2] == DoubleFloat{Float64}(f2)
 @test promote(DoubleFloat{Float64}(f2), DoubleFloat{Float16}(f2))[2] == DoubleFloat{Float64}(f2)
 @test promote(DoubleFloat{Float32}(f2), DoubleFloat{Float16}(f2))[2] == DoubleFloat{Float32}(f2)
