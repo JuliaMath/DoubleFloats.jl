1fe7ef212b45753c5a055d8ca0df3891e67b864d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-03 07:02:32 -0400

    Update specialvalues.jl

--- test/specialvalues.jl
+++ test/specialvalues.jl
@@ -1,14 +1,12 @@
-@testset "maxintfloat $T" for T in (Double32, Double64)
+@testset "maxintfloat $T" for T in (Double16, Double32, Double64)
     @test isinteger(maxintfloat(T))
 
     # Previous integer is representable, next integer is not
     @test maxintfloat(T) == (maxintfloat(T) - one(T)) + one(T)
     @test maxintfloat(T) != (maxintfloat(T) + one(T)) - one(T)
+    
+    @test maintfloat(T) < floatmax(T)
 end
-
-# Double16 is a special case
-@test isinteger(maxintfloat(Double16))
-@test maxintfloat(Double16) == floatmax(Double16)
 
 
 @testset "Inf and NaN generation $T" for T in (Double16, Double32, Double64)
