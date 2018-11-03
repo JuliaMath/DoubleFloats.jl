511caafbf6c4eb4aac40b80dd316df9b05828526
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-03 08:51:02 -0400

    remove quad twigs

--- src/type/constructors.jl
+++ src/type/constructors.jl
@@ -10,10 +10,6 @@ Base.Int8(x::DoubleFloat{T}) where {T<:IEEEFloat} = Int8(round(BigInt, BigFloat(
 
 Base.BigFloat(x::DoubleFloat{T}) where {T<:AbstractFloat} = BigFloat(HI(x)) + BigFloat(LO(x))
 
-Base.BigFloat(x::DoubleFloat{DoubleFloat{T}}) where {T<:IEEEFloat} =
-    BigFloat(HI(HI(x))) + BigFloat(LO(HI(x))) + BigFloat(HI(LO(x))) + BigFloat(LO(LO(x)))
-
-
 for (F,D) in ((:Float64, :Double64), (:Float32, :Double32), (:Float16, :Double16))
   @eval begin
     function $D(x::BigFloat)
@@ -25,22 +21,6 @@ for (F,D) in ((:Float64, :Double64), (:Float32, :Double32), (:Float16, :Double16
   end
 end
 
-
-function DoubleFloat{DoubleFloat{T}}(x::BigFloat) where {T<:IEEEFloat}
-    hihi = T(x)
-    hilo = T(x - hihi)
-    lohi = T(x - hihi - hilo)
-    lolo = T(x - hihi - hilo - lohi)
-    hi = DoubleFloat(hihi, hilo)
-    lo = DoubleFloat(lohi, lolo)
-    return DoubleFloat(hi, lo)
-end
-DoubleFloat{DoubleFloat{T}}(x::BigInt) where {T<:IEEEFloat} =
-    DoubleFloat{DoubleFloat{T}}(Base.BigFloat(x))
-
-DoubleFloat{T}(x::DoubleFloat{DoubleFloat{T}}) where {T<:IEEEFloat} = HI(x)
-DoubleFloat{DoubleFloat{T}}(x::DoubleFloat{T}) where {T<:IEEEFloat} =
-    DoubleFloat{DoubleFloat{T}}(x, DoubleFloat(zero(T)))
 
 Double64(x::Irrational{S}) where {S} = Double64(BigFloat(x))
 Double32(x::Irrational{S}) where {S} = Double32(BigFloat(x))
