50499384fcc3ea096db3db3f58c1d9189a7533b5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-03 06:50:35 -0400

    better floatmax and maxintfloat

--- src/type/specialvalues.jl
+++ src/type/specialvalues.jl
@@ -12,9 +12,13 @@ typemin(::Type{DoubleFloat{T}}) where {T<:AbstractFloat} = DoubleFloat{T}(typemi
 floatmax(::Type{DoubleFloat{T}}) where {T<:AbstractFloat} = DoubleFloat{T}(floatmax(T))
 floatmin(::Type{DoubleFloat{T}}) where {T<:AbstractFloat} = DoubleFloat{T}(floatmin(T))
 
+floatmax(::Type{DoubleFloat{Float64}}) = Double32(ldexp(Float64(0.0.9999999999999999), 1024), ldexp(Float64(0.9999999999999999), 970))
+floatmax(::Type{DoubleFloat{Float32}}) = Double32(ldexp(Float32(0.99999994f0), 128), ldexp(Float32(0.99999994f0), 103))
+floatmax(::Type{DoubleFloat{Float16}}) = Double16(ldexp(Float16(0.9995),16), ldexp(Float16(0.9996),4))
+
 maxintfloat(::Type{DoubleFloat{Float64}}) = DoubleFloat{Float64}(ldexp(0.5, 107), ldexp(0.5, 54))
 maxintfloat(::Type{DoubleFloat{Float32}}) = DoubleFloat{Float32}(ldexp(0.5f0, 49), ldexp(0.5f0, 25))
-maxintfloat(::Type{DoubleFloat{Float16}}) = DoubleFloat{Float16}(65504.0) # floatmax(Double16)
+maxintfloat(::Type{DoubleFloat{Float16}}) = DoubleFloat{Float16}(65519.0)
 
 isnan(x::DoubleFloat{T}) where {T<:IEEEFloat} = isnan(HI(x))
 isinf(x::DoubleFloat{T}) where {T<:IEEEFloat} = (HI(x) === T(Inf))
