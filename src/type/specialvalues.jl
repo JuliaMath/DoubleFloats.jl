9043481e8c04d4e02931e067712c8499a6c9f58c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-03 06:57:24 -0400

    same

--- src/type/specialvalues.jl
+++ src/type/specialvalues.jl
@@ -12,7 +12,7 @@ typemin(::Type{DoubleFloat{T}}) where {T<:AbstractFloat} = DoubleFloat{T}(typemi
 floatmax(::Type{DoubleFloat{T}}) where {T<:AbstractFloat} = DoubleFloat{T}(floatmax(T))
 floatmin(::Type{DoubleFloat{T}}) where {T<:AbstractFloat} = DoubleFloat{T}(floatmin(T))
 
-floatmax(::Type{DoubleFloat{Float64}}) = Double32(ldexp(Float64(0.0.9999999999999999), 1024), ldexp(Float64(0.9999999999999999), 970))
+floatmax(::Type{DoubleFloat{Float64}}) = Double32(ldexp(Float64(0.9999999999999999), 1024), ldexp(Float64(0.9999999999999999), 970))
 floatmax(::Type{DoubleFloat{Float32}}) = Double32(ldexp(Float32(0.99999994f0), 128), ldexp(Float32(0.99999994f0), 103))
 floatmax(::Type{DoubleFloat{Float16}}) = Double16(ldexp(Float16(0.9995),16), ldexp(Float16(0.9996),4))
 
