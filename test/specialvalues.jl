dae1e0c76637e13067765e9a7c06dc76246cc26c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-03 19:28:27 -0400

    Update specialvalues.jl

--- test/specialvalues.jl
+++ test/specialvalues.jl
@@ -10,18 +10,18 @@ end
 
 
 @testset "Inf and NaN generation $T" for T in (Double16, Double32, Double64)
-    @test T(Inf) == inf(T)
-    @test T(Inf) == posinf(T)
-    @test T(-Inf) == neginf(T)
-    @test T(NaN) == nan(T)
+    @test isinf(T(Inf)) == isinf(inf(T))
+    @test isinf(T(Inf)) == isposinf(posinf(T))
+    @test isinf(T(-Inf)) == isneginf(neginf(T))
+    @test isnan(T(NaN)) == isnan(nan(T))
 
-    @test T(Inf32) == inf(T)
-    @test T(Inf32) == posinf(T)
-    @test T(-Inf32) == neginf(T)
-    @test T(NaN32) == nan(T)
+    @test isinf(T(Inf32)) == isinf(inf(T))
+    @test isinf(T(Inf32)) == isposinf(posinf(T))
+    @test isinf(T(-Inf32)) == isneginf(neginf(T))
+    @test isnan(T(NaN32)) == isnan(nan(T))
 
-    @test T(Inf16) == inf(T)
-    @test T(Inf16) == posinf(T)
-    @test T(-Inf16) == neginf(T)
-    @test T(NaN16) == nan(T)
+    @test isinf(T(Inf16)) == isinf(inf(T))
+    @test isinf(T(Inf16)) == isposinf(posinf(T))
+    @test isinf(T(-Inf16)) == isneginf(neginf(T))
+    @test isnan(T(NaN16)) == isnan(nan(T))
 end
