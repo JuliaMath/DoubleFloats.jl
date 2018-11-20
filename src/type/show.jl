c4cf90bc5c7aa49d5ed5c5cfc84a630ac46ae3ab
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-19 20:53:16 -0500

    cleanup

--- src/type/show.jl
+++ src/type/show.jl
@@ -1,14 +1,17 @@
 function show(io::IO, x::DoubleFloat{T}) where {T<:IEEEFloat}
-    str = string(x)
-    print(io, str)
+    compact = get(io, :compact, true)
+    if compact
+        print(io, x.hi)
+    else
+        print(io, x)
+    end
+    return nothing
 end
 
+show(x::DoubleFloat{T}) where {T<:IEEEFloat} = show(Base.stdout, x)
+    
 show(io::IO, ::MIME"text/plain", x::DoubleFloat{T}) where {T<:IEEEFloat} = show(io, x)
 
-function show(io::IO, x::DoubleFloat{DoubleFloat{T}}) where {T<:IEEEFloat}
-    str = string(x)
-    print(io, str)
-end
 
 function showtyped(io::IO, x::DoubleFloat{T}) where {T<:IEEEFloat}
     str = stringtyped(x)
@@ -17,17 +20,4 @@ end
 
 showtyped(io::IO, ::MIME"text/plain", x::DoubleFloat{T}) where {T<:IEEEFloat} = showtyped(io, x)
 
-function showtyped(io::IO, x::DoubleFloat{DoubleFloat{T}}) where {T<:IEEEFloat}
-    str = stringtyped(x)
-    print(io, str)
-end
-
-function showtyped(x::DoubleFloat{T}) where {T<:IEEEFloat}
-    str = stringtyped(x)
-    print(str)
-end
-
-function showtyped(x::DoubleFloat{DoubleFloat{T}}) where {T<:IEEEFloat}
-    str = stringtyped(x)
-    print(str)
-end
+showtyped(x::DoubleFloat{T}) where {T<:IEEEFloat} = showtyped(Base.stdout, x)
