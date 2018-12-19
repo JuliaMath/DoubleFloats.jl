8f4971a534f9ce13557eed0502f4e14de61ef5d9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-19 04:31:53 -0500

    Update show.jl

--- src/type/show.jl
+++ src/type/show.jl
@@ -9,9 +9,26 @@ function show(io::IO, x::DoubleFloat{T}) where {T<:IEEEFloat}
     print(io, str)
 end
 
+function show(io::IO, x::Complex{DoubleFloat{T}}) where {T<:IEEEFloat}
+    compact = get(io, :compact, true)
+    if compact
+        str = string(x.re.hi, (signbit(x.im.hi) ? " - " : " + "), abs(x.im.hi), "im")
+    else
+        str = string(x.re, (signbit(x.im.hi) ? " - " : " + "), abs(x.im), "im")
+    end
+    print(io, str)
+end
+
+
 #show(x::DoubleFloat{T}) where {T<:IEEEFloat} = show(Base.stdout, x)
     
 show(io::IO, ::MIME"text/plain", x::DoubleFloat{T}) where {T<:IEEEFloat} = show(io, x)
+
+show(io::IO, ::MIME"text/plain", x::Complex{DoubleFloat{T}}) where {T<:IEEEFloat} = show(io, x)
+
+show(x::DoubleFloat{T}) where {T<:IEEEFloat} = show(Base.stdout, x)
+
+show(x::Complex{DoubleFloat{T}}) where {T<:IEEEFloat} = show(Base.stdout, x)
 
 
 function showtyped(io::IO, x::DoubleFloat{T}) where {T<:IEEEFloat}
@@ -21,4 +38,8 @@ end
 
 showtyped(io::IO, ::MIME"text/plain", x::DoubleFloat{T}) where {T<:IEEEFloat} = showtyped(io, x)
 
+showtyped(io::IO, ::MIME"text/plain", x::Complex{DoubleFloat{T}}) where {T<:IEEEFloat} = showtyped(io, x)
+
 showtyped(x::DoubleFloat{T}) where {T<:IEEEFloat} = showtyped(Base.stdout, x)
+
+showtyped(x::Complex{DoubleFloat{T}}) where {T<:IEEEFloat} = showtyped(Base.stdout, x)
