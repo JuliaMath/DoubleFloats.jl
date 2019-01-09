642b8b0815555a67c5149a2f69916b29a4f60969
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-01-08 22:57:50 -0500

    fixup

--- src/math/ops/op_fpdb_db.jl
+++ src/math/ops/op_fpdb_db.jl
@@ -1,20 +1,20 @@
 @inline function add_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    (isnan(LO(x)) | isnan(LO(y))) && return add_fpdb_db_nonfinite(x,y)
+    (isnan(x) | isnan(LO(y))) && return add_fpdb_db_nonfinite(x,y)
     return DoubleFloat{T}(add_fpdd_dd(HILO(x), y))
 end
 
 @inline function sub_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    (isnan(LO(x)) | isnan(LO(y))) && return sub_fpdb_db_nonfinite(x,y)
+    (isnan(x) | isnan(LO(y))) && return sub_fpdb_db_nonfinite(x,y)
     return DoubleFloat{T}(sub_fpdd_dd(HILO(x), y))
 end
 
 @inline function mul_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    (isnan(LO(x)) | isnan(LO(y))) && return mul_fpdb_db_nonfinite(x,y)
+    (isnan(x) | isnan(LO(y))) && return mul_fpdb_db_nonfinite(x,y)
     return DoubleFloat{T}(mul_fpdd_dd(HILO(x), y))
 end
 
 @inline function dvi_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    (isnan(LO(x)) | isnan(LO(y))) && return dvi_fpdb_db_nonfinite(x,y)
+    (isnan(x) | isnan(LO(y))) && return dvi_fpdb_db_nonfinite(x,y)
     return DoubleFloat{T}(dvi_fpdd_dd(HILO(x), y))
 end
 
