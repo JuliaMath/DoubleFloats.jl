00912df138becac2b5863a431d764bc16f753c98
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-01-08 22:57:06 -0500

    fixup

--- src/math/ops/op_dbfp_db.jl
+++ src/math/ops/op_dbfp_db.jl
@@ -1,20 +1,20 @@
 @inline function add_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    (isnan(LO(x)) | isnan(LO(y))) && return add_dbfp_db_nonfinite(x,y)
+    (isnan(LO(x)) | isnan(y)) && return add_dbfp_db_nonfinite(x,y)
     return DoubleFloat{T}(add_ddfp_dd(HILO(x), y))
 end
 
 @inline function sub_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    (isnan(LO(x)) | isnan(LO(y))) && return sub_dbfp_db_nonfinite(x,y)
+    (isnan(LO(x)) | isnan(y)) && return sub_dbfp_db_nonfinite(x,y)
     return DoubleFloat{T}(sub_ddfp_dd(HILO(x), y))
 end
 
 @inline function mul_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    (isnan(LO(x)) | isnan(LO(y))) && return mul_dbfp_db_nonfinite(x,y)
+    (isnan(LO(x)) | isnan(y)) && return mul_dbfp_db_nonfinite(x,y)
     return DoubleFloat{T}(mul_ddfp_dd(HILO(x), y))
 end
 
 @inline function dvi_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:AbstractFloat}
-    (isnan(LO(x)) | isnan(LO(y))) && return dvi_dbfp_db_nonfinite(x,y)
+    (isnan(LO(x)) | isnan(y)) && return dvi_dbfp_db_nonfinite(x,y)
     return DoubleFloat{T}(dvi_ddfp_dd(HILO(x), y))
 end
 
