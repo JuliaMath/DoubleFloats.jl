aae0b4996d53558ec0652cc071744787f1f5b5b5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-01-08 22:46:25 -0500

    speedup NaN,Inf handling

--- src/math/ops/op_fpdb_db.jl
+++ src/math/ops/op_fpdb_db.jl
@@ -1,23 +1,22 @@
 @inline function add_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(add_fpdd_dd(x, HILO(y)))
-    return !isnan(HI(res))  ? res : add_fpdb_db_nonfinite(x,y)
+    (isnan(LO(x)) | isnan(LO(y))) && return add_fpdb_db_nonfinite(x,y)
+    return DoubleFloat{T}(add_fpdd_dd(HILO(x), y))
 end
 
 @inline function sub_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(sub_fpdd_dd(x, HILO(y)))
-    return !isnan(HI(res))  ? res : sub_fpdb_db_nonfinite(x,y)
+    (isnan(LO(x)) | isnan(LO(y))) && return sub_fpdb_db_nonfinite(x,y)
+    return DoubleFloat{T}(sub_fpdd_dd(HILO(x), y))
 end
 
 @inline function mul_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(mul_fpdd_dd(x, HILO(y)))
-    return !isnan(HI(res))  ? res : mul_fpdb_db_nonfinite(x,y)
+    (isnan(LO(x)) | isnan(LO(y))) && return mul_fpdb_db_nonfinite(x,y)
+    return DoubleFloat{T}(mul_fpdd_dd(HILO(x), y))
 end
 
 @inline function dvi_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
-    res = DoubleFloat{T}(dvi_fpdd_dd(x, HILO(y)))
-    return !isnan(HI(res))  ? res : dvi_fpdb_db_nonfinite(x,y)
+    (isnan(LO(x)) | isnan(LO(y))) && return dvi_fpdb_db_nonfinite(x,y)
+    return DoubleFloat{T}(dvi_fpdd_dd(HILO(x), y))
 end
-
 
 @inline function add_fpdb_db_nonfinite(x::T, y::DoubleFloat{T}) where {T<:AbstractFloat}
     z = x + HI(y)
