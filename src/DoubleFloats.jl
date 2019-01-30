633eb8fd74d855693796a3f8a9df881756360bfb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-01-29 19:59:10 -0500

    fixup

--- src/DoubleFloats.jl
+++ src/DoubleFloats.jl
@@ -21,7 +21,7 @@ export DoubleFloat,
        intpart, fracpart, fmod,
        square, cube,
        add2, sub2, mul2, div2,
-       ⊕, ⊖, ⊗, ⊘
+       ⊕, ⊖, ⊗, ⊘,
        spread, sld, tld,
        signs
 
