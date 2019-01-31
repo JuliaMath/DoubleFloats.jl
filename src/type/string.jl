8f138580fa36db16cdf912ffd9c55b9b8869a6dc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-01-31 08:15:02 -0500

    Update string.jl

--- src/type/string.jl
+++ src/type/string.jl
@@ -11,12 +11,53 @@ function stringtyped(x::Double16)
     return str
 end
 
+"""
+    BigFloatBits(::IEEEType)
+
+significand bit precision for BigFloat when converting from DoubleFloat{IEEEType} into a string
+""" BigFloatBits
+
+"""
+    BigFloatDigits(::IEEEType)
+
+significant digits of BigFloat when converting to DoubleFloat{IEEEType} from a string
+""" BigFloatDigits
+
+# Float63 -> 106, Float32 -> 48, Float16 -> 22
+minprecdblfloat(::Type{T}) where {T<:IEEEFloat} = 
+    (trailing_ones(Base.significand_mask(T))+1)*2
+
+
+#=
+  The effective precision of some DoubleFloat{T} may be larger than this
+     where all bits betweeen the lsb of the HI oart and Lthe msb of the lo part
+     are 0b0 and there are more than 22 zero bits interposed for eg Double64.
+  When a DoubleFloat is the result of an arithmetic operation between DoubleFloats,
+     the effective precision is almost never expected be larger than this.
+=#
+# Float63 -> 128, Float32 -> 58, Float16 -> 27
+lrgprecdblfloat(::Type{T}) where {T<:IEEEFloat} = 
+    Int(ceil(Int, minprecdblfloat(T) * 1.2)))
+
+bigfloatbits(::Type{T}) where {T<:IEEEFloat} =
+    nextpow(2, (trailing_ones(Base.significand_mask(T))+1)*2) - 1
+
+BigFloatBits(::Type{Float64}) = 127 # bigfloatbits(Float64)
+BigFloatBits(::Type{Float32}) =  63 # bigfloatbits(Float32)
+BigFloatBits(::Type{Float32}) =  31 # bigfloatbits(Float16)
+
+BigFloatDigits(::Type{Float64}) = 35 # bigfloatbits(Float64)
+BigFloatDigits(::Type{Float32}) = 16 # bigfloatbits(Float32)
+BigFloatDigits(::Type{Float32}) =  8 # bigfloatbits(Float16)
+
+#= old
 @inline BigFloatBits(::Type{Float64}) = 512
 @inline BigFloatBits(::Type{Float32}) = 256
 @inline BigFloatBits(::Type{Float16}) = 128
 @inline BigFloatDigits(::Type{Float64}) = 35
 @inline BigFloatDigits(::Type{Float32}) = 18
 @inline BigFloatDigits(::Type{Float16}) =  9
+=#
 
 function string(x::DoubleFloat{T}) where {T<:IEEEFloat}
     !isfinite(HI(x)) && return string(HI(x))
