bbc37e4bec9b04cded4571dd077b5418a03b3955
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-05 08:46:04 -0500

    Create support.jl

---
+++ src/math/linearalgebra/support.jl
@@ -0,0 +1,9 @@
+@inline function issquare(m::AbstractArray{T,2}) where {T<:Number}
+    r, c = size(m)
+    return r === c
+end
+
+@inline function issquare(m::Array{DoubleFloat{T},2) where {T<:IEEEFloat}
+    r, c = size(m)
+    return r === c
+end
