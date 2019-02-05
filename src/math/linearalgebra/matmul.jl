9649af9a137b9919b3f11316d0518bb429553e75
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-05 09:00:53 -0500

    Update matmul.jl

--- src/math/linearalgebra/matmul.jl
+++ src/math/linearalgebra/matmul.jl
@@ -1,36 +1,9 @@
-#function (*)(a::Array{DoubleFloat{T},2}, b::Array{DoubleFloat{T},2}) where {T<:AbstractFloat}
-function (mmul)(a::Array{DoubleFloat{T},2}, b::Array{DoubleFloat{T},2}) where {T<:AbstractFloat}
-    if isempty(a) || isempty(b)
-        throw(ErrorException("cannot multiply empty matrices"))
-    end
-    arows, acols = size(a)
-    brows, bcols = size(b)
-    if  acols != brows || arows != bcols
-        throw(ErrorException(string("size(a) != size(b'): ",size(a), " != ", size(b'))))
-    end
-    
-    result_rows, result_cols = arows, bcols  
-    result = reshape(Array{DoubleFloat{T}, 1}(undef, result_rows*result_cols), (result_rows, result_cols))
-
-    for bcol = 1:bcols
-        for arow=1:arows
-            asum = zero(DoubleFloat{T})
-            for acol=1:acols
-                @inbounds asum = muladd(a[arow,acol], b[acol,bcol], asum)
-            end
-            @inbounds result[arow,bcol] = asum
-        end
-    end
-
-    return result
-end
-
 """
-    matmul(a, b)
+    *(a, b)
 
-A more accurate version of `a * b` where a, b are matrices
+A more performant version of `a * b` where a, b are matrices
 """
-function matmul(a::Array{DoubleFloat{T},2}, b::Array{DoubleFloat{T},2}) where {T<:AbstractFloat}
+function Base.:(*)(a::Array{DoubleFloat{T},2}, b::Array{DoubleFloat{T},2}) where {T<:AbstractFloat}
     if isempty(a) || isempty(b)
         throw(ErrorException("cannot multiply empty matrices"))
     end
