1c7fcd120573d77cb438ca244bff26211ca9353b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-05 08:49:13 -0500

    Update support.jl

--- src/math/linearalgebra/support.jl
+++ src/math/linearalgebra/support.jl
@@ -3,7 +3,7 @@
     return r === c
 end
 
-@inline function issquare(m::Array{DoubleFloat{T},2) where {T<:IEEEFloat}
+@inline function issquare(m::Array{DoubleFloat{T},2}) where {T<:IEEEFloat}
     r, c = size(m)
     return r === c
 end
