81a0812f48377f47d86da0b8b04ced82cb4174bf
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-12 01:41:51 -0500

    Update promote.jl

--- test/promote.jl
+++ test/promote.jl
@@ -1,4 +1,4 @@
-@testset "promote" begin
+@testset "promote 1" begin
   @test typeof(promote(one(Double64), one(Double32))[1]) === Double64
   @test typeof(promote(one(Double64), one(Double16))[1]) === Double64
   @test typeof(promote(one(Double32), one(Double16))[1]) === Double32  
@@ -15,3 +15,33 @@
   @test promote_type(Double64, Double16) === Double64
   @test promote_type(Double32, Double16) === Double32
 end  
+
+@testset "promote 2" begin
+
+ f2 = 2.0
+  
+  @test promote_type(DoubleFloat{Float64}, DoubleFloat{Float32}) == DoubleFloat{Float64}
+  @test promote_type(DoubleFloat{Float64}, DoubleFloat{Float16}) == DoubleFloat{Float64}
+  @test promote_type(DoubleFloat{Float32}, DoubleFloat{Float16}) == DoubleFloat{Float32}
+
+  @test promote_type(DoubleFloat{Float64}, BigFloat) == BigFloat
+  @test promote_type(DoubleFloat{Float32}, BigFloat) == BigFloat
+  @test promote_type(DoubleFloat{Float16}, BigFloat) == BigFloat
+
+  @test promote_type(DoubleFloat{Float64}, BigInt) == BigInt
+  @test promote_type(DoubleFloat{Float32}, BigInt) == BigInt
+  @test promote_type(DoubleFloat{Float16}, BigInt) == BigInt
+
+  @test promote(DoubleFloat{Float64}(f2), DoubleFloat{Float32}(f2))[2] == DoubleFloat{Float64}(f2)
+  @test promote(DoubleFloat{Float64}(f2), DoubleFloat{Float16}(f2))[2] == DoubleFloat{Float64}(f2)
+  @test promote(DoubleFloat{Float32}(f2), DoubleFloat{Float16}(f2))[2] == DoubleFloat{Float32}(f2)
+
+  @test promote(DoubleFloat{Float64}(f2), BigFloat(f2))[2] == DoubleFloat{Float64}(f2)
+  @test promote(DoubleFloat{Float32}(f2), BigFloat(f2))[2] == DoubleFloat{Float32}(f2)
+  @test promote(DoubleFloat{Float16}(f2), BigFloat(f2))[2] == DoubleFloat{Float16}(f2)
+
+  @test promote(DoubleFloat{Float64}(f2), BigInt(f2))[2] == DoubleFloat{Float64}(f2)
+  @test promote(DoubleFloat{Float32}(f2), BigInt(f2))[2] == DoubleFloat{Float32}(f2)
+  @test promote(DoubleFloat{Float16}(f2), BigInt(f2))[2] == DoubleFloat{Float16}(f2)
+
+end
