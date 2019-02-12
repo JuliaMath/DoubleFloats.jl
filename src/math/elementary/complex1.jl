bdec63737f0ae0eb8171f863017c55957a311b2d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-12 07:20:06 -0500

    Update complex1.jl

--- src/math/elementary/complex1.jl
+++ src/math/elementary/complex1.jl
@@ -1,8 +1,3 @@
-function inv(x::Complex{DoubleFloat{T}}) where {T<:IEEEFloat}
-    rea, ima = real(x), imag(x)
-    return Complex{DoubleFloat{T}}(inv_real(rea, ima), inv_imag(rea, ima))
-end
-
 function sqrt(x::Complex{DoubleFloat{T}}) where {T<:IEEEFloat}
     rea, ima = real(x), imag(x)
     return Complex{DoubleFloat{T}}(sqrt_real(rea, ima), sqrt_imag(rea, ima))
