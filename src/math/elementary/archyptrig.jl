b2cc18ec02426285f7a39cbf186a2a3b906a7374
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-11 22:35:21 -0500

    fix acsch asech acoth

--- src/math/elementary/archyptrig.jl
+++ src/math/elementary/archyptrig.jl
@@ -24,10 +24,12 @@ function atanh(x::DoubleFloat{T}) where {T<:AbstractFloat}
 end
 
 function acsch(x::DoubleFloat{T}) where {T<:AbstractFloat}
-    iszero(x) && throw(DomainError("$x"))
-    !isfinite(x) && return nan(typeof(x))
-    result = inv(x)
-    result = sinh(result)
+    isinf(x) && return zero(DoubleFloat{T})
+    isnan(x) && return nan(typeof(x))
+    iszero(x) && return signbit(x) ? -DoubleFloat{T}(Inf) : DoubleFloat{T}(Inf)
+    invx = inv(x)
+    invx2 = inv(x*x)
+    result = log(sqrt(1 + invx2) + invx)
     return result
 end
 
@@ -36,9 +38,9 @@ function asech(x::DoubleFloat{T}) where {T<:AbstractFloat}
     isone(x) && return zero(DoubleFloat{T})
     !isfinite(x) && return nan(typeof(x))
     (x < 0.0 || x > 1.0) && throw(DomainError("$x"))
-    result = inv(x)
-    result = cosh(result)
-    return result
+    invx = inv(x)
+    result = fma(sqrt(invx + 1), sqrt(invx - 1), invx)
+    return log(result)
 end
 
 function acoth(x::DoubleFloat{T}) where {T<:AbstractFloat}
@@ -46,7 +48,7 @@ function acoth(x::DoubleFloat{T}) where {T<:AbstractFloat}
     isone(-x) && return -inf(DoubleFloat{T})
     (-1.0 < x < 1.0) && throw(DomainError("$x"))
     !isfinite(x) && return nan(typeof(x))
-    result = inv(x)
-    result = tanh(result)
+    invx = inv(x)
+    result = (log(1+invx) - log(1-invx))/2
     return result
 end
