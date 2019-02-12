e3a89a8ff1beda7ee346a1a6883772114d583b8d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-12 18:39:56 -0500

    Update complex.jl

--- test/complex.jl
+++ test/complex.jl
@@ -1,47 +1,42 @@
 f0 = Complex(0.0, 0.0)
 f1 = Complex(1.0, 0.0)
 f1i = Complex(0.0, 1.0)
+f11 = Complex(1.0, 1.0)
 
 d0 = Complex(Double64(0), Double64(0))
 d1 = Complex(Double64(1), Double64(0))
 d1i = Complex(Double64(0), Double64(1))
+d11 = Complex(Double64(1), Double64(1))
 
 @testset "Complex" begin
-  @test isapprox(sin(f0), Complex{Float64}(sin(d0)))
-  @test isapprox(cos(f0), Complex{Float64}(cos(d0)))
-  @test isapprox(tan(f0), Complex{Float64}(tan(d0)))
-  @test isapprox(csc(f1), Complex{Float64}(csc(d1)))
-  @test isapprox(sec(f0), Complex{Float64}(sec(d0)))
-  @test isapprox(cot(f1), Complex{Float64}(cot(d1)))
-
-  @test isapprox(sinh(f0), Complex{Float64}(sinh(d0)))
-  @test isapprox(cosh(f0), Complex{Float64}(cosh(d0)))
-  @test isapprox(tanh(f0), Complex{Float64}(tanh(d0)))
-  @test isapprox(csch(f1), Complex{Float64}(csch(d1)))
-  @test isapprox(sech(f0), Complex{Float64}(sech(d0)))
-  @test isapprox(coth(f1), Complex{Float64}(coth(d1)))
-
+  @test isapprox(log(f11), Complex{Float64}(log(d11)))
+  @test isapprox(exp(f11), Complex{Float64}(exp(d11)))
   
-  @test isapprox(asin(f0), Complex{Float64}(asin(d0)))
-  @test isapprox(acos(f0), Complex{Float64}(acos(d0)))
-  @test isapprox(atan(f0), Complex{Float64}(atan(d0)))
-#=
-  @test isapprox(acsc(f1), Complex{Float64}(acsc(d1)))
-  @test isapprox(asec(f1), Complex{Float64}(asec(d1)))
-  @test isapprox(acot(f11), Complex{Float64}(acot(d11)))
+  @test isapprox(sin(f11), Complex{Float64}(sin(d11)))
+  @test isapprox(cos(f11), Complex{Float64}(cos(d11)))
+  @test isapprox(tan(f11), Complex{Float64}(tan(d11)))
+  @test isapprox(csc(f11), Complex{Float64}(csc(d11)))
+  @test isapprox(sec(f11), Complex{Float64}(sec(d11)))
+  @test isapprox(cot(f11), Complex{Float64}(cot(d11)))
 
-  @test isapprox(sinh(f0), Complex{Float64}(sinh(d0)))
-  @test isapprox(cosh(f0), Complex{Float64}(cosh(d0)))
-  @test isapprox(tanh(f0), Complex{Float64}(tanh(d0)))
-  @test isapprox(csch(f1), Complex{Float64}(csch(d1)))
-  @test isapprox(sech(f0), Complex{Float64}(sech(d0)))
-  @test isapprox(coth(f1), Complex{Float64}(coth(d1)))
+  @test isapprox(sinh(f11), Complex{Float64}(sinh(d11)))
+  @test isapprox(cosh(f11), Complex{Float64}(cosh(d11)))
+  @test isapprox(tanh(f11), Complex{Float64}(tanh(d11)))
+  @test isapprox(csch(f11), Complex{Float64}(csch(d11)))
+  @test isapprox(sech(f11), Complex{Float64}(sech(d11)))
+  @test isapprox(coth(f11), Complex{Float64}(coth(d11)))
+  
+  @test isapprox(asin(f11), Complex{Float64}(asin(d11)))
+  @test isapprox(acos(f11), Complex{Float64}(acos(d11)))
+#  @test isapprox(atan(f11), Complex{Float64}(atan(d11))) fix
+  @test isapprox(acsc(f11), Complex{Float64}(acsc(d11)))
+  @test isapprox(asec(f11), Complex{Float64}(asec(d11)))
+#  @test isapprox(acot(f11), Complex{Float64}(acot(d11))) fix
 
-  @test isapprox(asinh(f0), Complex{Float64}(asinh(d0)))
-  @test isapprox(acosh(f0), Complex{Float64}(acosh(d0)))
-  @test isapprox(atanh(f0), Complex{Float64}(atanh(d0)))
-  @test isapprox(acsch(f0), Complex{Float64}(acsch(d0)))
-  @test isapprox(asech(f0), Complex{Float64}(asech(d0)))
-  @test isapprox(acoth(f0), Complex{Float64}(acoth(d0)))
-=#
+  @test isapprox(asinh(f11), Complex{Float64}(asinh(d11)))
+  @test isapprox(acosh(f11), Complex{Float64}(acosh(d11)))
+  @test isapprox(atanh(f11), Complex{Float64}(atanh(d11)))
+  @test isapprox(acsch(f11), Complex{Float64}(acsch(d11)))
+  @test isapprox(asech(f11), Complex{Float64}(asech(d11)))
+  @test isapprox(acoth(f11), Complex{Float64}(acoth(d11)))
 end
