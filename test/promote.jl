c16d464e0a3a86afdcb57804af733a7973d19475
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-12 02:19:41 -0500

    Update promote.jl

--- test/promote.jl
+++ test/promote.jl
@@ -45,3 +45,35 @@ end
   @test promote(DoubleFloat{Float16}(f2), BigInt(f2))[2] == DoubleFloat{Float16}(f2)
 
 end
+
+@testset "promote_rule" begin
+
+  @test promote_rule(DoubleFloat{Float64}, DoubleFloat{Float32}) == DoubleFloat{Float64}
+  @test promote_rule(DoubleFloat{Float64}, DoubleFloat{Float16}) == DoubleFloat{Float64}
+  @test promote_rule(DoubleFloat{Float32}, DoubleFloat{Float16}) == DoubleFloat{Float32}
+  @test promote_rule(DoubleFloat{Float64}, Float64) == DoubleFloat{Float64}
+  @test promote_rule(DoubleFloat{Float32}, Float32) == DoubleFloat{Float32}
+  @test promote_rule(DoubleFloat{Float16}, Float16) == DoubleFloat{Float16}
+  @test promote_rule(DoubleFloat{Float64}, Int64) == DoubleFloat{Float64}
+  @test promote_rule(DoubleFloat{Float32}, Int32) == DoubleFloat{Float32}
+  @test promote_rule(DoubleFloat{Float16}, Int16) == DoubleFloat{Float16}
+  @test promote_rule(DoubleFloat{Float64}, BigInt) == BigInt
+  @test promote_rule(DoubleFloat{Float32}, BigInt) == BigInt
+  @test promote_rule(DoubleFloat{Float16}, BigInt) == BigInt
+  @test promote_rule(BigInt, DoubleFloat{Float64}) == BigInt
+  @test promote_rule(BigInt, DoubleFloat{Float32}) == BigInt
+  @test promote_rule(BigInt, DoubleFloat{Float16}) == BigInt
+  @test promote_rule(DoubleFloat{Float64}, BigFloat) == BigFloat
+  @test promote_rule(DoubleFloat{Float32}, BigFloat) == BigFloat
+  @test promote_rule(DoubleFloat{Float16}, BigFloat) == BigFloat
+  @test promote_rule(BigFloat, DoubleFloat{Float64}) == BigFloat
+  @test promote_rule(BigFloat, DoubleFloat{Float32}) == BigFloat
+  @test promote_rule(BigFloat, DoubleFloat{Float16}) == BigFloat
+  @test promote_rule(AbstractIrrational, DoubleFloat{Float16}) == DoubleFloat{Float16}
+  @test promote_rule(AbstractIrrational, DoubleFloat{Float32}) == DoubleFloat{Float32}
+  @test promote_rule(AbstractIrrational, DoubleFloat{Float64}) == DoubleFloat{Float64}
+  @test widen(DoubleFloat{Float16}) == DoubleFloat{Float32}
+  @test widen(DoubleFloat{Float32}) == DoubleFloat{Float64}
+  @test widen(DoubleFloat{Float64}) == BigFloat
+  
+end
