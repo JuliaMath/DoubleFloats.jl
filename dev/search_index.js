025f2009febb55780a746897209709d95cd84c88
autodocs <autodocs>
2019-02-13 08:24:48 +0000

    build based on 51365a2

--- dev/search_index.js
+++ dev/search_index.js
@@ -157,7 +157,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Characteristics",
     "title": "Representation",
     "category": "section",
-    "text": "Double64 is a magnitude ordered, nonoverlapping pair of Float64sDouble32 is a magnitude ordered, nonoverlapping pair of Float32sDouble16 is a magnitude ordered, nonoverlapping pair of Float16sarithmetic (+, -, *, /) is exact\nelementary functions are quite accurate\noften better than C \"double-double\" libraries"
+    "text": "Double64 is a magnitude ordered, nonoverlapping pair of Float64Double32 is a magnitude ordered, nonoverlapping pair of Float32Double16 is a magnitude ordered, nonoverlapping pair of Float16ComplexD64 is a (real, imag) pair of Double64ComplexD32 is a (real, imag) pair of Double32ComplexD16 is a (real, imag) pair of Double16arithmetic (+, -, *, /) is exact\nelementary functions are quite accurate\noften better than C \"double-double\" libraries"
 },
 
 {
