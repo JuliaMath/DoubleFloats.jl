0cb924caa1bec0512c582d1623b6c8c7aa583845
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-19 08:09:56 -0500

    three_hilo_sum four_hilo_sum

--- src/math/errorfree.jl
+++ src/math/errorfree.jl
@@ -98,6 +98,7 @@ end
         
 """
     two_hilo_sum(a, b)
+
 *unchecked* requirement `|a| ≥ |b|`
 Computes `s = fl(a+b)` and `e = err(a+b)`.
 """
@@ -106,6 +107,38 @@ Computes `s = fl(a+b)` and `e = err(a+b)`.
     e = b - (s - a)
     return s, e
 end
+
+
+"""
+    three_hilo_sum(a, b, c)
+    
+*unchecked* requirement `|a| ≥ |b| ≥ |c|`
+Computes `s = fl(a+b+c)` and `e1 = err(a+b+c), e2 = err(e1)`.
+"""
+function three_hilo_sum(a::T,b::T,c::T) where {T<:FloatWithFMA}
+    s, t = two_hilo_sum(b, c)
+    x, u = two_hilo_sum(a, s)
+    y, z = two_hilo_sum(u, t)
+    x, y = two_hilo_sum(x, y)
+    return x, y, z
+end
+
+"""
+    four_hilo_sum(a, b, c, d)
+    
+*unchecked* requirement `|a| ≥ |b| ≥ |c| ≥ |d|`
+Computes `s = fl(a+b+c+d)` and `e1 = err(a+b+c+d), e2 = err(e1), e3 = err(e2)`.
+"""
+function four_hilo_sum(a::T,b::T,c::T,d::T) where {T<:FloatWithFMA}
+    t0, t1 = two_hilo_sum(a ,  b)
+    t0, t2 = two_hilo_sum(t0,  c)
+    hi, t3 = two_hilo_sum(t0,  d)
+    t0, t1 = two_hilo_sum(t1, t2)
+    hm, t2 = two_hilo_sum(t0, t3) # here, t0 >= t3
+    ml, lo = two_hilo_sum(t1, t2)
+    return hi, hm, ml, lo
+end
+
 
 @inline function two_prod(a::T, b::T) where {T<:FloatWithFMA}
     s = a * b
