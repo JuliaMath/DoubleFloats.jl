22cd5c795a0efc16b1c5d34bf5616c3b8202e7aa
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-22 22:33:45 -0500

    Update construct.jl

--- test/construct.jl
+++ test/construct.jl
@@ -9,6 +9,16 @@
    @test Double16(one(Double32)) === one(Double16)
    @test Double16(one(Double16)) === one(Double16)
    
+   @test Double64(one(Float64)) === one(Double64)
+   @test Double64(one(Float32)) === one(Double64)
+   @test Double64(one(Float16)) === one(Double64)
+   @test Double32(one(Float64)) === one(Double32)
+   @test Double32(one(Float32)) === one(Double32)
+   @test Double32(one(Float16)) === one(Double32)
+   @test Double16(one(Float64)) === one(Double16)
+   @test Double16(one(Float32)) === one(Double16)
+   @test Double16(one(Float16)) === one(Double16)
+
    @test DoubleFloat(one(Double64)) === one(Double64)
    @test DoubleFloat(one(Double32)) === one(Double64)
    @test DoubleFloat(one(Double16)) === one(Double64)
@@ -20,4 +30,7 @@
    @test ComplexD64(1, 1) === ComplexD64(1.0, 1.0)
    @test ComplexD32(1, 1) === ComplexD32(1.0f0, 1.0f0)
    @test ComplexD16(1, 1) === ComplexD16(Float16(1.0), Float16(1.0))  
+   @test ComplexD64(1//1, 1//1) === ComplexD64(1.0, 1.0)
+   @test ComplexD32(1//1, 1//1) === ComplexD32(1.0f0, 1.0f0)
+   @test ComplexD16(1//1, 1//1) === ComplexD16(Float16(1.0), Float16(1.0))  
 end
