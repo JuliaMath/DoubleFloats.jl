f7309cb17adbac59ad1358d19dc9e8d0a59cffc1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-05 22:35:50 -0500

    deprecate warning for d64, d32, d16

--- src/type/parse.jl
+++ src/type/parse.jl
@@ -35,19 +35,31 @@ macro df16_str(val::AbstractString)
 end
 
 # DEPRECATED
+_used_d64_ = false
+_used_d32_ = false
+_used_d16_ = false
 
 macro d64_str(val::AbstractString)
-  @warn("@d64 is deprecated, use @df64")
+  if !_used_d64_
+    _used_d64_ = true
+    @warn("d64\"x\" is deprecated, use df64\"x\"")
+  end 
   :(Double64($val))
 end
 
 macro d32_str(val::AbstractString)
-  @warn("@d32 is deprecated, use @df32")
+  if !_used_d32_
+    _used_d32_ = true
+    @warn("d32\"x\" is deprecated, use df32\"x\"")
+  end
   :(Double32($val))
 end
 
 macro d16_str(val::AbstractString)
-  @warn("@d16 is deprecated, use @df16")
+  if !_used_d16_
+    _used_d16_ = true
+    @warn("d16\"x\" is deprecated, use df16\"x\"")
+  end
   :(Double16($val))
 end
 
