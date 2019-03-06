c17fbb640ec5c1638981055a7537981348285778
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-06 13:11:01 -0500

    Create agm.jl

---
+++ src/math/special/agm.jl
@@ -0,0 +1,14 @@
+function agm(x::T, y::T) where {T<:Real}
+    a, b = x, y
+    epsilon = eps(min(a,b))
+
+    signbit(a) != signbit(b) && throw(DomainError("$a * $b must be nonnegative"))
+
+    while abs(a-b) > epsilon
+     c = (a + b) * 0.5
+     b = sqrt(a * b)
+     a = c
+    end
+
+    return a
+end
