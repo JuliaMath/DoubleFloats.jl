28bf2cef5f7880188d0497850d20ed0784e37c68
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-06 06:46:35 -0500

    Update fma.jl

--- src/math/arithmetic/fma.jl
+++ src/math/arithmetic/fma.jl
@@ -73,53 +73,21 @@ end
    return fma(x[1], x[2], y[1], y[2], z[1], z[2])
 end
 
-
-function muladd(xhi::T, xlo::T, yhi::T, ylo::T, zhi::T, zlo::T) where {T<:IEEEFloat}
-   chi, c1 = two_prod(xhi, yhi)
-   t0 = xlo * ylo
-   c2 = fma(xlo, yhi, t0)
-   c3 = c1 + c2
-   dhi, dlo = two_hilo_sum(chi, c3)
-
-   shi, slo = two_sum(zhi, dhi)
-   thi, tlo = two_sum(zlo, dlo)
-   c = slo + thi
-   vhi, vlo = two_hilo_sum(shi, c)
-   w = tlo + vlo
-   hi, lo = two_hilo_sum(vhi, w)
-   return DoubleFloat{T}(hi, lo)
-end
+@inline muladd(xhi::T, xlo::T, yhi::T, ylo::T, zhi::T, zlo::T) where {T<:IEEEFloat} = fma(xhi, xlo, yhi, ylo, zhi, zlo)
 
 @inline function muladd(x::DoubleFloat{T}, y::DoubleFloat{T}, z::DoubleFloat{T}) where {T<:IEEEFloat}
    return muladd(x.hi, x.lo, y.hi, y.lo, z.hi, z.lo)
 end
 
-function muladd(x::T, y::T, zhi::T, zlo::T) where {T<:IEEEFloat}
-   chi, clo = two_prod(x, y)
-   shi, slo = two_sum(zhi, chi)
-   thi, tlo = two_sum(zlo, clo)
-   c = slo + thi
-   vhi, vlo = two_hilo_sum(shi, c)
-   w = tlo + vlo
-   hi, lo = two_hilo_sum(vhi, w)
-   return DoubleFloat{T}(hi, lo)
+@inline function muladd(x::T, y::T, zhi::T, zlo::T) where {T<:IEEEFloat}
+   return fma(x,y,zhi,zlo)
 end
-
 @inline function muladd(x::T, y::T, z::DoubleFloat{T}) where {T<:IEEEFloat}
-   return muladd(x, y, z.hi, z.lo)
+   return fma(x, y, z.hi, z.lo)
 end
 
-function muladd(xhi::T, xlo::T, yhi::T, ylo::T, z::T) where {T<:IEEEFloat}
-   chi, c1 = two_prod(xhi, yhi)
-   t0 = xlo * ylo
-   c2 = fma(xlo, yhi, t0)
-   c3 = c1 + c2
-   dhi, dlo = two_hilo_sum(chi, c3)
-
-   shi, slo = two_sum(z, dhi)
-   c = slo + dlo
-   hi, lo = two_hilo_sum(shi, c)
-   return DoubleFloat{T}(hi, lo)
+@inline function muladd(xhi::T, xlo::T, yhi::T, ylo::T, z::T) where {T<:IEEEFloat}
+   return fma(xhi, xlo, yhi, ylo, z)
 end
 
 @inline function muladd(x::DoubleFloat{T}, y::DoubleFloat{T}, z::T) where {T<:IEEEFloat}
