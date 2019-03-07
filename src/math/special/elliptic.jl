0f5b2cbfb4abad20a4d92b41a5e5c24093572ea5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-06 22:12:45 -0500

    Create elliptic.jl

---
+++ src/math/special/elliptic.jl
@@ -0,0 +1,13 @@
+function elliptick(x::DoubleFloat{T}) where {T<:IEEEFloat}
+    !(0.0 <= x <= 1.0) && throw(DomainError("$x not in 0..1"))
+    x === one(T) && return DoubleFloat{T}(Inf)
+
+    df_one    = one(DoubleFloat{T})
+    df_halfpi = DoubleFloat{T}(pi)/2
+        
+    k = sqrt(abs(x - df_one))
+    k = agm1(k)
+    k = df_halfpi / k
+
+    return k
+end
