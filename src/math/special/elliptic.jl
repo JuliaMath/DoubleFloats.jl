2adaf7e4af0ffe6d3f121e19559c63ed7563c7ec
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-09 08:32:47 -0500

    Update elliptic.jl

--- src/math/special/elliptic.jl
+++ src/math/special/elliptic.jl
@@ -1,5 +1,5 @@
 function elliptick(x::DoubleFloat{T}) where {T<:IEEEFloat}
-    !(0.0 <= x <= 1.0) && throw(DomainError("$x not in 0..1"))
+    !(-0.0 <= x <= 1.0) && throw(DomainError("$x not in 0..1"))
     x === one(T) && return DoubleFloat{T}(Inf)
 
     df_one    = one(DoubleFloat{T})
