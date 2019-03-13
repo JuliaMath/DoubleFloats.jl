eae783237dcc973c943039d9be15c9490368ea47
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-13 16:19:32 -0400

    fix Inf handling

--- src/math/elementary/hyptrig.jl
+++ src/math/elementary/hyptrig.jl
@@ -1,4 +1,5 @@
 function sinh(x::DoubleFloat{T}) where {T<:IEEEFloat}
+    isinf(x) && return x
     x < 0 && return -sinh(-x)
     iszero(x) && return zero(x)
     !isfinite(x) && return nan(typeof(x))
@@ -9,6 +10,7 @@ function sinh(x::DoubleFloat{T}) where {T<:IEEEFloat}
 end
 
 function cosh(x::DoubleFloat{T}) where {T<:IEEEFloat}
+    isinf(x) && return abs(x)
     x < 0 && return cosh(-x)
     iszero(x) && return one(x)
     !isfinite(x) && return nan(typeof(x))
@@ -19,6 +21,7 @@ function cosh(x::DoubleFloat{T}) where {T<:IEEEFloat}
 end
 
 function tanh(x::DoubleFloat{T}) where {T<:IEEEFloat}
+    isinf(x) && return copysign(one(DoubleFloat{T}), x)
     return sinh(x) / cosh(x)
 end
 #=
@@ -33,11 +36,17 @@ function tanh(x::DoubleFloat{T}) where {T<:IEEEFloat}
 end
 =#
 
-csch(x::DoubleFloat{T}) where {T<:IEEEFloat} =
-    inv(sinh(x))
+function csch(x::DoubleFloat{T}) where {T<:IEEEFloat}
+    isinf(x) && return zero(DoubleFloat{T})    
+    return inv(sinh(x))
+end
 
-sech(x::DoubleFloat{T}) where {T<:IEEEFloat} =
-    inv(cosh(x))
+function sech(x::DoubleFloat{T}) where {T<:IEEEFloat}
+    isinf(x) && return zero(DoubleFloat{T})    
+    return inv(cosh(x))
+end
 
-coth(x::DoubleFloat{T}) where {T<:IEEEFloat} =
-    inv(tanh(x))
+function coth(x::DoubleFloat{T}) where {T<:IEEEFloat}
+    isinf(x) && return copysign(one(DoubleFloat{T}), x)
+    return cosh(x) / sinh(x)
+end
