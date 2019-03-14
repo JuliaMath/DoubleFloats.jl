5ad81e5e1ae113be16be5c10f9b5a5fbde1dfef5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-14 07:58:54 -0400

    Update prearith.jl

--- test/prearith.jl
+++ test/prearith.jl
@@ -1,12 +1,14 @@
 @testset "prearith $T" for T in (Double16, Double32, Double64)
   
+    val = T(17)/3
+  
     @test -T(1) == T(-1)
     @test abs(T(-1)) == T(1)
     @test abs(T(1)) == T(1)
     @test flipsign(T(1), -1) == T(-1)
     @test flipsign(T(1), 1) == T(1)
     
-    @test ldexp(frexp(T(pi))...,) == T(pi)
-    @test ldexp(significand(T(pi)), exponent(T(pi))) == T(pi)
-    @test signs(T(pi)) = sign(HI(T(pi))), sign(LO(T(pi)))
+    @test ldexp(frexp(val)...,) == T(pi)
+    @test ldexp(significand(val), exponent(val)) == val
+    @test signs(val) = sign(HI(val)), sign(LO(val))
 end
