e8552f03e1113ac429accc0b61c6b0f5e5f9246c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-14 11:50:56 -0400

    add randpm function (-1.0 .. 1.0)

--- src/extras/random.jl
+++ src/extras/random.jl
@@ -20,3 +20,31 @@ function rand(rng::AbstractRNG, ::Random.SamplerTrivial{Random.CloseOpen01{Compl
     return Complex{DoubleFloat{T}}(re, im)
 end
 
+function randpm(::Type{DoubleFloat{T}}) where {T}
+    r = rand(DoubleFloat{T})
+    r = rand(Bool) ? r : -r
+    return r
+end
+
+function randpm(::Type{Complex{DoubleFloat{T}}}) where {T}
+    re = randpm(DoubleFloat{T})
+    im = randpm(DoubleFloat{T})
+    return Complex{DoubleFloat{T}}(re, im)
+end
+
+function randpm(::Type{DoubleFloat{T}}, n::Int) where {T}
+    rs = rand(DoubleFloat{T}, n)
+    sgns = ones(n) .- (2 .* rand(Bool, n))
+    return rs .* sgns
+end
+
+function randpm(::Type{Complex{DoubleFloat{T}}}, n::Int) where {T}
+    res = rand(DoubleFloat{T}, n)
+    ims = rand(DoubleFloat{T}, n)
+    o = ones(n)
+    rsgns = o .- (2 .* rand(Bool,n))
+    isgns = o .- (2 .* rand(Bool,n))
+    res = res .* rsgns
+    ims = ims .* isgns
+    return map((re,im)->Complex{DoubleFloat{T}}(re,im), res, ims)
+end
