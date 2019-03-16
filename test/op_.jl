4ec0cfb137851871ed0a9b8fa1ae9c6bc0153839
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-15 21:53:23 -0400

    Update op_.jl

--- test/op_.jl
+++ test/op_.jl
@@ -1,4 +1,6 @@
 @testset "op_dd_dd" begin
-    @test @df(neg_dd_dd, (100.0, 1.0e-20)) == (-100.0, -1.0e-20)
+    @test @df(neg_dd_dd, HILO(golden_df64)) == HILO(-golden_df64)
+    @test @df(abs_dd_dd, HILO(-golden_df64)) == HILO(golden_df64))
+    @test @df(negabs_dd_dd, HILO(golden_df32)) == (-abs(HI(golden_df32)), -abs(LO(golden_df32))
+    @test sum(abs.(HILO(golden_df64) .- @df(inv_dd_dd, @df(inv_dd_dd, HILO(golden_df64))))) < 1.0e-30
 end
-
