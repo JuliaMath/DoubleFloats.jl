7a36348db14dd8d83b919f38d4dd2f79195f43c2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-15 22:10:34 -0400

    Update op_.jl

--- test/op_.jl
+++ test/op_.jl
@@ -2,7 +2,7 @@
 
     @test @df(neg_dd_dd, HILO(gamma_df64)) == HILO(-gamma_df64)
     @test @df(abs_dd_dd, HILO(-gamma_df64)) == HILO(gamma_df64)
-    @test @df(negabs_dd_dd, HILO(gamma_df32)) == (-abs(HI(gamma_df32)), -abs(LO(gamma_df32)))
+    @test @df(negabs_dd_dd, -(gamma_df32)) == -(abs(-gamma_df32))
     @test sum(abs.(HILO(gamma_df64) .- @df(inv_dd_dd, @df(inv_dd_dd, HILO(gamma_df64))))) < 1.0e-30
 
 end
