3bea971f74642270922c31927198b84e28895be5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-25 07:31:37 -0400

    remove remhalfpi, remqrtrpi

--- test/modrem.jl
+++ test/modrem.jl
@@ -10,11 +10,7 @@ end
     x=cbrt(41)*sqrt(Double64(pi))
     @test DoubleFloats.rem2pi(x) == Double64(6.111805926475162, -1.667563077572613e-16)
     @test DoubleFloats.rem1pi(x) == Double64(2.9702132728853683, 1.54868222178066e-16)
-    @test DoubleFloats.remhalfpi(x) == Double64(1.3994169460904717, 9.363588222069835e-17)
-    @test DoubleFloats.remqrtrpi(x) == Double64(0.6140187826930236, -4.8002590220501136e-17)
 
     @test DoubleFloats.rem2pi(-x) == Double64(-6.111805926475162, 1.667563077572613e-16)
     @test DoubleFloats.rem1pi(-x) == Double64(-2.9702132728853683, -1.54868222178066e-16)
-    @test DoubleFloats.remhalfpi(-x) == Double64(-1.3994169460904717, -9.363588222069835e-17)
-    @test DoubleFloats.remqrtrpi(-x) == Double64(-0.6140187826930236, 4.8002590220501136e-17)
 end
