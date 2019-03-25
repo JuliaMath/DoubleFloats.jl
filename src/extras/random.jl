921ec1f7c487a943f7906fc53a5df3d8a9830fa9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-25 12:53:36 -0400

    improve

--- src/extras/random.jl
+++ src/extras/random.jl
@@ -20,30 +20,59 @@ function rand(rng::AbstractRNG, ::Random.SamplerTrivial{Random.CloseOpen01{Compl
     return Complex{DoubleFloat{T}}(re, im)
 end
 
-function randpm(::Type{DoubleFloat{T}}) where {T}
+function randpm(::Type{DoubleFloat{T}}) where {T<:IEEEFloat}
     r = rand(DoubleFloat{T})
     r = rand(Bool) ? r : -r
     return r
 end
 
-function randpm(::Type{Complex{DoubleFloat{T}}}) where {T}
+function randpm(rng::MersenneTwister, ::Type{DoubleFloat{T}}) where {T<:IEEEFloat}
+    r = rand(rng, DoubleFloat{T})
+    r = rand(Bool) ? r : -r
+    return r
+end
+
+function randpm(::Type{Complex{DoubleFloat{T}}}) where {T<:IEEEFloat}
     re = randpm(DoubleFloat{T})
     im = randpm(DoubleFloat{T})
     return Complex{DoubleFloat{T}}(re, im)
 end
 
-function randpm(::Type{DoubleFloat{T}}, n::Int) where {T}
+function randpm(rng::MersenneTwister, ::Type{Complex{DoubleFloat{T}}}) where {T<:IEEEFloat}
+    re = randpm(rng, DoubleFloat{T})
+    im = randpm(rng, DoubleFloat{T})
+    return Complex{DoubleFloat{T}}(re, im)
+end
+
+function randpm(::Type{DoubleFloat{T}}, n::Int) where {T<:IEEEFloat}
     rs = rand(DoubleFloat{T}, n)
     sgns = ones(n) .- (2 .* rand(Bool, n))
     return rs .* sgns
 end
 
-function randpm(::Type{Complex{DoubleFloat{T}}}, n::Int) where {T}
+function randpm(::Type{Complex{DoubleFloat{T}}}, n::Int) where {T<:IEEEFloat}
     res = rand(DoubleFloat{T}, n)
     ims = rand(DoubleFloat{T}, n)
     o = ones(n)
     rsgns = o .- (2 .* rand(Bool,n))
     isgns = o .- (2 .* rand(Bool,n))
+    res = res .* rsgns
+    ims = ims .* isgns
+    return map((re,im)->Complex{DoubleFloat{T}}(re,im), res, ims)
+end
+
+function randpm(rng::MersenneTwister, ::Type{DoubleFloat{T}}, n::Int) where {T<:IEEEFloat}
+    rs = rand(rng, DoubleFloat{T}, n)
+    sgns = ones(n) .- (2 .* rand(rng, Bool, n))
+    return rs .* sgns
+end
+
+function randpm(rng::MersenneTwister, ::Type{Complex{DoubleFloat{T}}}, n::Int) where {T<:IEEEFloat}
+    res = rand(rng, DoubleFloat{T}, n)
+    ims = rand(rng, DoubleFloat{T}, n)
+    o = ones(n)
+    rsgns = o .- (2 .* rand(rng, Bool,n))
+    isgns = o .- (2 .* rand(rng, Bool,n))
     res = res .* rsgns
     ims = ims .* isgns
     return map((re,im)->Complex{DoubleFloat{T}}(re,im), res, ims)
