671cfaa5d42f8bf6b68ae18aee2e7be28e2e5e9d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-25 22:17:41 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ makedocs(
         "Overview" => "index.md",
         "Construction" => "construction.md",
         "Characteristics" => "characteristics.md",
-        "String, Show, Parse" => "stringshowparse.md",
+        "Show, String, Parse" => "stringshowparse.md",
         "Capabilities" => "capabilities.md",
         "Additional Capabilities" => "special.md",
         "Linear Algebra" => "linearalgebra.md",
