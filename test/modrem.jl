9dcc64f0546cd0c16d0dc7560d4d6c24f7672bab
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-04-08 15:06:39 -0400

    Update modrem.jl

--- test/modrem.jl
+++ test/modrem.jl
@@ -3,7 +3,7 @@
     @test DoubleFloats.mod2pi(x) == Double64(6.111805926475162, -1.667563077572613e-16)
     @test DoubleFloats.mod1pi(x) == Double64(2.9702132728853683, 1.54868222178066e-16)
     @test DoubleFloats.modhalfpi(x) == Double64(1.3994169460904717, 9.362948122802731e-17)
-    @test DoubleFloats.modqrtrpi(x) == Double64(0.6140187826930236, -4.8008991213172176e-17))
+    @test DoubleFloats.modqrtrpi(x) == Double64(0.6140187826930236, -4.8008991213172176e-17)
 end
 
 @testset "rempi" begin
