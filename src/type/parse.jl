dbdf31ee06f8c12a0f996fbeebfad17f83886b4f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-04-08 14:57:50 -0400

    parse Complex{DoubleFloat}

--- src/type/parse.jl
+++ src/type/parse.jl
@@ -77,3 +77,45 @@ function tryparse(::Type{DoubleFloat{Float16}}, str::S; base::Int=10) where {S<:
     x = tryparse(BigFloat, str, base=base)
     return ifelse(isnothing(x), nothing, DoubleFloat{Float16}(x))
 end
+
+function tryparse(::Type{Complex{DoubleFloat{Float64}}}, str::S; base::Int=10) where {S<:AbstractString}
+    x = tryparse(Complex{BigFloat}, str)
+    return ifelse(isnothing(x), nothing, Complex{DoubleFloat{Float64}}(x))
+end
+
+function tryparse(::Type{Complex{DoubleFloat{Float32}}}, str::S; base::Int=10) where {S<:AbstractString}
+    x = tryparse(Complex{BigFloat}, str)
+    return ifelse(isnothing(x), nothing, Complex{DoubleFloat{Float32}}(x))
+end
+
+function tryparse(::Type{Complex{DoubleFloat{Float16}}}, str::S; base::Int=10) where {S<:AbstractString}
+    x = tryparse(Complex{BigFloat}, str)
+    return ifelse(isnothing(x), nothing, Complex{DoubleFloat{Float16}}(x))
+end
+
+@inline function Complex{Double64}(str::S) where {S<:AbstractString}
+  oldprec = precision(BigFloat)
+  setprecision(BigFloat, 508)
+  cplxbf = tryparse(Complex{BigFloat}, str)
+  result = Complex{Double64}(Double64(real(cplxbf)), Double64(imag(cplxbf)))
+  setprecision(BigFloat, oldprec)
+  return result
+end
+
+@inline function Complex{Double32}(str::S) where {S<:AbstractString}
+  oldprec = precision(BigFloat)
+  setprecision(BigFloat, 254)
+  cplxbf = tryparse(Complex{BigFloat}, str)
+  result = Complex{Double32}(Double32(real(cplxbf)), Double32(imag(cplxbf)))
+  setprecision(BigFloat, oldprec)
+  return result
+end
+
+@inline function Complex{Double16}(str::S) where {S<:AbstractString}
+  oldprec = precision(BigFloat)
+  setprecision(BigFloat, 120)
+  cplxbf = tryparse(Complex{BigFloat}, str)
+  result = Complex{Double16}(Double16(real(cplxbf)), Double16(imag(cplxbf)))
+  setprecision(BigFloat, oldprec)
+  return result
+end
