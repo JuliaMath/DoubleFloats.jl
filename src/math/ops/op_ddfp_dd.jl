75c1260e823aea33c0ab35f102b075dbe4493025
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-04-24 00:08:49 -0400

    Update op_ddfp_dd.jl

--- src/math/ops/op_ddfp_dd.jl
+++ src/math/ops/op_ddfp_dd.jl
@@ -10,11 +10,23 @@ end
     return hi, lo
 end
 
+#=
 @inline function mul_ddfp_dd(x::Tuple{T,T}, y::T) where {T<:IEEEFloat}
     hi, lo = x
     hihi, hilo = two_prod(y, hi)
     lohi, lolo = two_prod(y, lo)
     hi, lo = two_sumof4(hihi, hilo, lohi, lolo)
+    return hi, lo
+end
+=#
+
+# Algorithm 12 from Tight and rigourous error bounds.  relative error <= 5u²
+@inline function mul_ddfp_dd(x::Tuple{T,T}, y::T) where T<:IEEEFloat
+    xhi, xlo = x
+    hi, lo = two_prod(xhi, y)
+    t = xlo * y
+    t = t + lo
+    hi, lo = two_hilo_sum(hi, t)
     return hi, lo
 end
 
