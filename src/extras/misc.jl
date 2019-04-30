78bb102ae5e0d21f080ae161875a62ad920d4251
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-04-30 05:59:26 -0400

    isapprox custom rtol

--- src/extras/misc.jl
+++ src/extras/misc.jl
@@ -1,3 +1,13 @@
+function Base.isapprox(x::DoubleFloat{T}, y::T; rtol::Real=atol>0 ? 0 : eps(T)^(5/8), atol::Real=0, nans::Bool=false, norm::Function) where {T<:IEEEFloat}
+    return isapprox(x, DoubleFloat{T}(y), rtol=rtol, atol=atol, nans=nans, norm=norm)
+end
+function Base.isapprox(x::T, y::DoubleFloat{T}; rtol::Real=atol>0 ? 0 : eps(T)^(5/8), atol::Real=0, nans::Bool=false, norm::Function) where {T<:IEEEFloat}
+    return isapprox(x, DoubleFloat{T}(y), rtol=rtol, atol=atol, nans=nans, norm=norm)
+end
+function Base.isapprox(x::DoubleFloat{T}, y::DoubleFloat{T}; rtol::Real=0, atol::Real=0, nans::Bool=false, norm::Function) where {T<:IEEEFloat}
+    return isapprox(x, DoubleFloat{T}(y), rtol=((rtol==0 && atol>0) ? 0 : (rtol==0 ? eps(DoubleFloat{T})^(5/8) : rtol)), atol=atol, nans=nans, norm=norm)
+end
+        
 function Base.lerpi(j::Integer, d::Integer, a::DoubleFloat{T}, b::DoubleFloat{T}) where {T}
     t = DoubleFloat{T}(j)/d
     a = fma(-t, a, a)
