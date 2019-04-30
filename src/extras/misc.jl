12bd991577776517cf79b135b2ad51c81be03f3d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-04-30 03:42:20 -0400

    compat throwbacks

--- src/extras/misc.jl
+++ src/extras/misc.jl
@@ -3,3 +3,17 @@ function Base.lerpi(j::Integer, d::Integer, a::DoubleFloat{T}, b::DoubleFloat{T}
     a = fma(-t, a, a)
     return fma(t, b, a)
 end
+
+# for compatibility with outside linalg functions of zeroed matrices
+function Base.:(+)(v::Vector{DoubleFloat{T}}, x::T) where {T}
+    return v .+ x
+end
+function Base.:(-)(v::Vector{DoubleFloat{T}}, x::T) where {T}
+    return v .- x
+end
+function Base.:(+)(m::Matrix{DoubleFloat{T}}, x::T) where {T}
+    return m .+ x
+end
+function Base.:(-)(m::Matrix{DoubleFloat{T}}, x::T) where {T}
+    return m .- x
+end
