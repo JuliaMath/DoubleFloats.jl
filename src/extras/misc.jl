ab9c417c38ecb80d04be1d2eefab440c0dbe1726
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-08 18:13:17 -0400

    use norm directly

--- src/extras/misc.jl
+++ src/extras/misc.jl
@@ -1,10 +1,10 @@
-function Base.isapprox(x::DoubleFloat{T}, y::T; atol::Real=0, rtol::Real=atol>0 ? 0 : eps(T)^(5/8), nans::Bool=false, norm::Function=LinearAlgebra.norm) where {T<:IEEEFloat}
+function Base.isapprox(x::DoubleFloat{T}, y::T; atol::Real=0, rtol::Real=atol>0 ? 0 : eps(T)^(5/8), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
     return isapprox(x, DoubleFloat{T}(y), atol=atol, rtol=rtol, nans=nans, norm=norm)
 end
-function Base.isapprox(x::T, y::DoubleFloat{T}; atol::Real=0, rtol::Real=atol>0 ? 0 : eps(T)^(5/8), nans::Bool=false, norm::Function=LinearAlgebra.norm) where {T<:IEEEFloat}
+function Base.isapprox(x::T, y::DoubleFloat{T}; atol::Real=0, rtol::Real=atol>0 ? 0 : eps(T)^(5/8), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
     return isapprox(DoubleFloat{T}(x), y, atol=atol, rtol=rtol, nans=nans, norm=norm)
 end
-function Base.isapprox(x::DoubleFloat{T}, y::DoubleFloat{T}; atol::Real=0, rtol::Real=atol>0 ? 0 : eps(DoubleFloat{T})^(5/8), nans::Bool=false, norm::Function=LinearAlgebra.norm) where {T<:IEEEFloat}
+function Base.isapprox(x::DoubleFloat{T}, y::DoubleFloat{T}; atol::Real=0, rtol::Real=atol>0 ? 0 : eps(DoubleFloat{T})^(5/8), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
     x == y || (isfinite(x) && isfinite(y) && abs(x-y) <= max(atol, rtol*max(abs(x), abs(y)))) || (nans && isnan(x) && isnan(y))
 end
         
