847e2c3b995fc515491f14c5873d17952d41821c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-08 18:53:51 -0400

    Update modrem.jl

--- test/modrem.jl
+++ test/modrem.jl
@@ -1,5 +1,6 @@
 @testset "modpi" begin
     x=cbrt(41)*sqrt(Double64(pi))
+
     @test DoubleFloats.mod2pi(x) == Double64(6.111805926475162, -1.667563077572613e-16)
     @test DoubleFloats.mod1pi(x) == Double64(2.9702132728853683, 1.54868222178066e-16)
     @test DoubleFloats.modhalfpi(x) == Double64(1.3994169460904717, 9.362948122802731e-17)
@@ -8,6 +9,7 @@ end
 
 @testset "rempi" begin
     x=cbrt(41)*sqrt(Double64(pi))
+
     @test DoubleFloats.rem2pi(x) == Double64(6.111805926475162, -1.667563077572613e-16)
     @test DoubleFloats.rem1pi(x) == Double64(2.9702132728853683, 1.54868222178066e-16)
     @test DoubleFloats.rem2pi(-x) == Double64(-6.111805926475162, 1.667563077572613e-16)
