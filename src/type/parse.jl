86688184d6d3adee94edd11ac6d325c7bf1e8408
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-12 18:46:08 -0400

    remove deprecated

--- src/type/parse.jl
+++ src/type/parse.jl
@@ -34,35 +34,6 @@ macro df16_str(val::AbstractString)
   :(Double16($val))
 end
 
-# DEPRECATED
-const used_d64 = Ref(false)
-const used_d32 = Ref(false)
-const used_d16 = Ref(false)
-
-macro d64_str(val::AbstractString)
-  if !used_d64[]
-    used_d64[] = true
-    @warn("d64\"x\" is deprecated, use df64\"x\"")
-  end 
-  :(Double64($val))
-end
-
-macro d32_str(val::AbstractString)
-  if !used_d32[]
-    used_d32[] = true
-    @warn("d32\"x\" is deprecated, use df32\"x\"")
-  end
-  :(Double32($val))
-end
-
-macro d16_str(val::AbstractString)
-  if !used_d16[]
-    used_d16[] = true
-    @warn("d16\"x\" is deprecated, use df16\"x\"")
-  end
-  :(Double16($val))
-end
-
 function tryparse(::Type{DoubleFloat{Float64}}, str::S; base::Int=10) where {S<:AbstractString}
     x = tryparse(BigFloat, str, base=base)
     return ifelse(isnothing(x), nothing, DoubleFloat{Float64}(x))
