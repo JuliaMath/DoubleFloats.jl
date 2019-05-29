82c5e4378de05f3a959827735cf89e68d77bc84a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-29 16:21:34 -0400

    Update macros.jl

--- src/extras/macros.jl
+++ src/extras/macros.jl
@@ -1,15 +1,15 @@
-macro double64(fn, data)
+macro via_df64(fn, data)
     :( Float64.($(esc(fn)).(Double64.($(esc(data))))) )
 end
 
-macro double64(fn, xdata, ydata)
+macro via_df64(fn, xdata, ydata)
     :( Float64.($(esc(fn)).(Double64.($(esc(xdata))), Double64.($(esc(ydata))))) )
 end
 
-macro double32(fn, data)
+macro via_df32(fn, data)
     :( Float32.($(esc(fn)).(Double32.($(esc(data))))) )
 end
 
-macro double32(fn, xdata, ydata)
+macro via_df32(fn, xdata, ydata)
     :( Float32.($(esc(fn)).(Double32.($(esc(xdata))), Double32.($(esc(ydata))))) )
 end
