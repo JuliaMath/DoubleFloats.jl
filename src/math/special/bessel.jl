a7329faf6db15d58b1b0ce10858e923a8f987add
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-04 20:12:40 -0400

    make SpecialFunctions explicit

--- src/math/special/bessel.jl
+++ src/math/special/bessel.jl
@@ -1,4 +1,9 @@
 besselj0(x::Float128) = SpecialFunctions.besselj0(x)
+besselj1(x::Float128) = SpecialFunctions.besselj0(x)
+besselj(n::Integer, x::Float128) = SpecialFunctions.besselj(n,x)
+bessely0(x::Float128) = SpecialFunctions.besselj0(x)
+bessely1(x::Float128) = SpecialFunctions.besselj0(x)
+bessely(n::Integer, x::Float128) = SpecialFunctions.bessely(n,x)
 
 besselj0(x::Double64) = Double64Float128(besselj0, x)
 besselj1(x::Double64) = Double64Float128(besselj1, x)
