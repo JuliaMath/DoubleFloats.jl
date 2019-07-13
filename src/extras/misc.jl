d21ec78dbb8f4b947e50336be47ebb38728a920b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-13 19:49:54 -0400

    update `isapprox` `rtol=eps(10)^(37/64)`

--- src/extras/misc.jl
+++ src/extras/misc.jl
@@ -1,13 +1,20 @@
-function Base.isapprox(x::DoubleFloat{T}, y::T; atol::Real=0, rtol::Real=atol>0 ? 0 : eps(T)^(5/8), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
+function Base.isapprox(x::DoubleFloat{T}, y::T; atol::Real=0.0, rtol::Real=atol>0 ? 0 : eps(T)^(37/64), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
     return isapprox(x, DoubleFloat{T}(y), atol=atol, rtol=rtol, nans=nans, norm=norm)
 end
-function Base.isapprox(x::T, y::DoubleFloat{T}; atol::Real=0, rtol::Real=atol>0 ? 0 : eps(T)^(5/8), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
+function Base.isapprox(x::T, y::DoubleFloat{T}; atol::Real=0.0, rtol::Real=atol>0 ? 0 : eps(T)^(37/64), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
     return isapprox(DoubleFloat{T}(x), y, atol=atol, rtol=rtol, nans=nans, norm=norm)
 end
-function Base.isapprox(x::DoubleFloat{T}, y::DoubleFloat{T}; atol::Real=0, rtol::Real=atol>0 ? 0 : eps(DoubleFloat{T})^(5/8), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
+function Base.isapprox(x::DoubleFloat{T}, y::DoubleFloat{T}; atol::Real=0.0, rtol::Real=atol>0 ? 0 : eps(DoubleFloat{T})^(37/64), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
     x == y || (isfinite(x) && isfinite(y) && abs(x-y) <= max(atol, rtol*max(abs(x), abs(y)))) || (nans && isnan(x) && isnan(y))
 end
-        
+
+Base.isapprox(x::DoubleFloat{T}, y::F; atol::Real=0.0, rtol::Real=atol>0.0 ? 0.0 : eps(max(abs(x), abs(y)))^(37/64), nans::Bool=false, norm::Function=norm) where {F<:Real} =
+    isapprox(promote(x, y)..., atol=atol, rtol=rtol, nans=nans, norm=norm)
+
+Base.isapprox(x::F, y::DoubleFloat{T}; atol::Real=0.0, rtol::Real=atol>0.0 ? 0.0 : eps(max(abs(x), abs(y)))^(37/64), nans::Bool=false, norm::Function=norm) where {F<:Real} =
+    isapprox(promote(x, y)..., atol=atol, rtol=rtol, nans=nans, norm=norm)
+
+
 function Base.lerpi(j::Integer, d::Integer, a::DoubleFloat{T}, b::DoubleFloat{T}) where {T}
     t = DoubleFloat{T}(j)/d
     a = fma(-t, a, a)
