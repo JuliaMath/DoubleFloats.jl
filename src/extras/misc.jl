eb2a15a39d22e164d278aea81c575a8c135de357
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-15 15:45:35 -0400

    Update misc.jl

--- src/extras/misc.jl
+++ src/extras/misc.jl
@@ -16,7 +16,7 @@ end
 function Base.isapprox(x::T, y::DoubleFloat{T}; atol::Real=0.0, rtol::Real=atol>0 ? 0 : releps(T), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
     return isapprox(DoubleFloat{T}(x), y, atol=atol, rtol=rtol, nans=nans, norm=norm)
 end
-function Base.isapprox(x::DoubleFloat{T}, y::DoubleFloat{T}; atol::Real=0.0, rtol::Real=atol>0 ? 0 : eps(DoubleFloat{T})^(37/64), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
+function Base.isapprox(x::DoubleFloat{T}, y::DoubleFloat{T}; atol::Real=0.0, rtol::Real=atol>0 ? 0 : releps(DoubleFloat{T}), nans::Bool=false, norm::Function=norm) where {T<:IEEEFloat}
     x == y || (isfinite(x) && isfinite(y) && abs(x-y) <= max(max(1.0e-32, atol), rtol*max(abs(x), abs(y)))) || (nans && isnan(x) && isnan(y))
 end
 
