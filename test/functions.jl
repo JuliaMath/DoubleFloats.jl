08c2d3116588da07aca2a116777b1861d51532ab
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-08-26 13:53:46 -0400

    fix test 0^0

--- test/functions.jl
+++ test/functions.jl
@@ -16,7 +16,7 @@ end
 @testset "power functions"  begin
     @test Double64(10.0)^0 == Double64(1.0)
     @test Double64(10.0)^1 == Double64(10.0)
-    @test_throws DomainError Double64(0.0)^0
+    @test Double64(0.0)^0 == Double64(1.0)
 end
 
 f64a = sqrt(2.0)
