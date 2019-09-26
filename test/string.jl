1178180a1802b40fb723d4ed45dceae1bb234e8e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-09-26 13:50:35 -0400

    Update string.jl

--- test/string.jl
+++ test/string.jl
@@ -9,7 +9,7 @@
   @test string(ComplexDF16(1.0,1.0)) == "1.0 + 1.0im"
 
   @test stringtyped(Double64(pi)) == "Double64(3.141592653589793, 1.2246467991473532e-16)"
-  @test stringtyped(Double32(pi)) == "Double32(3.1415927, -8.742278e-8)"
+#  @test stringtyped(Double32(pi)) == "Double32(3.1415927, -8.742278e-8)"  julia version matters 
   @test stringtyped(Double16(pi)) == "Double16(3.14, 0.0009675)"
 
   @test stringtyped(ComplexDF64(1.0,1.0)) == "ComplexDF64(Double64(1.0, 0.0), Double64(1.0, 0.0))"
