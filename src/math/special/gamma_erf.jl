a4064fcf6b09753f1050ba045bc7a3a49eb5f19a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-11-29 02:57:05 -0500

    Update gamma_erf.jl

--- src/math/special/gamma_erf.jl
+++ src/math/special/gamma_erf.jl
@@ -1,8 +1,3 @@
-#erf(x::Float128) = Quadmath.erf(x)
-#erfc(x::Float128) = Quadmath.erfc(x)
-#gamma(x::Float128) = Quadmath.gamma(x)
-#lgamma(x::Float128) = Quadmath.lgamma(x)
-  
 erf(x::Double64) = Double64Float128(erf, x)
 erfc(x::Double64) = Double64Float128(erfc, x)
 gamma(x::Double64) = Double64Float128(gamma, x)
