36df77d397e20bc844fd5b5635db8a05b348e048
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-01-06 01:48:08 -0500

    two_sum|diffof3 --> two_sum|diff

--- src/math/ops/op_ddfp_dd.jl
+++ src/math/ops/op_ddfp_dd.jl
@@ -1,12 +1,12 @@
 @inline function add_ddfp_dd(x::Tuple{T,T}, y::T) where {T<:IEEEFloat}
     hi, lo = x
-    hi, lo = two_sumof3(hi, y, lo)
+    hi, lo = two_sum(hi, y, lo)
     return hi, lo
 end
 
 @inline function sub_ddfp_dd(x::Tuple{T,T}, y::T) where {T<:IEEEFloat}
     hi, lo = x
-    hi, lo = two_sumof3(hi, lo, -y)
+    hi, lo = two_sum(hi, lo, -y)
     return hi, lo
 end
 
@@ -15,7 +15,7 @@ end
     hi, lo = x
     hihi, hilo = two_prod(y, hi)
     lohi, lolo = two_prod(y, lo)
-    hi, lo = two_sumof4(hihi, hilo, lohi, lolo)
+    hi, lo = two_sum(hihi, hilo, lohi, lolo)
     return hi, lo
 end
 =#
