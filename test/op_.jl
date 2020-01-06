ba81ff04b69b30f619122a20cc94ac76e5f0d098
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-01-06 01:24:51 -0500

    Add files via upload

--- test/op_.jl
+++ test/op_.jl
@@ -1,12 +1,3 @@
-@testset "op_fp_dd" begin
-
-    @test @df(neg_fp_dd, HI(gamma_df64)) == HI(-gamma_df64)
-    @test @df(abs_fp_dd, HI(-gamma_df64)) == HI(gamma_df64)
-    @test sum(@df(negabs_fp_dd, HI(-(gamma_df32))) .- HI(-(abs(-gamma_df32)))) < 1.0e-15
-    @test sum(abs.(HI(gamma_df64) .- @df(inv_fp_dd, @df(inv_fp_dd, HI(gamma_df64))))) < 1.0e-15
-
-end
-
 @testset "op_dd_dd" begin
 
     @test @df(neg_dd_dd, HILO(gamma_df64)) == HILO(-gamma_df64)
