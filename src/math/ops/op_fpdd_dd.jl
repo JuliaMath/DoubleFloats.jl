5c1bc7dfb9170abcc0930405eb2b5dc434aedd22
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-01-06 01:52:07 -0500

    improve mul_fpdd_dd

--- src/math/ops/op_fpdd_dd.jl
+++ src/math/ops/op_fpdd_dd.jl
@@ -10,6 +10,16 @@ end
     return yhi, ylo
 end
 
+# Algorithm 12 from Tight and rigourous error bounds.  relative error <= 5u²
+@inline function mul_fpdd_dd(x::T, y::Tuple{T,T}) where T<:IEEEFloat
+    yhi, ylo = y
+    hi, lo = two_prod(yhi, x)
+    t = ylo * x
+    t = t + lo
+    hi, lo = two_hilo_sum(yhi, t)
+    return hi, lo
+end
+#=
 @inline function mul_fpdd_dd(x::T, y::Tuple{T,T}) where {T<:IEEEFloat}
     hi, lo = y
     hihi, hilo = two_prod(x, hi)
@@ -17,6 +27,7 @@ end
     hi, lo = two_sum(hihi, hilo, lohi, lolo)
     return hi, lo
 end
+=#
 
 @inline function dvi_fpdd_dd(x::T, y::Tuple{T,T}) where {T<:IEEEFloat}
     yinv = inv_dd_dd(y)
