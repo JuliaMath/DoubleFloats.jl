df587b39c11b57a4ab5894dfa675f36ce0ab9330
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-02-17 13:14:20 -0500

    Update show.jl

--- src/type/show.jl
+++ src/type/show.jl
@@ -1,7 +1,7 @@
 import Printf: ini_dec, fix_dec, ini_hex, ini_HEX
 
-Base.show(io::IO, ::Type{Double64}) = print(io, "Double64")
-Base.show(io::IO, ::Type{Double32}) = print(io, "Double32")
+show(io::IO, ::Type{Double64}) = print(io, "Double64")
+show(io::IO, ::Type{Double32}) = print(io, "Double32")
 
 function show(io::IO, x::DoubleFloat{T}) where {T<:IEEEFloat}
     compact = get(io, :compact, true)
