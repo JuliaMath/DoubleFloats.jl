310c0914509c481dc8ddf85d2d9b58cf30304916
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-02-29 12:49:58 -0500

    ishermitian (-0.0 handling)

--- test/linearalgebra.jl
+++ test/linearalgebra.jl
@@ -6,4 +6,8 @@
     @test_throws ErrorException exp(M)
     M = Complex{Double64}.([1 0; 1 1])
     @test_throws ErrorException exp(M)
+    
+    t=[Complex{Double64}(1.0,0.0) Complex{Double64}(0.0,1.0);
+       Complex{Double64}(0.0,-1.0) Complex{Double64}(1.0,0.0)]
+    @test ishermitian(t)
 end
