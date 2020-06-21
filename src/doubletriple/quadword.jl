e82619ad39c1553906e8b725fe39c279767c5d9c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-06-20 23:09:40 -0400

    Create quadword.jl

---
+++ src/doubletriple/quadword.jl
@@ -0,0 +1,7 @@
+function vec_sum(x0::T, x1::T, x2::T, x3::T) where {T}
+    s3 = x3
+    s2, e3 = two_sum(x2, s3)
+    s1, e2 = two_sum(x1, s2)
+    s0, e1 = two_sum(x0, s1)
+    return s0,e1,e2,e3
+end
