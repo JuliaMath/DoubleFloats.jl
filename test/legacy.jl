4ab135244c135f55c80dc8dc64adde075a226ec7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-11-17 18:31:06 -0500

    adjust for removed GRISU

--- test/legacy.jl
+++ test/legacy.jl
@@ -5,7 +5,7 @@ if VERSION < v"1.1"
     @test Base.Printf.fix_dec(Double64(-12.34567), 10) == (7, 2, true)
     @test Base.Printf.ini_dec(Double64(0.1234567), 3) == (3, 0, false)
     @test Base.Printf.ini_dec(Double64(-1234.567), 10) == (10, 4, true)
-else
+elseif VERSION < v"1.5"
     digits = Base.Grisu.getbuf()
     @test Printf.fix_dec(Double64(123.4567), 2, digits) == (5, 3, false)
     @test Printf.fix_dec(Double64(-12.34567), 10, digits) == (7, 2, true)
