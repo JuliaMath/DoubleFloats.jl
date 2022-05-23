675ab0f20a74c891875deefd9fdaf0e22af95f21
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-23 15:38:07 -0400

    update

--- test/specialvalues.jl
+++ test/specialvalues.jl
@@ -1,4 +1,4 @@
-@testset "maxintfloat $T" for T in (Double16, Double32, Double64)
+@testset "maxintfloat $T" for T in (Double32, Double64)
     @test isinteger(maxintfloat(T))
 
     # Previous integer is representable, next integer is not
