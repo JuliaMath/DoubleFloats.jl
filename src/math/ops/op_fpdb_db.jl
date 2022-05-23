71a9db3ad3cc49d89f44bbc8e7326c432806024c
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-23 12:38:22 -0400

    update

--- src/math/ops/op_fpdb_db.jl
+++ src/math/ops/op_fpdb_db.jl
@@ -1,20 +1,20 @@
 @inline function add_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:IEEEFloat}
-    isfinite(HI(x) + HI(y)) && return DoubleFloat{T}(add_fpdb_dd(x, HILO(y)))
+    isfinite(HI(x) + HI(y)) && return DoubleFloat{T}(add_fpdd_dd(x, HILO(y)))
     add_fpdb_db_nonfinite(x, y)
 end
 
 @inline function sub_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:IEEEFloat}
-    isfinite(HI(x) - HI(y)) && return DoubleFloat{T}(sub_fpdb_dd(x, HILO(y)))
+    isfinite(HI(x) - HI(y)) && return DoubleFloat{T}(sub_fpdd_dd(x, HILO(y)))
     sub_fpdb_db_nonfinite(x, y)
 end
 
 @inline function mul_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:IEEEFloat}
-    isfinite(HI(x) * HI(y)) && return DoubleFloat{T}(mul_fpdb_dd(x, HILO(y)))
+    isfinite(HI(x) * HI(y)) && return DoubleFloat{T}(mul_fpdd_dd(x, HILO(y)))
     mul_fpdb_db_nonfinite(x, y)
 end
 
 @inline function dvi_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:IEEEFloat}
-    isfinite(HI(x) / HI(y)) && return DoubleFloat{T}(dvi_fpdb_dd(x, HILO(y)))
+    isfinite(HI(x) / HI(y)) && return DoubleFloat{T}(dvi_fpdd_dd(x, HILO(y)))
     dvi_fpdb_db_nonfinite(x, y)
 end
 
