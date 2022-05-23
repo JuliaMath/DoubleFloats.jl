dc6975db443bda9ca233dfd673afc8abd6875534
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-23 14:20:07 -0400

    check dvi using * and not using /

--- src/math/ops/op_fpdb_db.jl
+++ src/math/ops/op_fpdb_db.jl
@@ -14,7 +14,7 @@ end
 end
 
 @inline function dvi_fpdb_db(x::T, y::DoubleFloat{T}) where {T<:IEEEFloat}
-    isfinite(HI(x) / HI(y)) && return DoubleFloat{T}(dvi_fpdd_dd(x, HILO(y)))
+    isfinite(x * HI(y)) && return DoubleFloat{T}(dvi_fpdd_dd(x, HILO(y)))
     dvi_fpdb_db_nonfinite(x, y)
 end
 
