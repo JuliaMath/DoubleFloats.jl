ec2dc8f8070ec6f4305008d431335337078a0b03
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-23 12:42:15 -0400

    fixup

--- src/math/ops/op_dbfp_db.jl
+++ src/math/ops/op_dbfp_db.jl
@@ -1,20 +1,20 @@
 @inline function add_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:IEEEFloat}
-    isfinite(HI(x) + HI(y)) && return DoubleFloat{T}(add_dbfp_dd(HILO(x), y))
+    isfinite(HI(x) + HI(y)) && return DoubleFloat{T}(add_ddfp_dd(HILO(x), y))
     add_dbfp_db_nonfinite(x, y)
 end
 
 @inline function sub_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:IEEEFloat}
-    isfinite(HI(x) - HI(y)) && return DoubleFloat{T}(sub_dbfp_dd(HILO(x), y))
+    isfinite(HI(x) - HI(y)) && return DoubleFloat{T}(sub_ddfp_dd(HILO(x), y))
     sub_dbfp_db_nonfinite(x, y)
 end
 
 @inline function mul_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:IEEEFloat}
-    isfinite(HI(x) + HI(y)) && return DoubleFloat{T}(mul_dbfp_dd(HILO(x), y))
+    isfinite(HI(x) + HI(y)) && return DoubleFloat{T}(mul_ddfp_dd(HILO(x), y))
     mul_dbfp_db_nonfinite(x, y)
 end
 
 @inline function dvi_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:IEEEFloat}
-    isfinite(HI(x) / HI(y)) && return DoubleFloat{T}(dvi_dbfp_dd(HILO(x), y))
+    isfinite(HI(x) / HI(y)) && return DoubleFloat{T}(dvi_ddfp_dd(HILO(x), y))
     dvi_dbfp_db_nonfinite(x, y)
 end
 
