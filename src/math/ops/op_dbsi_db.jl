af057223800354dcdaf31aa2aa3912c5113d0518
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-23 12:10:48 -0400

    continue

--- src/math/ops/op_dbsi_db.jl
+++ src/math/ops/op_dbsi_db.jl
@@ -1,3 +1,4 @@
 @inline function rootn_dbsi_db(x::DoubleFloat{T}, n::Signed) where {T<:IEEEFloat}
-    return DoubleFloat{T}(rootn_ddsi_dd(HILO(x), n))
+    isfinite(HI(y)^(1/n)) && return DoubleFloat{T}(rootn_ddsi_dd(HILO(x), n))
+    rootn_dd_dd_nonfinite(HILO(x), n)
 end
