c9e9f57a2d4bb5dd9582056c9737838bab06ce11
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-23 12:09:15 -0400

    continue fixes

--- src/math/ops/op_db_db.jl
+++ src/math/ops/op_db_db.jl
@@ -11,21 +11,26 @@ end
 end
 
 @inline function inv_db_db(x::DoubleFloat{T}) where {T<:IEEEFloat}
-    return DoubleFloat{T}(inv_dd_dd(HILO(x)))
+    isfinite(1.0/HI(y)) && return DoubleFloat{T}(inv_dd_dd(HILO(x)))
+    add_dd_dd_nonfinite(HILO(x))
 end
 
 @inline function square_db_db(x::DoubleFloat{T}) where {T<:IEEEFloat}
-    return DoubleFloat{T}(square_dd_dd(HILO(x)))
+    isfinite(HI(y)^2) && return DoubleFloat{T}(square_dd_dd(HILO(x)))
+    square_dd_dd_nonfinite(HILO(x))
 end
 
 @inline function cube_db_db(x::DoubleFloat{T}) where {T<:IEEEFloat}
-    return DoubleFloat{T}(cube_dd_dd(HILO(x)))
+    isfinite(HI(y)^3) && return DoubleFloat{T}(cube_dd_dd(HILO(x)))
+    cube_dd_dd_nonfinite(HILO(x))
 end
 
 @inline function sqrt_db_db(x::DoubleFloat{T}) where {T<:IEEEFloat}
-    return DoubleFloat{T}(sqrt_dd_dd(HILO(x)))
+    isfinite(sqrt(HI(y))) && return DoubleFloat{T}(sqrt_dd_dd(HILO(x)))
+    sqrt_dd_dd_nonfinite(HILO(x))
 end
 
 @inline function cbrt_db_db(x::DoubleFloat{T}) where {T<:IEEEFloat}
-    return DoubleFloat{T}(cbrt_dd_dd(HILO(x)))
+    isfinite(cbrt(HI(y))) && return DoubleFloat{T}(cbrt_dd_dd(HILO(x)))
+    cbrt_dd_dd_nonfinite(HILO(x))
 end
