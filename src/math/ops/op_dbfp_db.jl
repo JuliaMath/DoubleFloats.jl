ad4c0f56c4ce79086196b4300b9d3d86b0bb2535
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-23 15:34:21 -0400

    update dvi

--- src/math/ops/op_dbfp_db.jl
+++ src/math/ops/op_dbfp_db.jl
@@ -14,7 +14,7 @@ end
 end
 
 @inline function dvi_dbfp_db(x::DoubleFloat{T}, y::T) where {T<:IEEEFloat}
-    (isfinite(HI(x) * HI(y)) && isfinite(HI(x) * inv(HI(y)))) && return DoubleFloat{T}(dvi_ddfp_dd(HILO(x), y))
+    (isfinite(HI(x) * y) && isfinite(HI(x) * inv(y))) && return DoubleFloat{T}(dvi_ddfp_dd(HILO(x), y))
     dvi_dbfp_db_nonfinite(x, y)
 end
 
