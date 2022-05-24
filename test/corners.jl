bcef3187efa3d7dfbb2df04f0d1edd5b48c61d78
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-24 14:15:04 -0400

    test division corner cases

--- test/corners.jl
+++ test/corners.jl
@@ -1,5 +1,18 @@
 # testing corner cases brought to light by KlausC (Klaus Crusius)
 
+posinfinity = Double64(Inf)
+neginfinity = -posinfinity
+poszero = Double64(0.0)
+negzero = -poszero
+postwo = Double64(2)
+negtwo = -postwo
+
+posinfinityf = HI(posinifinity)
+neginfinityf = HI(neginifinity)
+poszerof = HI(poszero)
+negzerof = HI(negzero)
+postwof = HI(postwo)
+negtwof = HI(negtwo)
 
 huge = floatmax(Float64) * (Double64(2)/3)
 tiny = inv(huge)
@@ -31,4 +44,31 @@ smallf = HI(small)
     @test isapprox(DoubleFloats.div2(tinyf, hugef), tinyf / hugef)
     @test isapprox(DoubleFloats.div2(hugef, hugef), hugef / hugef)
     @test isapprox(DoubleFloats.div2(tinyf, tinyf), tinyf / tinyf)
+end
+
+@testset "division corners" begin
+    @test isapprox(posinfinity / neginifinity, posinfinityf / neginifinityf)
+    @test isapprox(posinfinity / poszero, posinfinityf / poszerof)
+    @test isapprox(posinfinity / negzero, posinfinityf / negzerof)
+    @test isapprox(poszero / posinfinity, poszerof / posinfinityf)
+    @test isapprox(negzero / posinfinity, negzerof / posinfinityf)
+    @test isapprox(poszero / neginfinity, poszerof / neginfinityf)
+    @test isapprox(negzero / neginfinity, negzerof / neginfinityf)
+    @test isapprox(neginfinity / negzero, neginfinityf / negzerof)
+
+    @test isapprox(posinfinity / postwo, posinfinityf / postwof)
+    @test isapprox(posinfinity / negtwo, posinfinityf / negtwof)
+    @test isapprox(postwo / posinfinity, postwof / posinfinityf)
+    @test isapprox(negtwo / posinfinity, negtwof / posinfinityf)
+    @test isapprox(postwo / neginfinity, postwof / neginfinityf)
+    @test isapprox(negtwo / neginfinity, negtwof / neginfinityf)
+    @test isapprox(neginfinity / negtwo, neginfinityf / negtwof)
+
+    @test isapprox(postwo / poszero, postwof / poszerof)
+    @test isapprox(postwo / negzero, postwof / negzerof)
+    @test isapprox(poszero / postwo, poszerof / postwof)
+    @test isapprox(negzero / postwo, negzerof / postwof)
+    @test isapprox(poszero / negtwo, poszerof / negtwof)
+    @test isapprox(negzero / negtwo, negzerof / negtwof)
+    @test isapprox(negtwo / negzero, negtwof / negzerof)
 end
