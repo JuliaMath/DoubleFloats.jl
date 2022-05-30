3980e19c5c1466097ff0a3b950a9247aacb49321
JeffreySarnoff <jeffrey.sarnoff@gmail.com>
2022-05-29 22:12:29 -0400

    add tests from KlausC "not NaN and Nan"

--- test/notnan.jl
+++ test/notnan.jl
@@ -71,3 +71,41 @@ end
     @test isapprox(infd64 / zerof64, inff64 / zerod64)
     @test isapprox(zerod64 / infd64, zerof64 / infd64)
 end
+
+@testset "not NaN and NaN" begin
+
+    @testset "unary operators $op($T)" for op in (log, exp, sqrt, cbrt, square, cube, log2, log10, exp2, exp10),
+        T in (Double64, Double32, Double16)
+        
+        d = floatmax(T)
+        if op ∈ (exp, square, cube, exp2, exp10)
+            @test isinf(op(d))
+        else
+            @test isfinite(op(d))
+        end
+        @test isinf(T(2.1) ^ d)
+        @test isinf(3 ^ d)
+        @test iszero(0.999 ^ d)
+        @test isinf(10 ^ T(500.5))
+
+        d = -floatmax(T)
+        op ∉ (sqrt, log, log2, log10) && @test !isnan(op(d))
+        d = T(NaN)
+        @test isnan(op(d))
+    end
+
+    @testset "binary operators $T $op $T" for op in (+, -, *, /), T in (Double64, Double32, Double16)
+
+        d1, d2 = floatmax(T), floatmax(T)
+        @test !isnan(op(d1, d2))
+        d1, d2 = floatmax(T), -floatmax(T)
+        @test !isnan(op(d1, d2))
+        d1, d2 = T(Inf), floatmax(T)
+        @test !isnan(op(d1, d2))
+        d1, d2 = T(Inf), T(Inf)
+        @test isnan(op(d1, d2)) == (op ∈ (-, /))
+        d1, d2 = zero(T), T(NaN)
+        @test isnan(op(d1, d2))
+    end
+
+end
