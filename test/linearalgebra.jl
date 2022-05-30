2f97fc8fd3bf669f45bddd84c1e74f319bdf9123
JeffreySarnoff <jeffrey.sarnoff@gmail.com>
2022-05-29 23:26:50 -0400

    make norm tests compliant

--- test/linearalgebra.jl
+++ test/linearalgebra.jl
@@ -20,14 +20,14 @@ using GenericLinearAlgebra, GenericSchur
        Complex{Double64}(0.0,-1.0) Complex{Double64}(1.0,0.0)]
     @test ishermitian(t)
 
-    @test norm(Complex{Double64}[2, 2im]) ≈ norm(ComplexF64[2, 2im])
-    @test norm(Complex{Double64}[2, 2im]) isa Double64
+    @test norm([Complex{Double64}[2, 2im]]) ≈ norm([ComplexF64[2, 2im]])
+    @test norm([Complex{Double64}[2, 2im]]) isa Double64
 
-    @test norm(Complex{Double64}[2, 2im], 3.0) ≈ norm(ComplexF64[2, 2im], 3.0)
-    @test norm(Complex{Double64}[2, 2im], 3.0) isa Double64
+    @test norm([Complex{Double64}[2, 2im], 3.0]) ≈ norm([ComplexF64[2, 2im], 3.0])
+    @test norm([Complex{Double64}[2, 2im], 3.0]) isa Double64
 
-    @test norm(Double64[2, 1]) ≈ norm(Float64[2, 1])
-    @test norm(Double64[2, 1], 3.0) ≈ norm(Float64[2, 1], 3.0)
+    @test norm([Double64[2, 1]]) ≈ norm([Float64[2, 1]])
+    @test norm([Double64[2, 1], 3.0]) ≈ norm([Float64[2, 1], 3.0])
 
     # issue #105
     for T in [Double16, Double32, Double64]
