aee60faf375eacab293f065bd6847eaa92415125
JeffreySarnoff <jeffrey.sarnoff@gmail.com>
2022-05-30 11:37:38 -0400

    fix copypaste error

--- test/morearith.jl
+++ test/morearith.jl
@@ -10,7 +10,7 @@ d0, f0 = zero(DT), zero(FT)
 d1, f1 = one(DT), one(FT)
 
 dmin, fmin = floatmin(DT), floatmin(FT)
-dmax, fmin = floatmax(DT), floatmax(FT)
+dmax, fmax = floatmax(DT), floatmax(FT)
 dmin₊, fmin₊ = dmin * (1025/1024), fmin * (1025/1024)
 dmax₋, fmax₋ = dmax * (1023/1024), fmax * (1023/1024)
 
@@ -23,8 +23,8 @@ cbrt2_dmax, cbrt2_fmax = cbrt_dmax^2, cbrt_fmax^2
 
 sqrt_dmin₊, sqrt_fmin₊ = sqrt(dmin₊), sqrt(fmin₊)
 sqrt_dmax₋, sqrt_fmax₋ = sqrt(dmax₋), sqrt(fmax₋)
-cbrt_dmax₋, cbrt_fmax₋ = cbrt(dmax₋), cbrt(fmax₋)
-sqrt_dmax₋, sqrt_fmax₋ = sqrt(dmax₋), sqrt(fmax₋)
+cbrt_dmin₊, cbrt_fmin₊ = cbrt(dmin₊), cbrt(fmin₊)
+cbrt_dmax₋, sqrt_fmax₋ = sqrt(dmax₋), sqrt(fmax₋)
 cbrt2_dmin₊, cbrt2_fmin₊ = cbrt_dmin₊^2, cbrt_fmin₊^2 
 cbrt2_dmax₋, cbrt2_fmax₋ = cbrt_dmax₋^2, cbrt_fmax₋^2
 
