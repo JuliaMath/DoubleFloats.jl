9c0d5c4319b6565f268da29015a52fe2a185ff1a
JeffreySarnoff <jeffrey.sarnoff@gmail.com>
2022-05-30 15:41:49 -0400

    mul

--- src/math/ops/op_fpdd_dd.jl
+++ src/math/ops/op_fpdd_dd.jl
@@ -39,7 +39,7 @@ end
     zhi, zlo = mul_fpdd_dd(x, yinv)
     return zhi, zlo
 end
-
+=#
 @inline function dvi_fpdd_dd_fast(x::T, y::Tuple{T,T}) where {T<:IEEEFloat}
     yinv = inv_dd_dd_fast(y)
     zhi, zlo = mul_fpdd_dd(x, yinv)
