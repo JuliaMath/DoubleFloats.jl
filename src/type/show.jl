00f7ea4a7e40cda238eb969591347431bc630871
Oscar Smith <oscardssmith@gmail.com>
2023-02-14 10:19:56 -0500

    use `compact=false` by default when printing.
    When doing complex calculations with `DoubleFloat`s it is often really nice to have these extra digits to reassure you that you didn't accidentally lose precision from an accidental `Float64` cast. This will make display slower, but I think it's completely worth it (especially since the compact show can show incorrect decimal digits. For example
    ```
    julia> x=Double64(big(1)+5e-16)
    1.00000000000000050000000000000003885
    
    julia> x.hi
    1.0000000000000004
    ```

--- src/type/show.jl
+++ src/type/show.jl
@@ -4,7 +4,7 @@ show(io::IO, ::Type{Double64}) = print(io, "Double64")
 show(io::IO, ::Type{Double32}) = print(io, "Double32")
 
 function show(io::IO, x::DoubleFloat{T}) where {T<:IEEEFloat}
-    compact = get(io, :compact, true)
+    compact = get(io, :compact, false)
     if compact
         str = string(x.hi)
     else
@@ -16,7 +16,7 @@ end
 function show(io::IO, x::Complex{DoubleFloat{T}}) where {T<:IEEEFloat}
     re, im = reim(x)
     imstr = isfinite(im) ? "im" : "*im"
-    compact = get(io, :compact, true)
+    compact = get(io, :compact, false)
     if compact
         str = string(HI(re), (signbit(x.im.hi) ? " - " : " + "), abs(HI(im)), imstr)
     else
