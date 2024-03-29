f2d5ea408da398d42da286bcdec694f0f68894d7
araujoms <maltusan@gmail.com>
2024-03-29 12:11:53 +0100

    write logabsgamma in terms of lgamma, remove loggamma, and add tests

--- src/math/special/gamma_erf.jl
+++ src/math/special/gamma_erf.jl
@@ -2,24 +2,18 @@ erf(x::Double64) = Double64Float128(erf, x)
 erfc(x::Double64) = Double64Float128(erfc, x)
 gamma(x::Double64) = Double64Float128(gamma, x)
 lgamma(x::Double64) = Double64Float128(lgamma, x)
-loggamma(x::Double64) = Double64Float128(lgamma, x)
 
 erf(x::Double32) = Double32Float128(erf, x)
 erfc(x::Double32) = Double32Float128(erfc, x)
 gamma(x::Double32) = Double32Float128(gamma, x)
 lgamma(x::Double32) = Double32Float128(lgamma, x)
-loggamma(x::Double32) = Double32Float128(lgamma, x)
 
 erf(x::Double16) = Double16Float128(erf, x)
 erfc(x::Double16) = Double16Float128(erfc, x)
 gamma(x::Double16) = Double16Float128(gamma, x)
 lgamma(x::Double16) = Double16Float128(lgamma, x)
-loggamma(x::Double16) = Double16Float128(lgamma, x)
 
 function logabsgamma(x::DoubleFloat{T}) where {T<:IEEEFloat}
-    keep_precision = precision(BigFloat)
-    setprecision(BigFloat, 128)
-    result = logabsgamma(BigFloat(x))
-    setprecision(BigFloat, keep_precision)
-    return DoubleFloat{T}(result[1]), result[2]
+    sign = x >= 0 ? 1 : 2*mod(ceil(Int64,x),2)-1
+    return lgamma(x), sign
 end
--- test/special_functions.jl
+++ test/special_functions.jl
@@ -1,1 +1,18 @@
+using SpecialFunctions
 
+@testset "special functions" begin
+    # The intention here is not to check the accuracy of the libraries,
+    # rather the sanity of library calls:
+    piq = Double64(pi)
+    halfq = Double64(0.5)
+    @test gamma(halfq) ≈ sqrt(piq)
+    for func in (erf, erfc, besselj0, besselj1, bessely0, bessely1, loggamma)
+        @test func(halfq) ≈ func(0.5)
+    end
+    for func in (bessely, besselj)
+        @test func(3,halfq) ≈ func(3,0.5)
+    end
+    @test gamma(Double64(2),3) ≈ gamma(2,3)
+    @test all(logabsgamma(Double64(-0.5)) .≈ logabsgamma(-0.5))
+    @test all(logabsgamma(Double64(-1.5)) .≈ logabsgamma(-1.5))
+end
